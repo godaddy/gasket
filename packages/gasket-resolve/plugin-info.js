501d196da11559b438c9403661ff1c9e5470f135
Charlie Robbins <charlie.robbins@gmail.com>
2019-06-13 20:35:45 -0400

    @gasket/resolve w/ CircleCI (#3)
    * [api test] Prepare @gasket/resolve for OSS release.
    
    * [dist] Consistency
    
    * [dist] Add CircleCI config.
    
    * [dist fix] Learn the lerna.
    
    * [dist fix] Latest deps.
    
    * [dist] Moar deps.

---
+++ packages/gasket-resolve/plugin-info.js
@@ -0,0 +1,21 @@
+/**
+ * Returns the standard `PluginInfo` representation of the
+ * given opts
+ * @param  {String} options.shortName e.g. 'core' for '@gasket/core-plugin'
+ * @param  {String} options.range     Semver range required
+ * @param  {String} options.preset    Preset that resolved this PluginInfo
+ * @param  {Object} options.required  Result of plugin resolution (`module.exports`).
+ * @return {PluginInfo} Standard plugin format for all gasket purposes.
+ */
+module.exports = function pluginInfoFor({ shortName, range, preset, required, config }) {
+  return {
+    required: required,
+    kind: 'plugin',
+    from: preset,
+    shortName,
+    name: `@gasket/${shortName}-plugin`,
+    range,
+    // TODO: Load an optional dirname/preset.json if it exists
+    config: config || {}
+  };
+};
---
+++ packages/gasket-resolve/test/resolver.test.js
@@ -0,0 +1,70 @@
+const Resolver = require('../resolver');
+
+describe('Resolver', () => {
+  it('exposes expected methods', () => {
+    const resolver = new Resolver();
+    expect(resolver.pluginFor).toBeDefined();
+    expect(resolver.presetFor).toBeDefined();
+    expect(resolver.tryRequire).toBeDefined();
+    expect(resolver.resolveShorthandModule).toBeDefined();
+  });
+
+  describe('Resolver({ resolveFrom })', () => {
+    it('should resolve all plugins correctly', () => {
+      const mockA = {};
+      const mockB = {};
+
+      jest
+        .doMock('/whatever/node_modules/@gasket/a-plugin', () => mockA, { virtual: true })
+        .doMock('/whatever/node_modules/@gasket/b-plugin', () => mockB, { virtual: true });
+
+      const resolver = new Resolver({
+        resolveFrom: '/whatever/node_modules'
+      });
+
+      const actualA = resolver.pluginFor('a');
+      const actualB = resolver.pluginFor('b');
+
+      expect(actualA).toBe(mockA);
+      expect(actualB).toBe(mockB);
+    });
+
+    it('should resolve all presets correctly', () => {
+      const mockA = {};
+      const mockB = {};
+
+      jest
+        .doMock('/whatever/node_modules/@gasket/a-preset', () => mockA, { virtual: true })
+        .doMock('/whatever/node_modules/@gasket/b-preset', () => mockB, { virtual: true });
+
+      const resolver = new Resolver({
+        resolveFrom: '/whatever/node_modules'
+      });
+
+      const actualA = resolver.presetFor('a');
+      const actualB = resolver.presetFor('b');
+
+      expect(actualA).toBe(mockA);
+      expect(actualB).toBe(mockB);
+    });
+  });
+
+  describe('.tryRequire', () => {
+    it('suppresses errors for MODULE_NOT_FOUND for that module', () => {
+      const resolver = new Resolver();
+      const required = resolver.tryRequire('whatever-no-exists');
+      expect(required).toBeNull();
+    });
+
+    it('throws errors for MODULE_NOT_FOUND of other modules', () => {
+      jest.doMock('@gasket/whatever', () => {
+        return require('something-else-no-exist');
+      }, { virtual: true });
+
+      const resolver = new Resolver();
+      expect(() => {
+        resolver.tryRequire('@gasket/whatever');
+      }).toThrow(/something-else-no-exist/);
+    });
+  });
+});
