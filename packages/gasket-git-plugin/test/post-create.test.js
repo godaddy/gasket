04b6e2300ffb4eec9a7a7cb6beebdb8c34a53be6
Andrew Gerard <agerard@godaddy.com>
2019-07-23 16:07:38 -0700

    [minor] move git init commands to postCreate of git-plugin

---
+++ packages/gasket-git-plugin/test/post-create.test.js
@@ -0,0 +1,64 @@
+const sinon = require('sinon');
+const assume = require('assume');
+const proxyquire = require('proxyquire');
+
+describe('postCreate', () => {
+  let sandbox, mockContext, mockImports, postCreate;
+  let runStub;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+
+    mockContext = {
+      appName: 'my-app',
+      dest: '/some/path/my-app',
+      gitInit: true
+    };
+
+    runStub = sandbox.stub();
+
+    mockImports = {
+      '@gasket/utils': {
+        runShellCommand: runStub
+      }
+    };
+
+    postCreate = proxyquire('../lib/post-create', mockImports);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('ignores if gitInit is false', async () => {
+    mockContext.gitInit = false;
+    await postCreate({}, mockContext);
+    assume(runStub).not.called();
+  });
+
+  it('ignores if gitInit not set', async () => {
+    delete mockContext.gitInit;
+    await postCreate({}, mockContext);
+    assume(runStub).not.called();
+  });
+
+  it('uses context dest for cwd', async () => {
+    await postCreate({}, mockContext);
+    assume(runStub).is.calledWithMatch('git', ['init'], sinon.match({ cwd: mockContext.dest }));
+  });
+
+  it('inits repo', async () => {
+    await postCreate({}, mockContext);
+    assume(runStub).is.calledWithMatch('git', ['init']);
+  });
+
+  it('adds files', async () => {
+    await postCreate({}, mockContext);
+    assume(runStub).is.calledWithMatch('git', ['add', '.']);
+  });
+
+  it('makes a git commit', async () => {
+    await postCreate({}, mockContext);
+    assume(runStub).is.calledWithMatch('git', ['commit', '-m', sinon.match.string]);
+  });
+});
