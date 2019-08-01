2d32f13972a910739f6cc57cc25501f760b6fb9f
dsanudovacas <sanudovacasdaniel@gmail.com>
2019-08-01 12:59:03 +0100

    Adding @gasket/config-plugin

---
+++ packages/gasket-config-plugin/lib/merge-config-files.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { merge } = require('lodash');
+const { tryRequire } = require('@gasket/utils');
+
+module.exports = function loadConfig(gasket) {
+  const { root, env, configPath } = gasket.config;
+
+  let configChain = ['base'];
+
+  if (env === 'local') {
+    configChain = configChain.concat([
+      'dev',
+      'development',
+      'local',
+      'local.overrides.json',
+      'local.overrides.js'
+    ]);
+  } else {
+    const envSegments = env.split('.');
+    for (let i = 1; i <= envSegments.length; i++) {
+      configChain.push(envSegments.slice(0, i).join('.'));
+    }
+  }
+
+  const configDir = path.resolve(root, configPath || './config');
+
+  return configChain.reduce((result, name) => {
+    return merge(result, tryRequire(path.resolve(configDir, name)));
+  }, {});
+};
