f5e994c193f68a496a2442e1cec16a8b259182c5
Conor Fellin <39741547+cfellin1@users.noreply.github.com>
2019-08-14 08:26:10 -0700

    Add intl-plugin (#49)
    Co-authored by: Salil Agrawal <sagrawal@godaddy.com>

---
+++ packages/gasket-intl-plugin/lib/workbox.js
@@ -0,0 +1,53 @@
+const urljoin = require('url-join');
+const { createGetLanguage, getAssetPrefix } = require('./utils');
+
+const reModulePath = /(.*_locales\/)(.*)(\/.*)/;
+
+/**
+ * Encode the identifier part of the locale file uri
+ * This is necessary to match request for these assets by `@gasket/intl`
+ *
+ * @param {Object} originalManifest - Workbox manifest
+ * @returns {Object} results - Transformed manifest
+ */
+const encodeLocaleUrls = originalManifest => {
+  const manifest = originalManifest.reduce((acc, entry) => {
+    const { url } = entry;
+    if (url.includes('_locales/')) {
+      const [, root, module, file] = reModulePath.exec(url);
+      entry.url = [root, encodeURIComponent(module), file].join('');
+    }
+    acc.push(entry);
+    return acc;
+  }, []);
+  return { manifest };
+};
+
+/**
+ * Workbox config partial to add next.js static assets to precache
+ *
+ * @param {Gasket} gasket - Gasket
+ * @param {Object} config - Initial workbox config
+ * @param {Request} req - Request
+ * @returns {Promise<Object>} config
+ */
+module.exports = async function workbox(gasket, config, req) {
+  const language = createGetLanguage(gasket)(req);
+  const assetPrefix = getAssetPrefix(gasket);
+
+  return {
+    globDirectory: '.',
+    globPatterns: [
+      `build/locales/**/*.${language}.json`
+    ],
+    modifyURLPrefix: {
+      'build/locales/': urljoin(assetPrefix, '_locales/')
+    },
+    manifestTransforms: [
+      encodeLocaleUrls
+    ]
+  };
+};
+
+
+module.exports.encodeLocaleUrls = encodeLocaleUrls;
