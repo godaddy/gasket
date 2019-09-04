eebbbbaf5978c732c4fed3dd8ab7f210a47be09d
Andrew Gerard <agerard@godaddy.com>
2019-09-04 11:40:45 -0700

    [major] use loader to gather plugin and preset info for metadata

--- packages/gasket-metadata-plugin/index.js
+++ packages/gasket-metadata-plugin/index.js
@@ -1,31 +1,59 @@
 /* eslint require-atomic-updates: warn */
-const path = require('path');
+const cloneDeep = require('lodash.clonedeep');
+const isFunction = require('lodash.isfunction');
+const isObject = require('lodash.isobject');
+
+/**
+ * Recurse through an object or array, and transform any functions to be empty
+ *
+ * @param {Object|Array} value - Item to consider
+ * @returns {Object|Array} transformed result
+ */
+function sanitize(value) {
+  if (isObject(value)) {
+    Object.entries(value).forEach(([k, v]) => {
+      if (isFunction(v)) {
+        value[k] = function redacted() {
+        };
+      } else {
+        sanitize(v);
+      }
+    });
+  } else if (Array.isArray(value)) {
+    value.forEach(v => sanitize(v));
+  }
+
+  return value;
+}
+
+/**
+ * Preset module with meta data
+ *
+ * @typedef {Object} Metadata
+ * @property {PresetInfo[]} presets - Preset infos with dependency hierarchy
+ * @property {PluginInfo[]} plugins - Flat list of registered plugin infos
+ */
 
 module.exports = {
   name: 'metadata',
   hooks: {
     async init(gasket) {
-      const plugins = gasket._plugins;
-      const metadata = gasket.config.metadata.plugins;
+      const { loader, config } = gasket;
+      const loaded = loader.loadConfigured(config.plugins);
+      const { presets, plugins } = sanitize(cloneDeep(loaded));
 
-      Object.keys(plugins).forEach(plugin => {
-        // Plugins that are defined locally to the app don't count
-        if (plugin.indexOf(gasket.config.root) !== -1) {
-          return;
-        }
-
-        const relativePath = gasket.resolver.tryResolvePluginRelativePath(plugin);
-        const pluginPath = path.resolve(relativePath);
-        const pkg = require(path.join(pluginPath, 'package.json'));
-        const { hooks } = require(pluginPath);
-
-        metadata[plugin] = metadata[plugin] || {};
-        Object.assign(metadata[plugin], pkg);
-        metadata[plugin].hooks = hooks;
-      });
+      /**
+       * @type {Metadata}
+       */
+      gasket.metadata = {
+        presets,
+        plugins
+      };
 
       await gasket.execApply('metadata', async ({ name }, handler) => {
-        metadata[name] = await handler(metadata[name]);
+        const idx = plugins.findIndex(p => p.module.name === name || p.name === name);
+        const pluginInfo = plugins[idx];
+        plugins[idx] = await handler(pluginInfo);
       });
     }
   }
