31cd100627ad068c5513848526da1efc8cb03679
Andrew Gerard <agerard@godaddy.com>
2019-09-16 15:04:22 -0700

    [major] configure hook

---
+++ packages/gasket-docs-plugin/lib/configure.js
@@ -0,0 +1,30 @@
+const merge = require('deepmerge');
+
+/**
+ * Docs defaults
+ */
+const defaultConfig = {
+  generate: true,
+  clean: true,
+  dir: '.docs',
+  docsify: {
+    theme: 'vue',
+    port: 3000,
+    config: {
+    }
+  }
+};
+
+/**
+ * Configure lifecycle to set up SW config with defaults
+ *
+ * @param {Gasket} gasket - Gasket
+ * @param {Object} baseConfig - Base gasket config
+ * @returns {Object} config
+ */
+module.exports = async function configureHook(gasket, baseConfig = {}) {
+  const userConfig = baseConfig.docs || {};
+
+  const docs = merge.all([defaultConfig, userConfig]);
+  return ({ ...baseConfig, docs });
+};
