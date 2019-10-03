fae735ed54330cf79d93384572a39c5290c6b2f1
Andrew Gerard <agerard@godaddy.com>
2019-10-03 12:46:30 -0700

    [tiny] breakup to smaller units of work

--- packages/gasket-metadata-plugin/lib/index.js
+++ packages/gasket-metadata-plugin/lib/index.js
@@ -6,6 +6,72 @@ const { sanitize, expand } = require('./utils');
 const isPlugin = name => pluginIdentifier.isValidFullName(name);
 const isPreset = name => presetIdentifier.isValidFullName(name);
 
+//
+// Add moduleData for app dependencies
+//
+function loadAppModules(loader, metadata) {
+  const { app, modules } = metadata;
+  Object.keys(app.package.dependencies)
+    .filter(name => !isPlugin(name) && !isPreset(name))
+    .forEach(name => {
+      const range = app.package.dependencies[name];
+      const moduleInfo = loader.getModuleInfo(null, name, { from: app.name, range });
+      modules.push(moduleInfo);
+    });
+}
+
+//
+// load moduleInfo for any supporting modules that are declared
+//
+function loadPluginModules(loader, plugins) {
+  plugins.forEach(pluginData => {
+    if (pluginData.modules) {
+      pluginData.modules = pluginData.modules
+        .map(mod => isObject(mod) ? mod : { name: mod })  // normalize string names to objects
+        .map(mod => loader.getModuleInfo(null, mod.name, mod));
+    }
+  });
+}
+
+//
+// flatten moduleData from plugin
+//
+function flattenPluginModules(metadata){
+  const { plugins, modules } = metadata;
+  plugins.reduce((acc, pluginData) => {
+    if (pluginData.modules) {
+      pluginData.modules.forEach(moduleData => {
+        const existing = acc.find(mod => mod.name === moduleData.name);
+        if (existing) {
+          expand(existing, moduleData);
+        } else {
+          acc.push(moduleData);
+        }
+      });
+    }
+  }, modules);
+}
+
+
+//
+// assign plugin instances back to preset hierarchy to avoid faulty data
+//
+function fixupHierarchy(metadata) {
+  const { plugins, presets } = metadata;
+
+  plugins.forEach(pluginData => {
+    function checkPreset(presetData) {
+      if (presetData.name === pluginData.from) {
+        const idx = presetData.plugins.findIndex(p => p.name === pluginData.name);
+        presetData.plugins[idx] = pluginData;
+      }
+      presetData.presets && presetData.presets.forEach(checkPreset);
+    }
+
+    presets.forEach(checkPreset);
+  });
+}
+
 module.exports = {
   name: 'metadata',
   hooks: {
@@ -26,6 +92,8 @@ module.exports = {
         modules: []
       };
 
+      loadAppModules(loader, gasket.metadata);
+
       //
       // Allow plugins to tune their own metadata via lifecycle
       //
@@ -36,57 +104,9 @@ module.exports = {
         plugins[idx] = await handler(pluginData);
       });
 
-      //
-      // Add moduleData for app dependencies
-      //
-      Object.keys(app.package.dependencies)
-        .filter(name => !isPlugin(name) && !isPreset(name))
-        .forEach(name => {
-          const range = app.package.dependencies[name];
-          const moduleInfo = loader.getModuleInfo(null, name, { from: app.name, range });
-          gasket.metadata.modules.push(moduleInfo);
-        });
-
-      //
-      // load moduleInfo for any supporting modules that are declared
-      //
-      plugins.forEach(pluginData => {
-        if (pluginData.modules) {
-          pluginData.modules = pluginData.modules
-            .map(mod => isObject(mod) ? mod : { name: mod })  // normalize string names to objects
-            .map(mod => loader.getModuleInfo(null, mod.name, mod));
-        }
-      });
-
-      //
-      // flatten moduleData from plugin
-      //
-      plugins.reduce((modules, pluginData) => {
-        if (pluginData.modules) {
-          pluginData.modules.forEach(moduleData => {
-            const existing = modules.find(mod => mod.name === moduleData.name);
-            if (existing) {
-              expand(existing, moduleData);
-            } else {
-              modules.push(moduleData);
-            }
-          });
-        }
-      }, gasket.metadata.modules);
-
-      //
-      // assign plugin instances back to preset hierarchy to avoid faulty data
-      //
-      plugins.forEach(pluginData => {
-        function checkPreset(presetData) {
-          if (presetData.name === pluginData.from) {
-            const idx = presetData.plugins.findIndex(p => p.name === pluginData.name);
-            presetData.plugins[idx] = pluginData;
-          }
-          presetData.presets && presetData.presets.forEach(checkPreset);
-        }
-        presets.forEach(checkPreset);
-      });
+      loadPluginModules(loader, gasket.metadata.plugins);
+      flattenPluginModules(gasket.metadata);
+      fixupHierarchy(gasket.metadata);
     },
     metadata(gasket, pluginData) {
       return {
