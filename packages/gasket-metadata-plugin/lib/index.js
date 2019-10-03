a50480791253aaef64036b9bf851058611a18e0d
Andrew Gerard <agerard@godaddy.com>
2019-10-03 13:42:45 -0700

    [tiny] move detail funcs to utils

--- packages/gasket-metadata-plugin/lib/index.js
+++ packages/gasket-metadata-plugin/lib/index.js
@@ -1,84 +1,11 @@
 const cloneDeep = require('lodash.clonedeep');
-const isObject = require('lodash.isobject');
-const { pluginIdentifier, presetIdentifier } = require('@gasket/resolve');
-const { sanitize, expand } = require('./utils');
-
-const isPlugin = name => pluginIdentifier.isValidFullName(name);
-const isPreset = name => presetIdentifier.isValidFullName(name);
-
-/**
- * Load moduleData for app dependencies
- *
- * @param {Loader} loader - Loader instance
- * @param {ModuleData} app - Metadata for app
- * @param {ModuleData[]} modules - Metadata for modules
- */
-function loadAppModules(loader, app, modules) {
-  Object.keys(app.package.dependencies)
-    .filter(name => !isPlugin(name) && !isPreset(name))
-    .forEach(name => {
-      const range = app.package.dependencies[name];
-      const moduleData = loader.getModuleInfo(null, name, { from: app.name, range });
-      modules.push(moduleData);
-    });
-}
-
-/**
- * Load moduleData for any supporting modules that are declared by plugin
- *
- * @param {PluginData} pluginData - Metadata for a plugin
- * @param {Loader} loader - Loader instance
- */
-function loadPluginModules(pluginData, loader) {
-  if (pluginData.modules) {
-    pluginData.modules = pluginData.modules
-      .map(mod => isObject(mod) ? mod : { name: mod })  // normalize string names to objects
-      .map(mod => loader.getModuleInfo(null, mod.name, mod));
-  }
-}
-
-/**
- * Flatten moduleData from plugin to top-level modules metadata
- *
- * @param {PluginData} pluginData - Metadata for a plugin
- * @param {ModuleData[]} modules - Metadata for modules
- */
-function flattenPluginModules(pluginData, modules) {
-  if (pluginData.modules) {
-    pluginData.modules.forEach(moduleData => {
-      const existing = modules.find(mod => mod.name === moduleData.name);
-      if (existing) {
-        expand(existing, moduleData);
-      } else {
-        modules.push(moduleData);
-      }
-    });
-  }
-}
-
-/**
- * Assign modified plugin data instances back to preset hierarchy to avoid faulty data
- *
- * @param {PluginData} pluginData - Metadata for a plugin
- * @param {ModuleData[]} presets - Metadata for presets
- */
-function fixupPresetHierarchy(pluginData, presets) {
-
-  /**
-   * Recursing fixer-upper
-   *
-   * @param {PresetData} presetData - Preset to fixup
-   */
-  function checkPreset(presetData) {
-    if (presetData.name === pluginData.from) {
-      const idx = presetData.plugins.findIndex(p => p.name === pluginData.name);
-      presetData.plugins[idx] = pluginData;
-    }
-    presetData.presets && presetData.presets.forEach(checkPreset);
-  }
-
-  presets.forEach(checkPreset);
-}
+const {
+  sanitize,
+  loadAppModules,
+  loadPluginModules,
+  flattenPluginModules,
+  fixupPresetHierarchy
+} = require('./utils');
 
 module.exports = {
   name: 'metadata',
--- packages/gasket-metadata-plugin/lib/utils.js
+++ packages/gasket-metadata-plugin/lib/utils.js
@@ -1,5 +1,10 @@
 const isFunction = require('lodash.isfunction');
 const isObject = require('lodash.isobject');
+const { pluginIdentifier, presetIdentifier } = require('@gasket/resolve');
+
+// Shortcuts
+const isPlugin = name => pluginIdentifier.isValidFullName(name);
+const isPreset = name => presetIdentifier.isValidFullName(name);
 
 /**
  * Recurse through an object or array, and transforms, by mutation,
@@ -35,7 +40,7 @@ function sanitize(value) {
  * @param {Object} target - Object to mutate
  * @param {Object} other - Object to pull from
  */
-function expand(target, other) {
+function safeAssign(target, other) {
   Object.keys(other)
     .reduce((acc, key) => {
       if (!(key in acc)) acc[key] = other[key];
@@ -43,7 +48,85 @@ function expand(target, other) {
     }, target);
 }
 
+/**
+ * Load moduleData for app dependencies
+ *
+ * @param {Loader} loader - Loader instance
+ * @param {ModuleData} app - Metadata for app
+ * @param {ModuleData[]} modules - Metadata for modules
+ */
+function loadAppModules(loader, app, modules) {
+  Object.keys(app.package.dependencies)
+    .filter(name => !isPlugin(name) && !isPreset(name))
+    .forEach(name => {
+      const range = app.package.dependencies[name];
+      const moduleData = loader.getModuleInfo(null, name, { from: app.name, range });
+      modules.push(moduleData);
+    });
+}
+
+/**
+ * Load moduleData for any supporting modules that are declared by plugin
+ *
+ * @param {PluginData} pluginData - Metadata for a plugin
+ * @param {Loader} loader - Loader instance
+ */
+function loadPluginModules(pluginData, loader) {
+  if (pluginData.modules) {
+    pluginData.modules = pluginData.modules
+      .map(mod => isObject(mod) ? mod : { name: mod })  // normalize string names to objects
+      .map(mod => loader.getModuleInfo(null, mod.name, mod));
+  }
+}
+
+/**
+ * Flatten moduleData from plugin to top-level modules metadata
+ *
+ * @param {PluginData} pluginData - Metadata for a plugin
+ * @param {ModuleData[]} modules - Metadata for modules
+ */
+function flattenPluginModules(pluginData, modules) {
+  if (pluginData.modules) {
+    pluginData.modules.forEach(moduleData => {
+      const existing = modules.find(mod => mod.name === moduleData.name);
+      if (existing) {
+        safeAssign(existing, moduleData);
+      } else {
+        modules.push(moduleData);
+      }
+    });
+  }
+}
+
+/**
+ * Assign modified plugin data instances back to preset hierarchy to avoid faulty data
+ *
+ * @param {PluginData} pluginData - Metadata for a plugin
+ * @param {ModuleData[]} presets - Metadata for presets
+ */
+function fixupPresetHierarchy(pluginData, presets) {
+
+  /**
+   * Recursing fixer-upper
+   *
+   * @param {PresetData} presetData - Preset to fixup
+   */
+  function fixup(presetData) {
+    if (presetData.name === pluginData.from) {
+      const idx = presetData.plugins.findIndex(p => p.name === pluginData.name);
+      presetData.plugins[idx] = pluginData;
+    }
+    presetData.presets && presetData.presets.forEach(fixup);
+  }
+
+  presets.forEach(fixup);
+}
+
 module.exports = {
   sanitize,
-  expand
+  safeAssign,
+  loadAppModules,
+  loadPluginModules,
+  flattenPluginModules,
+  fixupPresetHierarchy
 };
