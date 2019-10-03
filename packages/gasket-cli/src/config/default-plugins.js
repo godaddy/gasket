38b5dadc563b5a1d22ebefc156a84939b66fdbd3
Andrew Gerard <agerard@godaddy.com>
2019-10-03 08:00:40 -0700

    [fix] remove metrics from default plugins

--- packages/gasket-cli/src/config/default-plugins.js
+++ packages/gasket-cli/src/config/default-plugins.js
@@ -2,6 +2,5 @@ module.exports = [
   require('@gasket/command-plugin'),
   require('@gasket/lifecycle-plugin'),
   require('@gasket/metadata-plugin'),
-  require('@gasket/metrics-plugin'),
   require('@gasket/deploy-plugin')
 ];
