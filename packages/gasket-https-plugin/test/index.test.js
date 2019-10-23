d9a6fbc9bd5b0848cdc3d584cbdcc2d740917994
Arnout Kazemier <3rd-Eden@users.noreply.github.com>
2019-10-23 22:00:40 +0200

    [minor] Gracefull shutdown using Terminus (#90)
    * [minor] Adding terminus support to the http plugin
    
    * [test] Fix the incorrect mock
    
    * [test] Added terminus & lifecycle related test
    
    * [doc] Additional documentation about terminus

--- packages/gasket-https-plugin/test/index.test.js
+++ packages/gasket-https-plugin/test/index.test.js
@@ -6,16 +6,21 @@ const proxyquire = require('proxyquire').noCallThru();
 const sinon = require('sinon');
 const errs = require('errs');
 
-const createServersModule = sinon.stub().yields(null);
+const createServersModule = sinon.stub().yields(null, []);
+const HealthCheckError = sinon.spy();
+const createTerminus = sinon.spy();
 const debugStub = sinon.spy();
 
 const plugin = proxyquire('../', {
   'create-servers': createServersModule,
-  'diagnostics': sinon.stub().returns(debugStub)
+  'diagnostics': sinon.stub().returns(debugStub),
+  '@godaddy/terminus': {
+    createTerminus,
+    HealthCheckError
+  }
 });
 
 describe('Plugin', () => {
-
   it('is an object', () => {
     assume(plugin).is.an('object');
   });
@@ -38,8 +43,11 @@ describe('Plugin', () => {
 });
 
 describe('start hook', () => {
-  const start = plugin.hooks.start;
   let gasketAPI, handler;
+
+  async function start() {
+    return plugin.hooks.start(gasketAPI);
+  }
 
   beforeEach(() => {
     sinon.resetHistory();
@@ -53,7 +61,7 @@ describe('start hook', () => {
       }
     };
     handler = sinon.stub().yields();
-    createServersModule.yields(null);
+    createServersModule.yields(null, []);
 
     gasketAPI.exec.withArgs('createServers').resolves([handler]);
   });
@@ -65,7 +73,7 @@ describe('start hook', () => {
       https: { port: 3000 }
     };
 
-    await executeModule();
+    await start();
 
     const createServerOpts = createServersModule.lastCall.args[0];
     assume(createServerOpts).to.not.haveOwnProperty('http');
@@ -79,7 +87,7 @@ describe('start hook', () => {
       https: null
     };
 
-    await executeModule();
+    await start();
 
     const createServerOpts = createServersModule.lastCall.args[0];
     assume(createServerOpts).to.haveOwnProperty('http');
@@ -89,7 +97,7 @@ describe('start hook', () => {
   it('defaults HTTP server to port 80 if neither `http` or `https`', async () => {
     gasketAPI.config = {};
 
-    await executeModule();
+    await start();
 
     const createServerOpts = createServersModule.lastCall.args[0];
     assume(createServerOpts).property('http', 80);
@@ -107,7 +115,7 @@ describe('start hook', () => {
         http: 8080
       };
 
-      await executeModule();
+      await start();
 
       const logMessages = gasketAPI.logger.info.args.map(([message]) => message);
       assume(logMessages[0]).to.match(/http:\/\/local\.gasket\.godaddy\.com:8080\//);
@@ -119,7 +127,7 @@ describe('start hook', () => {
         https: { port: 8443 }
       };
 
-      await executeModule();
+      await start();
 
       const logMessages = gasketAPI.logger.info.args.map(([message]) => message);
       assume(logMessages[0]).to.match(/https:\/\/myapp\.godaddy\.com:8443\//);
@@ -131,7 +139,7 @@ describe('start hook', () => {
         https: { port: 3000 }
       };
 
-      await executeModule();
+      await start();
 
       const logMessages = gasketAPI.logger.info.args.map(([message]) => message);
       assume(logMessages[0]).to.match(/https:\/\/local\.gasket\.godaddy\.com:3000\//);
@@ -145,7 +153,7 @@ describe('start hook', () => {
 
       gasketAPI.execWaterfall.callsFake(() => Promise.resolve({ hostname: 'bogus.com', http: 9000 }));
 
-      await executeModule();
+      await start();
 
       const logMessages = gasketAPI.logger.info.args.map(([message]) => message);
       assume(logMessages[0]).to.match(/http:\/\/bogus\.com:9000\//);
@@ -155,7 +163,7 @@ describe('start hook', () => {
   it('rejects with an Error on failure', async () => {
     createServersModule.yields(errs.create({ https: { message: 'HTTP server failed to start', errno: 'something' } }));
 
-    await executeModule();
+    await start();
 
     const expected = 'failed to start the http/https servers';
     assume(gasketAPI.logger.error).calledWith(expected);
@@ -164,14 +172,13 @@ describe('start hook', () => {
   });
 
   it('rejects with an Error about ports on failure (with http)', async () => {
-
     createServersModule.yields(errs.create({
       http: {
         errno: 'EADDRINUSE'
       }
     }));
 
-    await executeModule();
+    await start();
 
     const expected = 'Port is already in use';
     assume(gasketAPI.logger.error).calledWithMatch(expected);
@@ -180,20 +187,64 @@ describe('start hook', () => {
   });
 
   it('rejects with an Error about ports on failure (with https)', async () => {
-
     createServersModule.yields(errs.create({
       https: {
         errno: 'EADDRINUSE'
       }
     }));
 
-    await executeModule();
+    await start();
 
     assume(debugStub.args[0][0].message).to.match('Port is already in use');
     assume(debugStub.args[0][1].https.errno).equals('EADDRINUSE');
   });
 
-  async function executeModule() {
-    return start(gasketAPI);
-  }
+  describe('terminus', function () {
+    const server = {};
+
+    beforeEach(function () {
+      createServersModule.yields(null, [server]);
+    });
+
+    it('passes each created server to terminus', async () => {
+      await start();
+
+      assume(createTerminus.args[0][0]).equals(server);
+    });
+
+    it('calls terminus with the options', async () => {
+      await start();
+
+      const config = createTerminus.args[0][1];
+
+      assume(config.signals).includes('SIGTERM');
+      assume(config.logger).is.a('function');
+      assume(config.onSignal).is.a('asyncfunction');
+      assume(config.onSendFailureDuringShutdown).is.a('asyncfunction');
+      assume(config.beforeShutdown).is.a('asyncfunction');
+      assume(config.onShutdown).is.a('asyncfunction');
+
+      assume(config.healthChecks).has.property('/healthcheck');
+      assume(config.healthChecks['/healthcheck']).is.a('asyncfunction');
+    });
+
+    ['onSendFailureDuringShutdown', 'beforeShutdown', 'onSignal', 'onShutdown'].forEach((type) => {
+      it(`calls the ${type} lifecycle`, async () => {
+        await start();
+        const lifecycle = createTerminus.args[0][1][type];
+        await lifecycle();
+
+        assume(gasketAPI.exec.args[gasketAPI.exec.args.length - 1][0]).equals(type);
+      });
+    });
+
+    it('calls the healthcheck lifecycle', async () => {
+      await start();
+      const lifecycle = createTerminus.args[0][1].healthChecks['/healthcheck']
+      await lifecycle();
+
+      assume(gasketAPI.exec.args[gasketAPI.exec.args.length - 1][0]).equals('healthcheck');
+      assume(gasketAPI.exec.args[gasketAPI.exec.args.length - 1][1]).equals(HealthCheckError);
+    });
+  });
 });
