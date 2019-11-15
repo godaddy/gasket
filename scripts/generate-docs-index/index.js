94c12ecef58440e21e7783e3e8cf67c8601a4484
Andrew Gerard <agerard@godaddy.com>
2019-11-14 22:11:41 -0700

    [tiny] cleanup

--- scripts/generate-docs-index/index.js
+++ scripts/generate-docs-index/index.js
@@ -1,27 +1,25 @@
 const { runShellCommand } = require('../../packages/gasket-utils');
 const fs = require('fs');
 const path = require('path');
-const {
-  promisify
-} = require('util');
+const { promisify } = require('util');
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
-const startTag = '<!-- GENERATED -->';
+const startTag = '<!-- START GENERATED -->';
 const endTag = '<!-- END GENERATED -->';
-
 
 const projectRoot = path.resolve(__dirname, '..', '..');
 const cliBin = path.join(projectRoot, 'packages', 'gasket-cli', 'bin', 'run');
 const sourcePath = path.join(__dirname, '.docs', 'README.md');
 const targetPath = path.join(projectRoot, 'docs', 'README.md');
 
-async function index() {
+async function main() {
   await runShellCommand(cliBin, ['docs', '--no-view'], { cwd: __dirname });
 
   let content = await readFile(sourcePath, 'utf-8');
 
+  // fix up the generated docs index
   content = content
     .replace(/# App[^#]+/gm, '')
     .replace(/:.+\/@gasket\//g, ':/packages/gasket-')
@@ -34,10 +32,12 @@ async function index() {
   const template = await readFile(targetPath, 'utf-8');
   const start = template.indexOf(startTag) + startTag.length;
   const end = template.indexOf(endTag);
+
+  // substitute in the generated content
   content = template.substring(0, start) + '\n\n' + content + template.substring(end);
 
   await writeFile(targetPath, content, 'utf-8');
   console.log('DONE');
 }
 
-index();
+main();
