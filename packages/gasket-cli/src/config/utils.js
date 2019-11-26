e392926e3773fbe6381e55b962c8f0c6331358ee
wjarvis1 <51758262+wjarvis1@users.noreply.github.com>
2019-11-26 13:48:55 -0700

    fail on missing modules imported to gasket.config (#115)
    * [test] fail on missing modules imported ot gasket.config
    
    * [tiny] added CHANGELOG

--- packages/gasket-cli/src/config/utils.js
+++ packages/gasket-cli/src/config/utils.js
@@ -3,7 +3,6 @@ const path = require('path');
 const fs = require('fs');
 const defaultsDeep = require('lodash.defaultsdeep');
 const { pluginIdentifier } = require('@gasket/resolve');
-const { tryRequire } = require('@gasket/utils');
 const defaultPlugins = require('./default-plugins');
 const { flattenPresets } = require('../scaffold/utils');
 
@@ -34,8 +33,15 @@ async function getGasketConfig(flags) {
  */
 function loadConfigFile(flags) {
   const { root, config } = flags;
-  const configFile = !path.isAbsolute(config) ? path.join(root, config) : config;
-  return tryRequire(configFile);
+  const configFilePath = !path.isAbsolute(config) ? path.join(root, config) : config;
+  const configFileName = configFilePath.endsWith('.js') ? configFilePath : configFilePath + '.js';
+  // require the file if config file exist, else return null
+  try {
+    fs.statSync(configFileName); // eslint-disable-line no-sync
+  } catch (err) {
+    return null;
+  }
+  return require(configFilePath);
 }
 
 /**
