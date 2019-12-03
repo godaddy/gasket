2a86403f11c6115fa924e072aa75fe300542834c
Andrew Gerard <agerard@godaddy.com>
2019-12-02 22:16:37 -0700

    [tiny] use new emblem

--- packages/gasket-plugin-nextjs/generator/app/pages/index.js
+++ packages/gasket-plugin-nextjs/generator/app/pages/index.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import Head from '../components/head';
-import GasketLogo from '@gasket/assets/react/gasket-logo';
+import GasketEmblem from '@gasket/assets/react/gasket-emblem';
 
 const pageStyle = { textAlign: 'center' };
-const logoStyle = { width: '250px' };
+const logoStyle = { width: '250px', height: '250px' };
 
 export const IndexPage = () => (
   <div style={ pageStyle }>
     <Head title='Home'/>
-    <GasketLogo style={ logoStyle }/>
+    <GasketEmblem style={ logoStyle }/>
     <h1>Welcome to Gasket!</h1>
     <p>To get started, edit <code>pages/index.js</code> and save to reload.</p>
     <p><a href='https://gasket.dev'>Learn Gasket</a></p>
