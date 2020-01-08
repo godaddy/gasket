ccb8febc22efc5fa218e677a84b2ddaf4a3f5a7e
Sivan Mehta <smehta@godaddy.com>
2020-01-08 12:59:12 -0800

    [wip] adjust tests

--- packages/gasket-plugin-docs-graph/test/plugin.test.js
+++ packages/gasket-plugin-docs-graph/test/plugin.test.js
@@ -1,8 +1,17 @@
+const path = require('path');
+const read = require('util').promisify(require('fs').readFile);
 const plugin = require('../');
 const hook = plugin.hooks.docsGenerate;
 const assume = require('assume');
 
 describe('docs graph plugin', function () {
+  let docsConfigSet;
+  beforeEach(function () {
+    docsConfigSet = {
+      docsRoot: path.join(__dirname, 'fixtures'),
+      lifecycles: []
+    };
+  })
   it('is named properly', function () {
     assume(plugin.name).equals('docs-graph');
   });
@@ -11,47 +20,54 @@ describe('docs graph plugin', function () {
     assume(hook).is.an('asyncfunction');
   });
 
-  it.skip('provides names the graph properly', function () {
-    const { name } = hook({}, { lifecycles: [] });
-    assume(name).equals('Lifecycles');
+  it('provides proper metadata', async function () {
+    const data = await hook({}, docsConfigSet);
+    assume(data.name).equals('Lifecycle Flowchart');
+    assume(data.description).equals('A flowchart detailing how lifecyles are interrelated.');
+    assume(data.link).equals('/mermaid.md');
+    assume(data.targetRoot).equals(path.join(__dirname, 'fixtures', 'generated-docs'));
   });
 
-  it.skip('provides a mermaid markdown block', function () {
-    const { content } = hook({}, { lifecycles: [] });
+  it('provides a mermaid markdown block', async function () {
+    const { targetRoot, link } = await hook({}, docsConfigSet);
+    const content = await read(path.join(targetRoot, link));
     assume(content).startWith('```mermaid\n');
     assume(content).endsWith('\n```');
   });
 
-  it.skip('provides a left to right mermaid graph', function () {
-    const { content } = hook({}, { lifecycles: [] });
+  it('provides a left to right mermaid graph', async function () {
+    const { targetRoot, link } = await hook({}, docsConfigSet);
+    const content = await read(path.join(targetRoot, link));
     assume(content).matches(/graph LR;/);
   });
 
-  it.skip('uses the correct arrows depending on lifecycle method', function () {
-    const { content } = hook({}, {
-      lifecycles: [{
-        parent: 'snap',
-        name: 'crackle'
-      }, {
-        parent: 'crackle',
-        name: 'pop',
-        method: 'exec'
-      }]
-    });
+  it('uses the correct arrows depending on lifecycle method', async function () {
+    docsConfigSet.lifecycles = [{
+      parent: 'snap',
+      name: 'crackle'
+    }, {
+      parent: 'crackle',
+      name: 'pop',
+      method: 'exec'
+    }];
 
-    assume(content).contains('snap --> crackle;');
-    assume(content).contains('crackle -- exec --> pop;');
+    const { targetRoot, link } = await hook({}, docsConfigSet);
+    const content = await read(path.join(targetRoot, link));
+
+    assume(content).matches('snap --> crackle;');
+    assume(content).matches('crackle -- exec --> pop;');
   });
 
-  it.skip('generates the LHS of the arrows from the correct attribute', function () {
-    const { content } = hook({}, {
-      lifecycles: [{
-        name: 'Anakin',
-        parent: 'Shmi',
-        after: '',
-        command: 'bring balance to the Force',
-        from: 'Tatooine'
-      }]
-    });
+  it.skip('generates the LHS of the arrows from the correct attribute', async function () {
+    docsConfigSet.lifecycles = [{
+      name: 'Anakin',
+      parent: 'Shmi',
+      after: '',
+      command: 'bring balance to the Force',
+      from: 'Tatooine'
+    }]
+
+    const { targetRoot, link } = await hook({}, docsConfigSet);
+    const content = await read(path.join(targetRoot, link));
   });
 });
