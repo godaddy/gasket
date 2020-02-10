830dc18f63b7c829b9029100360c10565715d66b
Andrew Gerard <agerard@godaddy.com>
2020-02-10 09:18:33 -0700

    Fix missing await (#144)
    * [fix] missing await
    
    * [test] saves locale manifest
    
    * [doc] changelog
    
    * [test] write reject

--- packages/gasket-plugin-intl/lib/builder.spec.js
+++ packages/gasket-plugin-intl/lib/builder.spec.js
@@ -203,12 +203,24 @@ describe('Builder', function () {
         fs.mkdirp = jest.fn().mockReturnValue(Promise.resolve());
         fs.readdir = jest.fn().mockResolvedValue(['test/folder/name.json']);
         fs.readJson = jest.fn().mockResolvedValue({ name: 'bogus-package' });
+        fsUtils.saveJsonFile = jest.fn().mockResolvedValue();
         builder.processFiles = jest.fn();
       });
 
       it('calls processFiles for each directory', async function () {
         await builder.processDirs(buildDirs);
         expect(builder.processFiles.mock.calls).toHaveLength(2);
+      });
+
+      it('saves locales manifest file', async function () {
+        await builder.processDirs(buildDirs);
+        expect(fsUtils.saveJsonFile).toHaveBeenCalledWith(expect.stringContaining('locales-manifest.json'), expect.any(Object));
+      });
+
+      it('throws if failure to write locales manifest file', function () {
+        const mockError = new Error('Bad things man');
+        fsUtils.saveJsonFile = jest.fn().mockRejectedValue(mockError);
+        return expect(builder.processDirs(buildDirs)).rejects.toBe(mockError);
       });
     });
 
