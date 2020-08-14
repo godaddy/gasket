2a7a6561b6ce37d143a210ea250ae4aae0d2baba
Maurya Ramadurgam <mramad2@gmail.com>
2020-08-14 13:51:59 -0700

    first draft of starter pack (#167)
    * first draft of starter pack
    
    * no unit tests yet. return 0
    
    * Update packages/create-gasket-app/README.md
    
    Co-authored-by: Andrew Gerard <kinetifex@gmail.com>
    
    * Update packages/create-gasket-app/package.json
    
    Co-authored-by: Andrew Gerard <kinetifex@gmail.com>
    
    * Update license
    
    * result from running align-packages
    
    * include path module
    
    Co-authored-by: Maurya Ramadurgam <mramadurgam@godaddy.com>
    Co-authored-by: Andrew Gerard <kinetifex@gmail.com>

---
+++ packages/create-gasket-app/index.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const { fork } = require('child_process');
+const path = require('path');
+
+const [,, ...args] = process.argv;
+
+function main() {
+  return fork(path.join(__dirname, 'node_modules', '.bin', 'gasket'), [...['create'], args], {stdio: 'inherit', stdin: 'inherit', stderr: 'inherit'});
+}
+main();
