2d0afdb6049ccb5b08b558099c56b0b4a79f3988
Andrew Gerard <kinetifex@gmail.com>
2020-08-20 16:40:31 -0700

    [fix] linting

--- packages/create-gasket-app/index.js
+++ packages/create-gasket-app/index.js
@@ -3,9 +3,14 @@
 const { fork } = require('child_process');
 const path = require('path');
 
-const [,, ...args] = process.argv;
+const [, , ...args] = process.argv;
 
 function main() {
-  return fork(path.join(__dirname, 'node_modules', '.bin', 'gasket'), ['create', ...args], { stdio: 'inherit', stdin: 'inherit', stderr: 'inherit' });
+  return fork(
+    path.join(__dirname, 'node_modules', '.bin', 'gasket'),
+    ['create', ...args],
+    { stdio: 'inherit', stdin: 'inherit', stderr: 'inherit' }
+  );
 }
+
 main();
