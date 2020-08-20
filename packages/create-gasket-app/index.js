4e91e9077d604dad4c81a79a0edeeb2319e06f69
Andrew Gerard <kinetifex@gmail.com>
2020-08-20 16:38:13 -0700

    [fix] additional args spread

--- packages/create-gasket-app/index.js
+++ packages/create-gasket-app/index.js
@@ -6,6 +6,6 @@ const path = require('path');
 const [,, ...args] = process.argv;
 
 function main() {
-  return fork(path.join(__dirname, 'node_modules', '.bin', 'gasket'), [...['create'], args], {stdio: 'inherit', stdin: 'inherit', stderr: 'inherit'});
+  return fork(path.join(__dirname, 'node_modules', '.bin', 'gasket'), ['create', ...args], { stdio: 'inherit', stdin: 'inherit', stderr: 'inherit' });
 }
 main();
