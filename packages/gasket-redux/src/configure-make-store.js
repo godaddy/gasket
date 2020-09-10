bb65a7c71e1bee1b97d35c4f4bdff7dd53ff5d74
Andrew Gerard <kinetifex@gmail.com>
2020-09-10 16:04:54 -0700

    [minor] add support for setting a root reducer

--- packages/gasket-redux/src/configure-make-store.js
+++ packages/gasket-redux/src/configure-make-store.js
@@ -5,20 +5,52 @@ import Log from '@gasket/log';
 import placeholderReducers from './placeholder-reducers';
 
 /**
+ * Compose the reducer
+ *
+ * @param {object} allReducers - Map of identifiers and reducers
+ * @param {function} [rootReducer] - Entry reducer to run before combined reducers
+ * @returns {function} reducer
+ * @private
+ */
+export function prepareReducer(allReducers, rootReducer) {
+  const combinedReducer = Object.keys(allReducers).length ? combineReducers(allReducers) : (f = {}) => f;
+
+  if (rootReducer) {
+    return (state, action) => {
+      const nextState = rootReducer(state, action);
+      // if state results are unchanged, continue to combined reducers
+      if (nextState === state) {
+        return combinedReducer(state, action);
+      }
+      return nextState;
+    };
+  }
+
+  return combinedReducer;
+}
+/**
  * Set up redux store configuration and return a makeStore function
  *
  * @param {Object} options - Options for create store
  * @param {Object.<String,Function>} options.reducers - Map of identifiers and reducers
+ * @param {function} [options.rootReducer] - Entry reducer to run before combined reducers
  * @param {Object} [options.initialState] - Initial redux state
  * @param {Array} [options.middleware] - Middleware
  * @param {Function[]} [options.enhancers] - Any additional enhancers
  * @param {Boolean} [options.logging] - logging is enabled by default. Passing false will disable logging completely.
  * @param {Function} [options.thunkMiddleware] - Optionally provide an extra argument for thunks
  * @param {Function} [postCreate] - Optional callback
- * @returns {function(*=): Store<{}>} makeStore
+ * @returns {MakeStore} makeStore
  */
 export default function configureMakeStore(
-  { reducers = {}, initialState = {}, middleware = [], enhancers = [f => f], logging = false, thunkMiddleware = thunk } = {},
+  {
+    reducers = {},
+    rootReducer,
+    initialState = {},
+    middleware = [],
+    enhancers = [f => f],
+    logging = false, thunkMiddleware = thunk
+  } = {},
   postCreate
 ) {
   const baseMiddleware = [thunkMiddleware];
@@ -26,15 +58,14 @@ export default function configureMakeStore(
   /**
    * Wrapper for store create to create instance with SSR and to hydrate in browser.
    *
-   * Conforms to API required by next-redux-wrapper
-   * https://github.com/kirill-konshin/next-redux-wrapper
+   * @typedef {Function} MakeStore
    *
    * @param {Object} state - The initial redux state
    * @param {Object} options - Options
    * @param {Request} options.req - Request if SSR
    * @returns {Store} reduxStore
    */
-  return function makeStore(state = {}, options = {}) {
+  function makeStore(state = {}, options = {}) {
     const { req, logger = new Log() } = options;
 
     //
@@ -63,11 +94,13 @@ export default function configureMakeStore(
 
     const preloadedState = { ...initialState, ...state };
     const allReducers = { ...reducers, ...placeholderReducers(reducers, preloadedState) };
-    const reducer = Object.keys(allReducers).length ? combineReducers(allReducers) : f => f || {};
+    const reducer = prepareReducer(allReducers, rootReducer);
     const store = createStore(reducer, { ...initialState, ...state }, enhancer);
 
     if (postCreate) postCreate(store);
 
     return store;
-  };
+  }
+
+  return makeStore;
 }
