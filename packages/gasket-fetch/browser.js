2e4798df462d5d4bff5674f1ab80b5f698ff4d55
mramadurgam <mramadurgam@godaddy.com>
2020-09-13 17:08:58 -0700

    feat: use polyfilled window.fetch on browser

---
+++ packages/gasket-fetch/browser.js
@@ -0,0 +1,9 @@
+
+exports = window.fetch; // To import fetch from @gasket/fetch
+exports.default = window.fetch; // For TypeScript consumers without esModuleInterop.
+exports.fetch = window.fetch; // To import {fetch} from @gasket/fetch
+exports.Headers = window.Headers;
+exports.Request = window.Request;
+exports.Response = window.Response;
+
+module.exports = exports;
