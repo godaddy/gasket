534056934794f7e8e29ac95eed8600dfa1fb7df3
mramadurgam <mramadurgam@godaddy.com>
2020-09-26 18:17:38 -0700

    refactor: remove un-needed react-native entry

--- packages/gasket-fetch/react-native.js
+++
@@ -1,9 +0,0 @@
-
-exports = global.fetch; // To import fetch from @gasket/fetch
-exports.default = global.fetch; // For TypeScript consumers without esModuleInterop.
-exports.fetch = global.fetch; // To import {fetch} from @gasket/fetch
-exports.Headers = global.Headers;
-exports.Request = global.Request;
-exports.Response = global.Response;
-
-module.exports = exports;
