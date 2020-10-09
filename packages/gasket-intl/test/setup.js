620cd629635bcc78ec9644c4ffe0c802f0710349
Andrew Gerard <kinetifex@gmail.com>
2020-10-09 10:43:02 -0700

    [fix] cleanup depends

--- packages/gasket-intl/test/setup.js
+++ packages/gasket-intl/test/setup.js
@@ -1,7 +1,7 @@
-/* eslint-disable no-process-env, no-console */
+/* eslint-disable no-process-env */
 const path = require('path');
+
 process.env.GASKET_INTL_MANIFEST_FILE = path.resolve(__dirname, 'fixtures/mock-manifest.json');
 process.env.GASKET_INTL_LOCALES_DIR = path.resolve(__dirname, 'fixtures/locales');
 
-console.log('process.env.GASKET_INTL_LOCALES_DIR', process.env.GASKET_INTL_LOCALES_DIR);
-console.log('process.env.GASKET_INTL_MANIFEST_FILE', process.env.GASKET_INTL_MANIFEST_FILE);
+global.Intl = require('intl');
--- packages/gasket-plugin-intl/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "extends": ["godaddy", "plugin:jest/recommended"],
-  "rules": {
-    "jest/valid-expect": 0
-  }
-};
