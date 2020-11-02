828935491427ad2011e48d666b6ddd348add4f17
Kawika Bader <ekbader@gmail.com>
2020-11-02 11:09:30 -0700

    Update packages/gasket-plugin-swagger/index.js
    Co-authored-by: Andrew Gerard <kinetifex@gmail.com>

--- packages/gasket-plugin-swagger/index.js
+++ packages/gasket-plugin-swagger/index.js
@@ -77,7 +77,7 @@ module.exports = {
 
         if (!swaggerSpec) {
           gasket.logger.warning(
-            'Missing swagger config in gasket.config.js: swagger.json was not generated...'
+            `No JSDocs for Swagger were found in files (${jsdocs.apis}). Definition file not generated...`
           );
         } else {
           let content;
