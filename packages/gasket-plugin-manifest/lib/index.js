51c7308e133d4da305c0adff80ec281804e07b92
Kawika Bader <ekbader@gmail.com>
2020-12-02 10:02:34 -0700

    Updated index and related tests.

--- packages/gasket-plugin-manifest/lib/index.js
+++ packages/gasket-plugin-manifest/lib/index.js
@@ -1,91 +1,13 @@
-/* eslint require-atomic-updates: warn */
-const fs = require('fs');
-const { promisify } = require('util');
-const path = require('path');
-const mkdirp = promisify(require('mkdirp'));
-const baseConfig = require('./base-config');
-const escapeRegex = require('escape-string-regexp');
-
-const writeFile = promisify(fs.writeFile);
-
-/**
- * Merges manifest defaults with gasket.config manifest and passes to
- * every defined `manifest` hook for further manipulation.
- *
- * @param  {Gasket} gasket The gasket API
- * @param {Request} req incoming HTTP request
- * @returns {Object} manifest.json
- */
-async function gatherManifestData(gasket, req) {
-  gasket.logger.debug(`Gathering manifest for ${req.originalUrl}`);
-
-  const fromConfig = gasket.config.manifest;
-  const manifest = { ...baseConfig, ...fromConfig };
-
-  if (!fromConfig.staticOutput) {
-    return await gasket.execWaterfall('manifest', manifest, { req }) || [];
-  }
-
-  return manifest;
-}
+const build = require('./build');
+const express = require('./express');
+const middleware = require('./middleware');
 
 module.exports = {
   name: require('../package').name,
   hooks: {
-    build: async function build(gasket) {
-      if (gasket.config.manifest && gasket.config.manifest.staticOutput) {
-        const manifest = await gatherManifestData(gasket, { originalUrl: 'static page.' });
-        const pubRoot = path.join(gasket.config.root, '/public');
-        // eslint-disable-next-line no-sync
-        if (!fs.existsSync(pubRoot)) {
-          await mkdirp(pubRoot);
-        }
-
-        try {
-          await writeFile(path.join(pubRoot, gasket.config.manifest.staticOutput), JSON.stringify(manifest), 'utf-8');
-        } catch (err) {
-          gasket.logger.error('Failed to write manifest.json: ', err);
-        }
-      }
-    },
-    /**
-     * If configured, serve the resolved manifest.json
-     * @param  {Gasket} gasket The gasket API
-     * @param  {Express} app gasket's express server
-     * @async
-     */
-    async express(gasket, app) {
-      const { config } = gasket;
-      const { path } = (config && config.manifest || {});
-      app.get(path || baseConfig.path, (req, res) => {
-        res.send(req.manifest || {});
-      });
-    },
-    middleware: {
-      timing: {
-        last: true
-      },
-      /**
-       * Add some middleware to gather manifest details for certain endpoints
-       *
-       * @param {Gasket} gasket - The gasket API
-       * @returns {function} Express middleware to apply
-       */
-      handler: function (gasket) {
-        const { serviceWorker: { url: swUrl = '' } = {} } = gasket.config;
-
-        const endpoints = [/manifest\.json/];
-        if (swUrl) endpoints.push(new RegExp(escapeRegex(swUrl)));
-
-        return async function manifestMiddleware(req, res, next) {
-          if (endpoints.some(p => req.path.match(p))) {
-            req.manifest = await gatherManifestData(gasket, req);
-          }
-
-          next();
-        };
-      }
-    },
+    build,
+    express,
+    middleware,
     metadata(gasket, meta) {
       return {
         ...meta,
--- packages/gasket-plugin-manifest/test/index.test.js
+++ packages/gasket-plugin-manifest/test/index.test.js
@@ -1,19 +1,17 @@
-const plugin = require('../');
+const plugin = require('../lib');
 const assume = require('assume');
-const sinon = require('sinon');
-const proxyquire = require('proxyquire');
 
 describe('Plugin', function () {
 
-  it('is an object', () => {
+  it('is an object', function () {
     assume(plugin).is.an('object');
   });
 
-  it('has expected name', () => {
+  it('has expected name', function () {
     assume(plugin).to.have.property('name', require('../package').name);
   });
 
-  it('has expected hooks', () => {
+  it('has expected hooks', function () {
     const expected = [
       'build',
       'express',
@@ -27,213 +25,11 @@ describe('Plugin', function () {
     assume(hooks).eqls(expected);
     assume(hooks).is.length(expected.length);
   });
-});
 
-describe('#hooks.build', function () {
-  const writeFileStub = sinon.stub();
-  const mkdirpStub = sinon.stub();
-  const existsSyncStub = sinon.stub();
-
-  const buildManifest = proxyquire('../lib/index', {
-    fs: {
-      writeFile: writeFileStub,
-      existsSync: existsSyncStub
-    },
-    mkdirp: mkdirpStub,
-    util: {
-      promisify: f => f
-    }
-  });
-
-  const { build } = buildManifest.hooks;
-  let gasket;
-
-  beforeEach(() => {
-    gasket = {
-      execWaterfall: sinon.stub().resolves([]),
-      config: {
-        root: 'test',
-        manifest: {
-          staticOutput: '/manifest.json'
-        }
-      },
-      logger: {
-        debug: sinon.stub(),
-        error: sinon.stub()
-      }
-    };
-  });
-
-  afterEach(function () {
-    sinon.reset();
-  });
-
-  it('is a function', function () {
-    assume(build).is.a('asyncfunction');
-    assume(build).has.length(1);
-  });
-
-  it('does nothing when staticOutput config is not set', async function () {
-    gasket.config.manifest = {};
-    await build(gasket);
-
-    assume(writeFileStub.called).false();
-  });
-
-  it('gathers manifest data', async function () {
-    await build(gasket);
-    assume(writeFileStub.called).true();
-    assume(mkdirpStub.called).true();
-  });
-
-  it('logs error when file cannot be written', async function () {
-    writeFileStub.throws();
-    await build(gasket);
-    assume(gasket.logger.error.calledOnce).true();
-  });
-});
-
-describe('#hooks.middleware', function () {
-  const { middleware } = plugin.hooks;
-  const { timing, handler } = middleware;
-  let gasket;
-
-  beforeEach(() => {
-    gasket = {
-      execWaterfall: sinon.stub().resolves([]),
-      config: {
-        manifest: {
-          name: 'Walter White',
-          superpower: 'Chemistry'
-        },
-        serviceWorker: {
-          url: 'sw.js'
-        }
-      },
-      logger: {
-        debug: sinon.stub()
-      }
-    };
-  });
-
-  describe('#timing', () => {
-
-    it('is set to last', function () {
-      assume(timing).property('last', true);
+  describe('meta', function () {
+    it('returns lifecycle metadata', function () {
+      const results = plugin.hooks.metadata({}, {});
+      assume(results.lifecycles).is.an('array');
     });
-  });
-
-  describe('#handler', () => {
-
-    it('is a function', function () {
-      assume(handler).is.a('function');
-      assume(handler).has.length(1);
-    });
-
-    it('returns middleware', function () {
-      const fn = handler(gasket, {});
-      assume(fn).is.an('asyncfunction');
-      assume(fn).has.length(3);
-    });
-
-    it('gathers manifest data if looking for manifest.json', async function () {
-      const fn = handler(gasket, {});
-      const req = {
-        path: 'manifest.json'
-      };
-      await fn(req, {}, () => { });
-      assume(gasket.execWaterfall.calledOnce).is.true();
-    });
-
-    it('gathers manifest data if looking for the service worker script', async function () {
-      const fn = handler(gasket, {});
-      const req = {
-        path: 'sw.js'
-      };
-      await fn(req, {}, () => { });
-      assume(gasket.execWaterfall.calledOnce).is.true();
-    });
-
-    it('passes the incoming request to the manifest hook', async function () {
-      const fn = handler(gasket, {}, {});
-      const req = {
-        path: 'manifest.json'
-      };
-      await fn(req, {}, () => { });
-      assume(gasket.execWaterfall.args[0]).has.length(3);
-      assume(gasket.execWaterfall.args[0][2]).deep.equals({ req });
-    });
-
-    it('uses the default configuration', async function () {
-      const fn = handler(gasket, {}, {});
-      const req = {
-        path: 'manifest.json'
-      };
-      await fn(req, {}, () => { });
-      assume(gasket.execWaterfall.args[0][1].display).equals('standalone');
-    });
-
-    it('takes precedence from gasket config over base config', async function () {
-      gasket.config.manifest.display = 'BOGUS';
-      const fn = handler(gasket, {}, {});
-      const req = {
-        path: 'manifest.json'
-      };
-      await fn(req, {}, () => { });
-      assume(gasket.execWaterfall.args[0][1].display).equals('BOGUS');
-    });
-  });
-});
-
-describe('#hooks.express', function () {
-  const { express } = plugin.hooks;
-
-  it('is a function', function () {
-    assume(express).is.a('asyncfunction');
-    assume(express).has.length(2);
-  });
-
-  it('adds a route to the express server', async function () {
-    const app = {
-      get: sinon.stub()
-    };
-
-    await express({}, app);
-    assume(app.get.calledOnce).is.true();
-  });
-
-  it('returns the configured manifest on the route /manifest.json when path option is not set', async function (done) {
-    const stub = sinon.stub();
-    const get = (route, f) => {
-      assume(route).equals('/manifest.json');
-      f({}, { send: stub });
-      assume(stub.calledOnce);
-      done();
-    };
-
-    const app = { get };
-    await express({}, app);
-  });
-
-  it('returns the configured manifest on the configured path if option is set', async function (done) {
-    const customPath = '/some/custom/path';
-    const gasket = {
-      config: {
-        manifest: {
-          path: customPath
-        }
-      }
-    };
-
-    const stub = sinon.stub();
-    const get = (route, f) => {
-      assume(route).equals(customPath);
-      f({}, { send: stub });
-      assume(stub.calledOnce);
-      done();
-    };
-
-    const app = { get };
-    await express(gasket, app);
   });
 });
