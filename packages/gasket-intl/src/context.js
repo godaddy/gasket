e70bfb7913ab0b47983e15b0d3c7b08997350af4
Andrew Gerard <kinetifex@gmail.com>
2020-12-08 16:51:47 -0700

    [major] centralize server data loading to helper pkg

--- packages/gasket-intl/src/context.js
+++ packages/gasket-intl/src/context.js
@@ -1,21 +1,5 @@
 import React from 'react';
-
-/**
- * State of loaded locale files
- *
- * @typedef {object} LocalesState
- * @property {{string: string}} messages
- * @property {{LocalePath: LocalePathStatus}} status
- */
-
 import { manifest } from './config';
-
-/**
- * Props for a Next.js page containing locale and initial state
- *
- * @typedef {LocalesState} LocalesProps
- * @property {Locale} locale
- */
 
 export const GasketIntlContext = React.createContext({
   locale: manifest.defaultLocale,
