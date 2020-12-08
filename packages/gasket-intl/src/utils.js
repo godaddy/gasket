a66ed41ef08cb736b2053a65b2601cbf88bc04d8
Andrew Gerard <kinetifex@gmail.com>
2020-12-08 16:52:13 -0700

    [major] centralize server data loading to helper pkg

--- packages/gasket-intl/src/utils.js
+++ packages/gasket-intl/src/utils.js
@@ -3,18 +3,7 @@ import { LocaleUtils } from '@gasket/helper-intl';
 
 export const localeUtils = new LocaleUtils({ manifest, basePath });
 
-/**
- * Fetch status of a locale file
- * @typedef {string} LocalePathStatus
- * @readonly
- */
-
-/** @type {LocalePathStatus} */
-export const LOADING = 'loading';
-/** @type {LocalePathStatus} */
-export const LOADED = 'loaded';
-/** @type {LocalePathStatus} */
-export const ERROR = 'error';
+export { LOADED, LOADING, ERROR } from '@gasket/helper-intl';
 
 /**
  * Determines the active locale from either what was rendered for the page (preferred),
