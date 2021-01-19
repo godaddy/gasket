5f919730f0a2ada633478246f4496c491f4fce49
Andrew Gerard <kinetifex@gmail.com>
2021-01-19 11:17:45 -0700

    [doc] regen with repo guides

--- scripts/generate-docs-index/plugins/config-plugin.js
+++ scripts/generate-docs-index/plugins/config-plugin.js
@@ -23,6 +23,27 @@ async function metadata(gasket, data) {
 module.exports = {
   hooks: {
     metadata,
-    docsSetup: () => ({})
+    docsSetup: () => ({}),
+    // Add repo-level docs in to the top of the guides section
+    docsGenerate: {
+      timing: {
+        first: true
+      },
+      handler: async function docsGenerate(gasket, docsConfigSet) {
+        return [
+          {
+            name: 'Quick Start Guide',
+            description: 'Get up and running on Gasket',
+            link: '/docs/quick-start.md',
+            targetRoot: docsConfigSet.docsRoot
+          },
+          {
+            name: 'Upgrades Guide',
+            description: 'Steps necessary to upgrade major versions',
+            link: '/docs/upgrades.md',
+            targetRoot: docsConfigSet.docsRoot
+          }];
+      }
+    }
   }
 };
