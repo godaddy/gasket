0afbe3527145927f355d21f64b9480cd4d5584c5
Kawika Bader <ekbader@gmail.com>
2021-02-04 15:26:56 -0700

    Deciding to use empty object solution instead.

--- packages/gasket-react-intl/src/config.js
+++ packages/gasket-react-intl/src/config.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-process-env */
-const path = require('path');
-
 /**
  * @type {LocaleManifest}
  */
-const manifest = process.env.TEST_LOCALE_PATH ?
-  path.join(process.cwd(), process.env.TEST_LOCALE_PATH)
-  : require(process.env.GASKET_INTL_MANIFEST_FILE);
-
+const manifest = process.env.GASKET_INTL_MANIFEST_FILE ?
+  require(process.env.GASKET_INTL_MANIFEST_FILE) :
+  {};
 const isBrowser = typeof window !== 'undefined';
 const clientData = isBrowser && require('@gasket/data')?.intl || {};
 
