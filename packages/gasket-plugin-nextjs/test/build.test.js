390828fb9be50a97f3e95321a4c35bfdbe9324df
Kawika Bader <ekbader@gmail.com>
2021-05-04 14:31:34 -0700

    Updated functions.

--- packages/gasket-plugin-nextjs/test/build.test.js
+++ packages/gasket-plugin-nextjs/test/build.test.js
@@ -7,7 +7,7 @@ const { stub } = sinon;
 describe('build', function () {
   let createConfigStub, builderStub, execStub, loggerStub;
 
-  const getMockedBuildHook = (imports = {}) => {
+  const getMockedBuildHook = function (imports = {}) {
     createConfigStub = stub();
     builderStub = stub();
     execStub = stub().resolves({ stdout: 'Status: Enabled' });
@@ -30,31 +30,31 @@ describe('build', function () {
     });
   };
 
-  it('does not build for local command', async () => {
+  it('does not build for local command', async function () {
     const buildHook = getMockedBuildHook();
     await buildHook({ command: { id: 'local' }, logger: loggerStub });
     assume(builderStub).not.called();
   });
 
-  it('uses current next build', async () => {
+  it('uses current next build', async function () {
     const buildHook = getMockedBuildHook();
     await buildHook({ command: { id: 'build' }, logger: loggerStub });
     assume(builderStub).called();
   });
 
-  it('supports older gasket.command format', async () => {
+  it('supports older gasket.command format', async function () {
     const buildHook = getMockedBuildHook();
     await buildHook({ command: 'local', logger: loggerStub });
     assume(builderStub).not.called();
   });
 
-  it('checks telemetry status', async () => {
+  it('checks telemetry status', async function () {
     const buildHook = getMockedBuildHook();
     await buildHook({ command: 'local', logger: loggerStub });
     assume(execStub.args[0][0]).eqls('npx next telemetry status');
   });
 
-  it('disables telemetry', async () => {
+  it('disables telemetry', async function () {
     const buildHook = getMockedBuildHook();
     await buildHook({ command: 'local', logger: loggerStub });
     assume(execStub).called(2);
