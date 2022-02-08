02e069b131ffccb4219d67aef745568cebb6c1d0
Andrew Gerard <kinetifex@gmail.com>
2022-02-08 14:54:14 -0700

    [fix] do not polyfill intl for node >= 14 (#336)

--- packages/gasket-plugin-intl/lib/init.js
+++ packages/gasket-plugin-intl/lib/init.js
@@ -1,9 +1,15 @@
 /**
- * For SSR, the node we need to polyfill Intl to make sure all locale data is available.
- * For the browser, this should be polyfilled matching app requirements.
- *
+ * For SSR with Node < 14, we must polyfill Intl to make sure all locale data is
+ * available. For the browser, this may need polyfilled matching app requirements.
  * @see: https://techoverflow.net/2018/09/19/fixing-nodejs-intl-datetimeformat-not-formatting-properly-for-locales/
+ *
+ * For Node >= 14, full-icu is supported, so we avoid the polyfill
+ * @see: https://formatjs.io/docs/guides/runtime-requirements/#nodejs
  */
 module.exports = function initHook() {
-  global.Intl = require('intl');
+  const semver = require('semver');
+  const current = semver.coerce(process.version).version;
+  if (!semver.satisfies(current, '>=14')) {
+    global.Intl = require('intl');
+  }
 };
