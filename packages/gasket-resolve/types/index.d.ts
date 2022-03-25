81f7dcbe517cfa8b9c52200397c50dbb72c39bc1
Andrew Gerard <kinetifex@gmail.com>
2022-03-25 14:39:24 -0700

    [nit] fix exports

--- packages/gasket-resolve/types/index.d.ts
+++ packages/gasket-resolve/types/index.d.ts
@@ -1,5 +1,4 @@
-export { pluginIdentifier } from './identifiers';
-export { presetIdentifier } from './identifiers';
+export { pluginIdentifier, presetIdentifier } from './identifiers';
 export { projectIdentifier } from './package-identifier';
 export { Resolver } from './resolver';
 export { Loader, PluginInfo, ModuleInfo, PresetInfo } from './loader';
