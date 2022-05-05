75da0a16c625728257e082176bcecb5fa7a41496
Jacob Page <jpage@godaddy.com>
2022-05-05 08:53:28 -0700

    Make linter happy

--- packages/gasket-typescript-tests/test/engine.spec.ts
+++ packages/gasket-typescript-tests/test/engine.spec.ts
@@ -2,7 +2,7 @@ import Gasket from '@gasket/engine';
 
 describe('@gasket/engine', () => {
   it('exposes the constructor interface', () => {
-    // eslint-disable no-new
+    // eslint-disable-next-line no-new
     new Gasket(
       { root: __dirname, env: 'test' },
       { resolveFrom: __dirname }
