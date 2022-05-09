6c314bb74414b51b57cd51a7a1224ef511eace95
Bree Betts <bbetts@godaddy.com>
2022-05-09 11:54:59 -0700

    add cypress as option during prompt, run align-packages script and create cypress test generator in nextjs plugin

---
+++ packages/gasket-plugin-nextjs/generator/cypress/test/cypress/index.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { IndexPage } from '../../pages/index';
+
+describe('IndexPage', () => {
+  it('renders page', () => {
+    render(<IndexPage />);
+
+    expect(screen.getByRole('heading').textContent).to.equal('Welcome to Gasket!');
+  });
+});
