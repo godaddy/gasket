7a59179213d3724dc5b944a01f955fd87fc9b3fb
Andrew Gerard <kinetifex@gmail.com>
2022-05-11 14:20:07 -0700

    Fix types path (#358)
    * [dist] fix types
    
    * [fix] types and tests
    
    * [nit] rm jsdoc types
    
    * [types] tweak HOC returns
    
    * [types] use ReactNode

--- packages/gasket-react-intl/src/index.d.ts
+++ packages/gasket-react-intl/src/index.d.ts
@@ -6,43 +6,43 @@ export { LocaleStatus };
  * Make an HOC that adds a provider to managing locale files as well as the react-intl Provider.
  * This can be used to wrap a top level React or a Next.js custom App component.
  *
- * @returns {function} wrapper
+ * @returns wrapper
  */
-export function withIntlProvider(): Function;
+export function withIntlProvider<Props>(): (Component: React.ComponentType<Props>) => React.ComponentType<Props>;
 
 /**
  * Make an HOC that loads a locale file before rendering wrapped component
  *
- * @param {LocalePathPart} localePathPart - Path containing locale files
- * @param {object} [options] - Options
- * @param {React.Component} [options.loading=null] - Custom component to show while loading
- * @param {boolean} [options.initialProps=false] - Preload locales during SSR with Next.js pages
- * @param {boolean} [options.forwardRef=false] - Forward refs
- * @returns {function} wrapper
+ * @param localePathPart? - Path containing locale files
+ * @param [options.loading=null] - Custom component to show while loading
+ * @param [options.forwardRef=false] - Forward refs
+ * @param [options] - Options
+ * @param [options.initialProps=false] - Preload locales during SSR with Next.js pages
+ * @returns wrapper
  */
-export function withLocaleRequired(localePathPart?: LocalePathPart, options?: {
-  loading?: React.Component;
+export function withLocaleRequired<Props>(localePathPart?: LocalePathPart, options?: {
+  loading?: React.ReactNode;
   initialProps?: boolean;
   forwardRef?: boolean;
-}): Function;
+}): (Component: React.ComponentType<Props>) => React.ComponentType<Props>;
 
 /**
  * Component that loads a locale file before rendering children
  *
- * @param {object} props - Props
- * @param {LocalePathPart} props.localesPath - Path containing locale files
- * @param {React.Component} [props.loading] - Custom component to show while loading
- * @returns {JSX.Element|null} element
+ * @param props - Props
+ * @param props.localesPath - Path containing locale files
+ * @param [props.loading] - Custom component to show while loading
+ * @returns element
  */
 export function LocaleRequired(props: {
   localesPath: LocalePathPart;
-  loading?: React.Component;
-}): JSX.Element | null;
+  loading?: React.ReactNode;
+}): React.ReactNode | null;
 
 /**
  * React that fetches a locale file and returns loading status
  *
- * @param {LocalePathPart} localePathPart - Path containing locale files
- * @returns {LocaleStatus} status
+ * @param localePathPart - Path containing locale files
+ * @returns status
  */
 export function useLocaleRequired(localePathPart: LocalePathPart): LocaleStatus;
