848e76739aec4b61a12f7b4be060f2ecd0ce52af
Michael Mason <mmason2@godaddy.com>
2022-05-13 13:35:32 -0700

    @gasket/plugin-config - more descriptive error handling for hooks into the appEnvConfig and appRequestConfig lifecycles

--- packages/gasket-plugin-config/lib/middleware.js
+++ packages/gasket-plugin-config/lib/middleware.js
@@ -16,6 +16,14 @@ function handler(gasket) {
         req,
         res);
 
+      // Error handling
+      // Better error description for the appRequestConfig & appEnvConfig lifecycles
+      // In the event a config object isn't returned from the hook(s)
+      // it will result in a error in the destructuring of req.config
+      if (!req.config) {
+        throw new Error('req.config is undefined - Ensure that all plugins hooking into the appRequestConfig and appEnvConfig lifecycle return a config object.')
+      }
+
       const { gasketData = {} } = res.locals;
       const { public: config = {} } = req.config;
 
