7516f7c0b83b7f266be31aeedcfb1b3993b11664
Michael Mason <mmason2@godaddy.com>
2022-05-31 15:40:10 -0700

    fix indentation

--- packages/gasket-plugin-docusaurus/lib/generate-default-config.js
+++ packages/gasket-plugin-docusaurus/lib/generate-default-config.js
@@ -8,9 +8,9 @@ const defaultConfig = path.join(__dirname, '..', 'generator', 'docusaurus.config
  * @param {object} configOptions - Object to contain Docusaurus config options that will be added to the root config
  * @returns {string} - docusaurus config file
  */
- module.exports = async function generateDefaultConfig(configOptions) {
+module.exports = async function generateDefaultConfig(configOptions) {
   const configStr = await readFile(defaultConfig, 'utf-8');
   return configStr.replace(/\$\{(\w+)\}/g, (full, propName) => {
-      return configOptions[propName];
+    return configOptions[propName];
   });
 };
