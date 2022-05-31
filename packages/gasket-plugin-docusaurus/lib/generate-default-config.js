2926f758b271bf7f635fe88e226d53413317c165
Michael Mason <mmason2@godaddy.com>
2022-05-31 15:38:04 -0700

    update generator with a better regex and one line replace/return

--- packages/gasket-plugin-docusaurus/lib/generate-default-config.js
+++ packages/gasket-plugin-docusaurus/lib/generate-default-config.js
@@ -8,14 +8,9 @@ const defaultConfig = path.join(__dirname, '..', 'generator', 'docusaurus.config
  * @param {object} configOptions - Object to contain Docusaurus config options that will be added to the root config
  * @returns {string} - docusaurus config file
  */
-module.exports = async function generateDefaultConfig(configOptions) {
-  const configProperties = Object.keys(configOptions);
-  let configStr = await readFile(defaultConfig, 'utf-8');
-
-  configProperties.forEach(property => {
-    const regex = `\${${property}}`.replace(/[${}]/g, '\\$&');
-    configStr = configStr.replace(new RegExp(regex, 'g'), configOptions[property]);
+ module.exports = async function generateDefaultConfig(configOptions) {
+  const configStr = await readFile(defaultConfig, 'utf-8');
+  return configStr.replace(/\$\{(\w+)\}/g, (full, propName) => {
+      return configOptions[propName];
   });
-
-  return configStr;
 };
