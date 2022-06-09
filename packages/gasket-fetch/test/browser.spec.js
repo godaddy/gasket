20fb56dbc9b533fd77701bae2e2b2c882f9847c9
Andrew Gerard <63810935+agerard-godaddy@users.noreply.github.com>
2022-06-08 17:02:22 -0700

    [minor] wrap fetch for patches (#372)

--- packages/gasket-fetch/test/browser.spec.js
+++ packages/gasket-fetch/test/browser.spec.js
@@ -1,4 +1,5 @@
 import { describe, it } from 'mocha';
+import assume from 'assume';
 
 import fetch from '../lib/browser';
 const { AbortController, Request, Headers, Response } = fetch;
@@ -9,4 +10,10 @@ describe('fetch is available in browsers', function () {
   it('can fetch resources', assertGet(fetch));
   it('fetch can POST to API', assertPost(fetch));
   it('can abort fetch', assertAbort(fetch, AbortController));
+  it('uses patched version', function () {
+    const original = window.fetch;
+    window.fetch = () => 'replaced window.fetch';
+    assume(fetch()).equals('replaced window.fetch');
+    window.fetch = original;
+  });
 });
