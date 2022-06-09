30d8223151aafd84c5975adb227aba08cf406617
Michael Mason <mmason2@godaddy.com>
2022-06-09 13:18:04 -0700

    docs(@gasket/plugin-docs): fix block scoping of exported interfaces

--- packages/gasket-plugin-docs/lib/index.d.ts
+++ packages/gasket-plugin-docs/lib/index.d.ts
@@ -9,70 +9,6 @@ declare module '@gasket/engine' {
     }
   }
 
-  export interface DocsSetupModulesConfig {
-    [key: string]: DocsSetup
-  }
-
-  export interface DocsTransformHandlerData {
-    filename: string,
-    docsConfig: ModuleDocsConfig,
-    docsConfigSet: DocsConfigSet,
-  }
-
-  export interface DocsTransformHandler {
-    content: string,
-    data: DocsTransformHandlerData,
-  }
-
-  export interface DocsTransform {
-    global?: boolean,
-    test: RegExp,
-    handler: DocsTransformHandler
-  }
-
-  export interface DocsSetup {
-    link: string,
-    files?: Array<string>,
-    transforms?: Array<DocsTransform>,
-    modules?: DocsSetupModulesConfig
-  }
-
-  export interface DocsConfig {
-    name: string,
-    description?: string,
-    link?: string,
-    sourceRoot: string,
-    targetRoot: string
-  }
-
-  export interface ModuleDocsConfig extends DocsConfig {
-    files: Array<string>,
-    transforms: Array<DocsTransform>,
-    metadata: ModuleData
-  }
-
-  export interface DetailDocsConfig extends DocsConfig {
-    from: string
-  }
-
-  export interface LifecycleDocsConfig extends DetailDocsConfig {
-    method: string,
-    parent?: string,
-    command?: string
-  }
-  export interface DocsConfigSet {
-    app: ModuleDocsConfig,
-    plugins: Array<ModuleDocsConfig>,
-    presets: Array<ModuleDocsConfig>,
-    modules: Array<ModuleDocsConfig>,
-    structures: Array<DetailDocsConfig>,
-    commands: Array<DetailDocsConfig>,
-    guides: Array<DetailDocsConfig>,
-    lifecycles: Array<LifecycleDocsConfig>,
-    transforms: Array<DocsTransform>,
-    root: string,
-    docsRoot: string
-  }
   export interface HookExecTypes {
     docsSetup(args: { defaults: PluginData }): MaybeAsync<PluginData & {
       files?: Array<string>,
@@ -87,4 +23,69 @@ declare module '@gasket/engine' {
 
     docsGenerate(docs: DocsConfigSet): MaybeAsync<DetailData>
   }
+}
+
+export interface DocsSetupModulesConfig {
+  [key: string]: DocsSetup
+}
+
+export interface DocsTransformHandlerData {
+  filename: string,
+  docsConfig: ModuleDocsConfig,
+  docsConfigSet: DocsConfigSet,
+}
+
+export interface DocsTransformHandler {
+  content: string,
+  data: DocsTransformHandlerData,
+}
+
+export interface DocsTransform {
+  global?: boolean,
+  test: RegExp,
+  handler: DocsTransformHandler
+}
+
+export interface DocsSetup {
+  link: string,
+  files?: Array<string>,
+  transforms?: Array<DocsTransform>,
+  modules?: DocsSetupModulesConfig
+}
+
+export interface DocsConfig {
+  name: string,
+  description?: string,
+  link?: string,
+  sourceRoot: string,
+  targetRoot: string
+}
+
+export interface ModuleDocsConfig extends DocsConfig {
+  files: Array<string>,
+  transforms: Array<DocsTransform>,
+  metadata: ModuleData
+}
+
+export interface DetailDocsConfig extends DocsConfig {
+  from: string
+}
+
+export interface LifecycleDocsConfig extends DetailDocsConfig {
+  method: string,
+  parent?: string,
+  command?: string
+}
+export interface DocsConfigSet {
+  app: ModuleDocsConfig,
+  plugins: Array<ModuleDocsConfig>,
+  presets: Array<ModuleDocsConfig>,
+  modules: Array<ModuleDocsConfig>,
+  structures: Array<DetailDocsConfig>,
+  commands: Array<DetailDocsConfig>,
+  guides: Array<DetailDocsConfig>,
+  lifecycles: Array<LifecycleDocsConfig>,
+  transforms: Array<DocsTransform>,
+  root: string,
+  docsRoot: string
 }
