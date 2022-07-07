fb13b0797e379d9b1cf4b79a8a723fa6493d2e33
Bree Betts <bbetts@godaddy.com>
2022-07-07 15:58:08 -0700

    create fastify hook module

---
+++ packages/gasket-plugin-workbox/lib/fastify.js
@@ -0,0 +1,18 @@
+const serveStatic = require('serve-static');
+const { getOutputDir } = require('./utils');
+
+/**
+ * Express lifecycle to set up route for serving workbox libraries
+ *
+ * @param {Gasket} gasket - Gasket
+ * @param {Object} app - Fastify app
+ */
+module.exports = function express(gasket, app) {
+  const outputDir = getOutputDir(gasket);
+
+  app.register(serveStatic(outputDir, {
+    index: false,
+    maxAge: '1y',
+    immutable: true
+  }), { prefix: '/_workbox' });
+};
