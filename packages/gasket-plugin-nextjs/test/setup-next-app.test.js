e11bf3ad19dffc9ebc11eb4be5cf5ff1d16476c9
Bree Betts <bbetts@godaddy.com>
2022-07-12 16:51:38 -0700

    setup next app module testing

--- packages/gasket-plugin-nextjs/test/setup-next-app.test.js
+++ packages/gasket-plugin-nextjs/test/setup-next-app.test.js
@@ -6,30 +6,52 @@ const { setupNextApp } = require('../lib/setup-next-app');
 const { spy, stub } = sinon;
 
 describe('setupNextApp', () => {
-  let next, nextHandler;
+  let next, nextHandler, module;
 
-  let result, gasket, data;
+  let result, gasket, data, mockConfig, mockCreateConfig, mockCreateApp;
+
+  const getModule = () => {
+    return proxyquire('../lib/setup-next-app', {
+        // './config': {
+        //     createConfig: mockCreateConfig
+        // }, 
+        // 'next': {
+        //     default: mockCreateApp
+        // }
+    });
+  }
+
   beforeEach(() => {
-    gasket = mockGasketApi();
-    // fastifyApp = {
-    //     decorate: spy(),
-    //     register: spy(),
-    //     all: spy()
-    // };
-    // nextHandler = {
-    //   prepare: stub().resolves(),
-    //   getRequestHandler: stub().resolves({})
-    // };
+
+    nextHandler = {
+      prepare: stub().resolves(),
+      getRequestHandler: stub().resolves({})
+    };
+    mockCreateApp = stub().resolves(nextHandler)
+    
+    // module = proxyquire('../lib/setup-next-app', {
+    //     next: mockCreateApp
+    // })
+
+    module = getModule()
     // next = stub().returns(nextHandler);
 
     // plugin = proxyquire('../lib/', { next });
     // hook = plugin.hooks.fastify.handler;
   });
 
+  it('exports setupNextApp instance', function () {
+    assume(module).property('setupNextApp');
+    console.log(`MODULE : ${module.setupNextApp}`);
+    assume(setupNextApp).to.be.a('function');
+  });
+
   it('executes the `next` lifecycle', async function () {
-    result = await setupNextApp(gasket);
+    gasket = mockGasketApi();
+    result = setupNextApp(gasket);
     console.log('RESULT ==> ', result);
     assume(gasket.exec).has.been.calledWith('next', nextHandler);
+    // assume(result).property('prepare')
   });
 
   // it('does not derive a webpack config if not running a dev server', async () => {
