57bf59e6ab448249d63df830bfb6aa85126148d8
Michael Mason <105235096+mmason2-godaddy@users.noreply.github.com>
2022-07-12 16:21:46 -0700

    fix(fetch): tune up TS types (#394)
    * fix(fetch): tune up TS types
    
    * fix(fetch): resolve error with ts tests
    
    * fix(fetch): remove outdated test case

--- packages/gasket-typescript-tests/test/fetch.spec.ts
+++ packages/gasket-typescript-tests/test/fetch.spec.ts
@@ -1,5 +1,4 @@
 import { fetch } from '@gasket/fetch';
-import { AbortController } from '@gasket/fetch';
 
 describe('@gasket/fetch', function () {
   const perform = false;
@@ -11,13 +10,6 @@ describe('@gasket/fetch', function () {
       fetch(123);
       // @ts-expect-error
       fetch('url', { bogus: true });
-    }
-  });
-
-  it('exposes AbortController', function () {
-    if (perform) {
-      const controller = new AbortController();
-      const signal: AbortSignal = controller.signal;
     }
   });
 });
