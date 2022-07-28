dbe7e9ab4e00dc73c60018e5170170bf0d3a2eaa
bbetts-godaddy <104379885+bbetts-godaddy@users.noreply.github.com>
2022-07-28 13:26:56 -0700

    Support fastify with nextjs plugin (#398)
    * create fastify lifecycle in nextjs plugin
    
    * add tests for fastify hook
    
    * add nextFastify to lifecycles
    
    * update readme with nextFastify lifecycle
    
    * create helper function for creating next app to reduce code repetition
    
    * extract next app creation into helper function & write tests
    
    * tests passing for express hook
    
    * clean up fastify hook and fix tests
    
    * add next lifecycle with fastify parent to metadata
    
    * setup next app module testing
    
    * tests for setupNextApp function:
    
    * return app in fastify hook
    
    * check dev server logic
    
    * additional tests for devserver in setup next app
    
    * run npm lint:fix
    
    * add buildId test in fastify hook
    
    * fix broken tests

---
+++ packages/gasket-plugin-nextjs/lib/setup-next-app.js
@@ -0,0 +1,35 @@
+const { createConfig } = require('./config');
+const createNextApp = require('next');
+
+/**
+ * Small helper function that creates nextjs app from the gasket
+ * configuration.
+ *
+ * @param   {Gasket}  gasket                The gasket API.
+ * @returns {NextServer} The Nextjs App
+ * @private
+ */
+async function setupNextApp(gasket) {
+  const { exec, command } = gasket;
+  const devServer = (command.id || command) === 'local';
+
+  const app = createNextApp({
+    dev: devServer,
+    conf: await createConfig(gasket, devServer)
+  });
+
+  //
+  // We need to call the `next` lifecycle before we prepare the application
+  // as the prepare step initializes all the routes that a next app can have.
+  // If we wait later, it's possible that our added routes/pages are not
+  // recognized.
+  //
+  await exec('next', app);
+  await app.prepare();
+
+  return app;
+}
+
+module.exports = {
+  setupNextApp
+};
