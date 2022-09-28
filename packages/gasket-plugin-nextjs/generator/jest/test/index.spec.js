615bf2807b1f3433c98eb8ee195f254f223adcdb
jpina1 <jpina1@godaddy.com>
2022-09-28 12:42:11 -0700

    chore: changed eslint extends

--- packages/gasket-plugin-nextjs/generator/jest/test/index.spec.js
+++ packages/gasket-plugin-nextjs/generator/jest/test/index.spec.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { IndexPage } from '../pages/index';
+import { expect } from '@jest/globals';
 
 describe('IndexPage', () => {
   it('renders page', () => {
