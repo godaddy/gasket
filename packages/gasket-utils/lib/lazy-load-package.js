5862d68d1249f75a5c6df26fa30ff3f766768114
Michael Mason <mmason2@godaddy.com>
2022-11-30 14:25:00 -0800

    fix: adjust lazyload package invocation and logic

--- packages/gasket-utils/lib/lazy-load-package.js
+++ packages/gasket-utils/lib/lazy-load-package.js
@@ -10,39 +10,15 @@ const PackageManager = require('./package-manager');
  */
 async function getPkgManager(root, logger) {
   const yarnLock = await fs.readFile(path.join(root, 'yarn.lock'), 'utf8').catch(err => {
-    logger.info('Yarn not found - Falling back to NPM');
+    logger.info('Installing using npm');
   });
 
   if (yarnLock) {
-    logger.info('Yarn package manager found');
+    logger.info('Installing using yarn');
     return 'yarn';
   }
 
   return 'npm';
-}
-
-/**
- * Check package for path traversals - infers a CLI package
- * @param {string} package The passed package name
- * @returns {object} Package descriptor
- */
-function checkPackage(package) {
-  const pkgPaths = package.split('/');
-  return {
-    package: pkgPaths[0],
-    path: pkgPaths.slice(1).join('')
-  };
-}
-
-/**
- * Check if package is a devDependency
- * @param {string} root Gasket root
- * @param {string} package The passed package name
- * @returns {boolean} If package is a devDependency
- */
-function checkDevDependencies(root, package) {
-  const { devDependencies } = require(path.join(root, 'package.json'));
-  return !!devDependencies[package];
 }
 
 /**
@@ -56,17 +32,17 @@ module.exports = async function lazyLoadPackage(dependency, { gasket, saveDev = 
   const { logger } = gasket;
   const { root } = gasket.config;
   const cmd = await getPkgManager(root, logger);
-  const { package, path } = checkPackage(dependency);
-  const isDevDep = checkDevDependencies(root, package);
-  const dep = path ? `${package}/${path}` : package;
+  const package = dependency.split('/')[0];
+  const { devDependencies } = gasket.metadata.app.package;
+  const isDevDep = !!devDependencies[package];
   const flag = saveDev ? '-D' : '--no-save';
 
   try {
-    require(require.resolve(`${dep}`, { paths: [root, __dirname] }));
-    logger.info(`Package ${dep} already installed`);
-    if (isDevDep === saveDev) return require(dep);
+    require(require.resolve(`${dependency}`, { paths: [root, __dirname] }));
+    logger.info(`Package ${dependency} already installed`);
+    if (isDevDep === saveDev) return require(dependency);
   } catch (err) {
-    logger.info(`Package ${dep} is not installed`);
+    logger.info(`Package ${dependency} is not installed`);
   }
 
   if (!saveDev) logger.warning('Package not saved to devDependencies');
@@ -75,5 +51,5 @@ module.exports = async function lazyLoadPackage(dependency, { gasket, saveDev = 
   logger.info(`Installing package - ${package}...`);
   const manager = new PackageManager({ packageManager: cmd, dest: root });
   await manager.exec('install', [package, flag]);
-  return require(dep);
+  return require(dependency);
 }
---
+++ packages/gasket-utils/test/lazy-load-package.test.js
@@ -0,0 +1,7 @@
+const proxyquire = require('proxyquire');
+const assume = require('assume');
+const sinon = require('sinon');
+
+describe('lazyLoadPackage', function () {
+
+});
