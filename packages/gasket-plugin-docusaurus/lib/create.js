6a24a370270990f64c249ba339b3e247d8fd3fdd
Michael Mason <mmason2@godaddy.com>
2022-12-15 11:11:31 -0800

    testing cli

--- packages/gasket-plugin-docusaurus/lib/create.js
+++ packages/gasket-plugin-docusaurus/lib/create.js
@@ -1,19 +1,17 @@
+const { devDependencies } = require('../package');
 const timing = { after: ['@gasket/nextjs'] };
 
 async function handler(gasket, context) {
   const { pkg } = context;
-  const hasTestLibReact = pkg.has('devDependencies', '@testing-library/react');
 
   pkg.add('dependencies', {
     'react': devDependencies.react,
     'react-dom': devDependencies['react-dom']
   }, { force: true });
 
-  if (hasTestLibReact) {
-    pkg.add('devDependencies', {
-      '@testing-library/react': '^12.0.0'
-    }, { force: true });
-  }
+  pkg.add('devDependencies', {
+    '@testing-library/react': devDependencies['@testing-library/react']
+  }, { force: true });
 }
 
 module.exports = {
