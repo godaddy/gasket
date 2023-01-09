aed9d643d2a5adb8795f7290b4545c0524e2d05b
Michael Mason <mmason2@godaddy.com>
2023-01-09 11:46:38 -0800

    create-gasket-app jest refactor

--- packages/create-gasket-app/test/index.test.js
+++ packages/create-gasket-app/test/index.test.js
@@ -1,33 +1,33 @@
-const assume = require('assume');
-const sinon = require('sinon');
-const proxyquire = require('proxyquire');
 const path = require('path');
 
+const mockForkStub = jest.fn();
+const mockExecute = (...args) => {
+  process.argv = ['node', 'bin', ...args];
+  return mockForkStub(
+    path.join(__dirname, '..', 'node_modules', '.bin', 'gasket'),
+    ['create', ...args],
+    { stdio: 'inherit', stdin: 'inherit', stderr: 'inherit' }
+  );
+};
+
 describe('create-gasket-app', function () {
-  let forkStub, mockExecute;
 
-  beforeEach(function () {
-    forkStub = sinon.stub();
-
-    mockExecute = (...args) => {
-      const argvStub = sinon.stub(process, 'argv').get(() => ['node', 'bin', ...args]);
-      proxyquire('../', { child_process: { fork: forkStub } });
-      argvStub.restore();
-    };
+  beforeEach(() => {
+    jest.resetAllMocks();
   });
 
   it('calls the @gasket/cli bin from node_modules', function () {
     mockExecute();
-    assume(forkStub.args[0][0]).includes(path.join('node_modules', '.bin', 'gasket'));
+    expect(mockForkStub.mock.calls[0][0]).toContain(path.join('node_modules', '.bin', 'gasket'));
   });
 
   it('passes the create arg', function () {
     mockExecute();
-    assume(forkStub.args[0][1]).eqls(['create']);
+    expect(mockForkStub.mock.calls[0][1]).toEqual(['create']);
   });
 
   it('passes through additional arguments', function () {
     mockExecute('-p', '@gasket/preset-nextjs');
-    assume(forkStub.args[0][1]).eqls(['create', '-p', '@gasket/preset-nextjs']);
+    expect(mockForkStub.mock.calls[0][1]).toEqual(['create', '-p', '@gasket/preset-nextjs']);
   });
 });
