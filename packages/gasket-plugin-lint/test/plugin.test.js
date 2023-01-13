d2e8f9711fad5f67ed6cd0f52674eca3c687097a
Michael Mason <mmason2@godaddy.com>
2023-01-13 15:25:43 -0800

    jest refactor @gasket/plugin-lint

--- packages/gasket-plugin-lint/test/plugin.test.js
+++ packages/gasket-plugin-lint/test/plugin.test.js
@@ -1,15 +1,13 @@
-const { describe, it } = require('mocha');
-const assume = require('assume');
 const plugin = require('../lib');
 
 describe('Plugin', function () {
 
   it('is an object', () => {
-    assume(plugin).is.an('object');
+    expect(typeof plugin).toBe('object');
   });
 
   it('has expected name', () => {
-    assume(plugin).to.have.property('name', require('../package').name);
+    expect(plugin).toHaveProperty('name', require('../package').name);
   });
 
   it('has expected hooks', () => {
@@ -19,10 +17,10 @@ describe('Plugin', function () {
       'postCreate'
     ];
 
-    assume(plugin).to.have.property('hooks');
+    expect(plugin).toHaveProperty('hooks');
 
     const hooks = Object.keys(plugin.hooks);
-    assume(hooks).eqls(expected);
-    assume(hooks).is.length(expected.length);
+    expect(hooks).toEqual(expected);
+    expect(hooks).toHaveLength(expected.length);
   });
 });
