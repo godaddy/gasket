5add1d4bf6ea09aee88620987b163d1613eb32f6
kbader-godaddy <kbader@godaddy.com>
2023-01-13 14:06:16 -0700

    Update tests with RTL.

--- packages/gasket-nextjs/test/gasket-data-script.test.js
+++ packages/gasket-nextjs/test/gasket-data-script.test.js
@@ -1,14 +1,17 @@
-// import React from 'react';
-// import assume from 'assume';
-// import { render, screen, queryByAttribute } from '@testing-library/react';
+import React from 'react';
+import assume from 'assume';
+import { render, queryByAttribute } from '@testing-library/react';
 
-// import { GasketDataScript } from '../src';
+import { GasketDataScript } from '../src';
 
-// const getById = queryByAttribute.bind(null, 'id');
+const getById = queryByAttribute.bind(null, 'id');
 
-// describe('<GasketDataScript/>', function () {
-//   it('renders GasketDataScript', function () {
-//     render(<GasketDataScript {...{ data: { bogus: true } }} />);
-//     // assume(screen.getByText('{"bogus":true}')).exists();
-//   });
-// });
+describe('<GasketDataScript/>', function () {
+  it('renders GasketDataScript', function () {
+    const dom = render(<GasketDataScript {...{ data: { bogus: true } }} />);
+    const gasketDataScript = getById(dom.container, 'GasketData');
+
+    assume(gasketDataScript).exists();
+    assume(gasketDataScript.textContent).equals('{"bogus":true}');
+  });
+});
--- packages/gasket-nextjs/test/with-gasket-data-provider.test.js
+++ packages/gasket-nextjs/test/with-gasket-data-provider.test.js
@@ -1,67 +1,67 @@
-// import React from 'react';
-// import { shallow } from 'enzyme';
-// import assume from 'assume';
-// import decache from 'decache';
-// import mock from 'mock-require';
+import React from 'react';
+import assume from 'assume';
+import decache from 'decache';
+import mock from 'mock-require';
+import { render } from '@testing-library/react';
 
-// describe('withGasketDataProvider', function () {
+describe('withGasketDataProvider', function () {
 
-//   const setup = (testData = {}) => {
-//     mock('@gasket/data', testData);
-//     const { withGasketDataProvider } = require('../src/with-gasket-data-provider');
-//     return { withGasketDataProvider };
-//   };
+  const setup = (testData = {}) => {
+    mock('@gasket/data', testData);
+    const { withGasketDataProvider } = require('../src/with-gasket-data-provider');
+    return { withGasketDataProvider };
+  };
 
-//   beforeEach(() => {
-//     decache('../src/with-gasket-data-provider');
-//   });
+  beforeEach(() => {
+    decache('../src/with-gasket-data-provider');
+  });
 
-//   it('should render the component', () => {
-//     const { withGasketDataProvider } = setup();
-//     const HocComponent = withGasketDataProvider()(() => <div/>);
-//     const container = shallow(<HocComponent/>);
+  it('should render the component', () => {
+    const { withGasketDataProvider } = setup();
+    const HocComponent = withGasketDataProvider()(() => <div/>);
+    const container = render(<HocComponent/>);
 
-//     assume(container).exists();
-//   });
+    assume(container).exists();
+  });
 
-//   it('should inject gasketData when client side', async () => {
-//     const testData = { test: 'hello' };
-//     const { withGasketDataProvider } = setup(testData);
+  it('should inject gasketData when client side', async () => {
+    const testData = { test: 'hello' };
+    const { withGasketDataProvider } = setup(testData);
 
-//     const HocComponent = withGasketDataProvider()(({ children }) => <div>{children}</div>);
-//     const intPropsResponse = await HocComponent.getInitialProps({});
+    const HocComponent = withGasketDataProvider()(({ children }) => <div>{children}</div>);
+    const intPropsResponse = await HocComponent.getInitialProps({});
 
-//     assume(intPropsResponse).eql({ gasketData: testData });
-//   });
-
-
-//   it('should inject gasketData when SSR', async () => {
-//     const { withGasketDataProvider } = setup();
-//     const serverTestData = { test: 'hello world' };
-
-//     // eslint-disable-next-line react/prop-types
-//     const Component = ({ children }) => <div>{children}</div>;
-
-//     const HocComponent = withGasketDataProvider()(Component);
-//     const intPropsResponse = await HocComponent.getInitialProps({ ctx: { res: { locals: { gasketData: serverTestData } } } });
-
-//     assume(intPropsResponse).eql({ gasketData: serverTestData });
-//   });
+    assume(intPropsResponse).eql({ gasketData: testData });
+  });
 
 
-//   it('should call wrappedComponents getInitialProps', async () => {
-//     const { withGasketDataProvider } = setup();
+  it('should inject gasketData when SSR', async () => {
+    const { withGasketDataProvider } = setup();
+    const serverTestData = { test: 'hello world' };
 
-//     const serverTestData = { test: 'hello world' };
-//     const intProps = { called: true };
+    // eslint-disable-next-line react/prop-types
+    const Component = ({ children }) => <div>{children}</div>;
 
-//     // eslint-disable-next-line react/prop-types
-//     const Component = ({ children }) => <div>{children}</div>;
-//     Component.getInitialProps = async () => (intProps);
+    const HocComponent = withGasketDataProvider()(Component);
+    const intPropsResponse = await HocComponent.getInitialProps({ ctx: { res: { locals: { gasketData: serverTestData } } } });
 
-//     const HocComponent = withGasketDataProvider()(Component);
-//     const intPropsResponse = await HocComponent.getInitialProps({ ctx: { res: { locals: { gasketData: serverTestData } } } });
+    assume(intPropsResponse).eql({ gasketData: serverTestData });
+  });
 
-//     assume(intPropsResponse).eql({ ...intProps, gasketData: serverTestData });
-//   });
-// });
+
+  it('should call wrappedComponents getInitialProps', async () => {
+    const { withGasketDataProvider } = setup();
+
+    const serverTestData = { test: 'hello world' };
+    const intProps = { called: true };
+
+    // eslint-disable-next-line react/prop-types
+    const Component = ({ children }) => <div>{children}</div>;
+    Component.getInitialProps = async () => (intProps);
+
+    const HocComponent = withGasketDataProvider()(Component);
+    const intPropsResponse = await HocComponent.getInitialProps({ ctx: { res: { locals: { gasketData: serverTestData } } } });
+
+    assume(intPropsResponse).eql({ ...intProps, gasketData: serverTestData });
+  });
+});
