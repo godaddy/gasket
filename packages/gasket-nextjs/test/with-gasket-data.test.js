ad27f68d39ed42266cd22a27561b8c8d3624b640
kbader-godaddy <kbader@godaddy.com>
2023-01-17 14:48:41 -0700

    Update final test.

--- packages/gasket-nextjs/test/with-gasket-data.test.js
+++ packages/gasket-nextjs/test/with-gasket-data.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import assume from 'assume';
 import sinon from 'sinon';
-import { render } from '@testing-library/react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { withGasketData, GasketDataScript } from '../src';
+import { withGasketData } from '../src';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -16,15 +15,45 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head/>
+        <Head />
         <body>
-          <Main/>
-          <NextScript/>
+          <Main />
+          <NextScript />
           <p>some footer content</p>
         </body>
       </Html>
     );
   }
+}
+
+/**
+ * Recursively searches through the rendered element tree for the first element
+ * that matches the search type
+ * 
+ * @param renderedElement - The element that was rendered by the component.
+ * @param searchType - The type of the element you're looking for.
+ * @returns the first element that matches the searchType.
+ */
+function getElementReference(renderedElement, searchType) {
+  let elemRef;
+
+  Object.keys(renderedElement).some(function (currType) {
+    if (currType === 'type' && renderedElement[currType] === searchType) {
+      elemRef = renderedElement;
+      return true;
+    }
+
+    if (currType === 'props'
+      || currType === 'children'
+      || renderedElement[currType]?.props
+      || renderedElement[currType]?.children
+      || renderedElement[currType]?.type?.name) {
+      elemRef = getElementReference(renderedElement[currType], searchType);
+      return !!elemRef;
+    }
+  });
+
+  return elemRef;
 }
 
 describe('withGasketData', function () {
@@ -86,140 +115,164 @@ describe('withGasketData', function () {
   });
 
   describe('renders GasketDataScript', function () {
-    // it('in document body', function () {
-    //   WrappedDocument = withGasketData()(Document);
+    // NOTE: React Testing Library is currently unable to render the Document
+    // component, so we are manually traversing the WrappedDocument's render
+    // output. This is not ideal and should be replaced with a better solution.
+    it('in document body', function () {
+      WrappedDocument = withGasketData()(Document);
+      const element = new WrappedDocument();
+      element.props = mockProps;
 
-    //   const wrapper = render(<WrappedDocument { ...mockProps } />);
-    //   assume(container.querySelector('body').find(GasketDataScript)).length(1);
-    // });
+      const body = element.render().props.children[1];
+      const gasketDataScript = body.props.children[1];
 
-    // it('in custom document body', function () {
-    //   WrappedDocument = withGasketData()(MyDocument);
+      assume(gasketDataScript).exists();
+    });
 
-    //   const wrapper = render(<WrappedDocument { ...mockProps } />);
-    //   assume(container.querySelector('body').find(GasketDataScript)).length(1);
-    // });
+    it('in custom document body', function () {
+      WrappedDocument = withGasketData()(MyDocument);
+      const element = new WrappedDocument();
+      element.props = mockProps;
+      const body = element.render().props.children[1];
+      const gasketDataScript = body.props.children[1];
 
-  //   it('retains all original content', function () {
-  //     WrappedDocument = withGasketData()(MyDocument);
+      assume(gasketDataScript).exists();
+    });
 
-  //     const wrapper = render(<WrappedDocument { ...mockProps } />);
+    it('retains all original content', function () {
+      WrappedDocument = withGasketData()(MyDocument);
+      const element = new WrappedDocument();
+      element.props = mockProps;
+      const html = element.render();
+      const head = html.props.children[0];
+      const body = getElementReference(html, 'body');
+      const main = body.props.children[0];
+      const gasketDataScript = body.props.children[1];
+      const nextScript = body.props.children[1];
 
-  //     assume(container.querySelector('body').find(GasketDataScript)).length(1);
+      assume(gasketDataScript).exists();
 
-  //     // from parent
-  //     assume(container.querySelector(Html)).length(1);
-  //     assume(container.querySelector(Head)).length(1);
-  //     assume(container.querySelector('body').find(Main)).length(1);
-  //     assume(container.querySelector('body').find(NextScript)).length(1);
-  //     assume(container.querySelector('body').find('p')).length(1);
-  //   });
+      // from parent
+      assume(html).exists();
+      assume(head).exists();
+      assume(main).exists();
+      assume(nextScript).exists();
+      assume(getElementReference(html, 'p')).exists();
+    });
 
-  //   it('between Main and NextScript', function () {
-  //     WrappedDocument = withGasketData()(MyDocument);
+    it('between Main and NextScript', function () {
+      WrappedDocument = withGasketData()(MyDocument);
+      const element = new WrappedDocument();
+      element.props = mockProps;
+      const body = getElementReference(element.render(), 'body');
 
-  //     const wrapper = render(<WrappedDocument { ...mockProps } />);
-  //     assume(container.querySelector('body').children().at(0).name()).equals('Main');
-  //     assume(container.querySelector('body').children().at(1).name()).equals('GasketDataScript');
-  //     assume(container.querySelector('body').children().at(2).name()).equals('NextScript');
-  //   });
+      assume(body.props.children[0].type.name).equals('Main');
+      assume(body.props.children[1].type.name).equals('GasketDataScript');
+      assume(body.props.children[2].type.name).equals('NextScript');
+    });
 
-  //   it('before other body scripts', function () {
-  //     class MyDocumentWithScript extends Document {
-  //       static async getInitialProps(ctx) {
-  //         const initialProps = await Document.getInitialProps(ctx);
-  //         return { ...initialProps };
-  //       }
+    it('before other body scripts', function () {
+      class MyDocumentWithScript extends Document {
+        static async getInitialProps(ctx) {
+          const initialProps = await Document.getInitialProps(ctx);
+          return { ...initialProps };
+        }
 
-  //       render() {
-  //         return (
-  //           <Html>
-  //             <Head/>
-  //             <body>
-  //               <div/>
-  //               <script id='CustomScript'/>
-  //               <NextScript/>
-  //             </body>
-  //           </Html>
-  //         );
-  //       }
-  //     }
+        render() {
+          return (
+            <Html>
+              <Head />
+              <body>
+                <div />
+                <script id='CustomScript' />
+                <NextScript />
+              </body>
+            </Html>
+          );
+        }
+      }
 
-  //     WrappedDocument = withGasketData()(MyDocumentWithScript);
+      WrappedDocument = withGasketData()(MyDocumentWithScript);
+      const element = new WrappedDocument();
+      element.props = mockProps;
+      const body = getElementReference(element.render(), 'body');
 
-  //     const wrapper = render(<WrappedDocument { ...mockProps } />);
-  //     assume(container.querySelector('body').children().at(0).name()).equals('div');
-  //     assume(container.querySelector('body').children().at(1).name()).equals('GasketDataScript');
-  //     assume(container.querySelector('body').children().at(2).name()).equals('script');
-  //     assume(container.querySelector('body').children().at(3).name()).equals('NextScript');
-  //   });
+      assume(body.props.children[0].type).equals('div');
+      assume(body.props.children[1].type.name).equals('GasketDataScript');
+      assume(body.props.children[2].type).equals('script');
+      assume(body.props.children[3].type.name).equals('NextScript');
+    });
 
-  //   it('after first element (assume to be wrapping Main)', function () {
-  //     class MyDocumentWithScript extends Document {
-  //       static async getInitialProps(ctx) {
-  //         const initialProps = await Document.getInitialProps(ctx);
-  //         return { ...initialProps };
-  //       }
+    it('after first element (assume to be wrapping Main)', function () {
+      class MyDocumentWithScript extends Document {
+        static async getInitialProps(ctx) {
+          const initialProps = await Document.getInitialProps(ctx);
+          return { ...initialProps };
+        }
 
-  //       render() {
-  //         return (
-  //           <Html>
-  //             <Head/>
-  //             <body>
-  //               <div id='SomethingWrappingMain'>
-  //                 <Main/>
-  //               </div>
-  //               <div id='SomethingWrappingNextScript'>
-  //                 <NextScript/>
-  //               </div>
-  //             </body>
-  //           </Html>
-  //         );
-  //       }
-  //     }
+        render() {
+          return (
+            <Html>
+              <Head />
+              <body>
+                <div id='SomethingWrappingMain'>
+                  <Main />
+                </div>
+                <div id='SomethingWrappingNextScript'>
+                  <NextScript />
+                </div>
+              </body>
+            </Html>
+          );
+        }
+      }
 
-  //     WrappedDocument = withGasketData()(MyDocumentWithScript);
+      WrappedDocument = withGasketData()(MyDocumentWithScript);
+      const element = new WrappedDocument();
+      element.props = mockProps;
+      const body = getElementReference(element.render(), 'body');
 
-  //     const wrapper = render(<WrappedDocument { ...mockProps } />);
-  //     assume(container.querySelector('body').children().at(0).name()).equals('div');
-  //     assume(container.querySelector('body').children().at(1).name()).equals('GasketDataScript');
-  //     assume(container.querySelector('body').children().at(2).name()).equals('div');
-  //   });
+      assume(body.props.children[0].type).equals('div');
+      assume(body.props.children[1].type.name).equals('GasketDataScript');
+      assume(body.props.children[2].type).equals('div');
+    });
 
-  //   it('at forced index set in options', function () {
-  //     class MyDocumentWithScript extends Document {
-  //       static async getInitialProps(ctx) {
-  //         const initialProps = await Document.getInitialProps(ctx);
-  //         return { ...initialProps };
-  //       }
+    it('at forced index set in options', function () {
+      class MyDocumentWithScript extends Document {
+        static async getInitialProps(ctx) {
+          const initialProps = await Document.getInitialProps(ctx);
+          return { ...initialProps };
+        }
 
-  //       render() {
-  //         return (
-  //           <Html>
-  //             <Head/>
-  //             <body>
-  //               <header />
-  //               <Main/>
-  //               <script />
-  //               <div />
-  //               <footer />
-  //               <NextScript/>
-  //             </body>
-  //           </Html>
-  //         );
-  //       }
-  //     }
+        render() {
+          return (
+            <Html>
+              <Head />
+              <body>
+                <header />
+                <Main />
+                <script />
+                <div />
+                <footer />
+                <NextScript />
+              </body>
+            </Html>
+          );
+        }
+      }
 
-  //     WrappedDocument = withGasketData({ index: 4 })(MyDocumentWithScript);
+      WrappedDocument = withGasketData({ index: 4 })(MyDocumentWithScript);
+      const element = new WrappedDocument();
+      element.props = mockProps;
+      const body = getElementReference(element.render(), 'body');
 
-  //     const wrapper = render(<WrappedDocument { ...mockProps } />);
-  //     assume(container.querySelector('body').children().at(0).name()).equals('header');
-  //     assume(container.querySelector('body').children().at(1).name()).equals('Main');
-  //     assume(container.querySelector('body').children().at(2).name()).equals('script');
-  //     assume(container.querySelector('body').children().at(3).name()).equals('div');
-  //     assume(container.querySelector('body').children().at(4).name()).equals('GasketDataScript');
-  //     assume(container.querySelector('body').children().at(5).name()).equals('footer');
-  //     assume(container.querySelector('body').children().at(6).name()).equals('NextScript');
-  //   });
+      assume(body.props.children[0].type).equals('header');
+      assume(body.props.children[1].type.name).equals('Main');
+      assume(body.props.children[2].type).equals('script');
+      assume(body.props.children[3].type).equals('div');
+      assume(body.props.children[4].type.name).equals('GasketDataScript');
+      assume(body.props.children[5].type).equals('footer');
+      assume(body.props.children[6].type.name).equals('NextScript');
+    });
   });
 });
