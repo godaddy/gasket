a11417e0b0d3e48e2685aa205e04b698735dadea
John Detlefs <jdetlefs@godaddy.com>
2023-05-25 16:29:28 -0500

    Rename config

--- packages/gasket-plugin-happyfeet/lib/index.js
+++ packages/gasket-plugin-happyfeet/lib/index.js
@@ -4,11 +4,11 @@ module.exports = {
     name: require('../package').name,
     hooks: {
         preboot: async function preboot(gasket) {
-            const happyConfig = gasket.config.get('happyConfig') || {}
+            const happyConfig = gasket.config.get('happyFeet') || {}
             gasket.happyFeet = happyFeet(happyConfig)
         },
         healthcheck: async function healthcheck(gasket, HealthCheckError) {
-            const happyConfig = gasket.config.get('happyConfig') || {}
+            const happyConfig = gasket.config.get('happyFeet') || {}
             const happy = gasket.happyFeet;
             if (happy.state === happy.STATE.UNHAPPY) {
                 // flag pod to be removed from LB
