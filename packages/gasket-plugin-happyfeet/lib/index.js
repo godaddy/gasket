bbb4eaed066465f3e89c08d109e9fdaa40941299
John Detlefs <jdetlefs@godaddy.com>
2023-05-26 10:25:48 -0500

    Address review comments

--- packages/gasket-plugin-happyfeet/lib/index.js
+++ packages/gasket-plugin-happyfeet/lib/index.js
@@ -1,20 +1,19 @@
 const happyFeet = require('happy-feet');
 
 module.exports = {
-    name: require('../package').name,
-    hooks: {
-        preboot: async function preboot(gasket) {
-            const happyConfig = gasket.config.get('happyFeet') || {}
-            gasket.happyFeet = happyFeet(happyConfig)
-        },
-        healthcheck: async function healthcheck(gasket, HealthCheckError) {
-            const happyConfig = gasket.config.get('happyFeet') || {}
-            const happy = gasket.happyFeet;
-            if (happy.state === happy.STATE.UNHAPPY) {
-                // flag pod to be removed from LB
-                throw new HealthCheckError(`Happy Feet entered an unhappy state`)
-            }
-            return 'page ok';
-        }
+  name: require('../package').name,
+  hooks: {
+    preboot: async function preboot(gasket) {
+      const happyConfig = gasket.config.happyFeet || {};
+      gasket.happyFeet = happyFeet(happyConfig);
+    },
+    healthcheck: async function healthcheck(gasket, HealthCheckError) {
+      const happy = gasket.happyFeet;
+      if (happy.state === happy.STATE.UNHAPPY) {
+        // flag pod to be removed from LB
+        throw new HealthCheckError(`Happy Feet entered an unhappy state`);
+      }
+      return 'page ok';
     }
+  }
 };
--- packages/gasket-plugin-happyfeet/test/index.test.js
+++ packages/gasket-plugin-happyfeet/test/index.test.js
@@ -1,33 +1,34 @@
+// eslint-disable-next-line no-unused-vars
 const happyFeet = require('happy-feet');
 
 jest.mock('happy-feet', () => () => ({
-    state: jest.fn(),
-    STATE: { UNHAPPY: 'unhappy' }
+  state: jest.fn(),
+  STATE: { UNHAPPY: 'unhappy' }
 }));
 
 
 const plugin = require('../lib');
 
 describe('Plugin', () => {
-    beforeEach(() => {
-        jest.restoreAllMocks();
-    })
-    it('Returns error when happyfeet is sad', async () => {
-        const gasket = { config: { get: jest.fn() } };
-        await plugin.hooks.preboot(gasket)
-        gasket.happyFeet.state = 'unhappy'
-        try {
-            await plugin.hooks.healthcheck(gasket, Error)
-        } catch (e) {
-            expect(e.message).toEqual('Happy Feet entered an unhappy state');
-        }
-    });
-    it('Returns page ok when happyfeet is fine', async () => {
-        const gasket = { config: { get: jest.fn() } };
-        await plugin.hooks.preboot(gasket)
-        gasket.happyFeet.state = 'happy'
-        const response = await plugin.hooks.healthcheck(gasket, Error);
-        console.log(response);
-        expect(response).toEqual('page ok')
-    })
-})
+  beforeEach(() => {
+    jest.restoreAllMocks();
+  });
+  it('Returns error when happyfeet is sad', async () => {
+    const gasket = { config: { get: jest.fn() } };
+    await plugin.hooks.preboot(gasket);
+    gasket.happyFeet.state = 'unhappy';
+    try {
+      await plugin.hooks.healthcheck(gasket, Error);
+    } catch (e) {
+      // eslint-disable-next-line jest/no-conditional-expect
+      expect(e.message).toEqual('Happy Feet entered an unhappy state');
+    }
+  });
+  it('Returns page ok when happyfeet is fine', async () => {
+    const gasket = { config: { get: jest.fn() } };
+    await plugin.hooks.preboot(gasket);
+    gasket.happyFeet.state = 'happy';
+    const response = await plugin.hooks.healthcheck(gasket, Error);
+    expect(response).toEqual('page ok');
+  });
+});
