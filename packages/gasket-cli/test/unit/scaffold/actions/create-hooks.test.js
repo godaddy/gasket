d7b1b4b4357c5eda62c24d05af53d53e3e5c25c6
Michael Mason <mmason2@godaddy.com>
2024-01-12 10:59:48 -0800

    refactor apply-preset-config,cli-version,create-hooks

--- packages/gasket-cli/test/unit/scaffold/actions/create-hooks.test.js
+++ packages/gasket-cli/test/unit/scaffold/actions/create-hooks.test.js
@@ -1,72 +1,60 @@
-const sinon = require('sinon');
-const assume = require('assume');
-const proxyquire = require('proxyquire');
-const ConfigBuilder = require('../../../../src/scaffold/config-builder');
+const mockHandlerStub = jest.fn();
+const mockExecApplyStub = jest.fn();
+const mockCreateEngineStub = jest.fn();
 
+jest.mock('../../../../src/scaffold/create-engine', () => mockCreateEngineStub);
+jest.mock('../../../../src/scaffold/files', () => class Files {});
+jest.mock('../../../../src/scaffold/action-wrapper', () => require('../../../helpers').mockActionWrapper);
+const Files = require('../../../../src/scaffold/files');
+const ConfigBuilder = require('../../../../src/scaffold/config-builder');
+const createHooks = require('../../../../src/scaffold/actions/create-hooks');
 
 describe('createHooks', () => {
-  let sandbox, mockImports, mockContext, mockPlugin, createHooks;
-  let createEngineStub, execApplyStub, handlerStub;
+  let mockContext, mockPlugin;
 
   beforeEach(() => {
-    sandbox = sinon.createSandbox();
+    mockHandlerStub.mockResolvedValue();
+    mockCreateEngineStub.mockReturnValue({ execApply: mockExecApplyStub });
 
     mockPlugin = { name: 'mockPlugin' };
-    handlerStub = sandbox.stub().resolves();
-    execApplyStub = sandbox.stub();
-
     mockContext = {
       appName: 'my-app',
       dest: '/some/path/my-app',
       presets: ['bogus-preset'],
       plugins: ['bogus-A-plugin', 'bogus-B-plugin'],
       pkg: {},
-      runWith: sinon.stub().callsFake(plugin => ({ ...plugin, proxied: true }))
+      runWith: jest.fn().mockImplementation(plugin => ({ ...plugin, proxied: true }))
     };
-
-    createEngineStub = sandbox.stub().returns({ execApply: execApplyStub });
-
-    mockImports = {
-      '../create-engine': createEngineStub,
-      '../files': class Files {
-      },
-      '../action-wrapper': require('../../../helpers').mockActionWrapper
-    };
-
-
-    createHooks = proxyquire('../../../../src/scaffold/actions/create-hooks', mockImports);
   });
 
   afterEach(() => {
-    sandbox.restore();
+    jest.clearAllMocks();
   });
 
   it('is decorated action', async () => {
-    assume(createHooks).property('wrapped');
+    expect(createHooks).toHaveProperty('wrapped');
   });
 
   it('adds files to context', async () => {
     await createHooks(mockContext);
-    assume(mockContext.files).to.be.instanceOf(mockImports['../files']);
+    expect(mockContext.files).toBeInstanceOf(Files);
   });
 
   it('adds gasketConfig to context', async () => {
     await createHooks(mockContext);
-    assume(mockContext.gasketConfig).is.instanceOf(ConfigBuilder);
+    expect(mockContext.gasketConfig).toBeInstanceOf(ConfigBuilder);
   });
 
   it('executes the create hook with applyCreate callback', async () => {
     await createHooks(mockContext);
-    assume(execApplyStub).is.calledWithMatch('create', sinon.match(value => {
-      return typeof value === 'function' && value.name === 'applyCreate';
-    }));
+    expect(mockExecApplyStub).toHaveBeenCalledWith('create', expect.any(Function));
   });
 
   it('applyCreate callback executes handler with proxied plugin source', async () => {
     await createHooks(mockContext);
-    const callbackFn = execApplyStub.getCall(0).args[1];
-    await callbackFn(mockPlugin, handlerStub);
-    assume(mockContext.runWith).calledWith(mockPlugin);
-    assume(handlerStub).calledWithMatch({ ...mockPlugin, proxied: true });
+    const callbackFn = mockExecApplyStub.mock.calls[0][1];
+    await callbackFn(mockPlugin, mockHandlerStub);
+    expect(mockContext.runWith).toHaveBeenCalledWith(mockPlugin);
+    expect(mockHandlerStub).toHaveBeenCalledWith({ ...mockPlugin, proxied: true });
   });
 });
