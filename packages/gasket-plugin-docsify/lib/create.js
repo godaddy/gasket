04a0e0aff67da1752ab1bbe60c1b53a8b330c4ed
Andrew Gerard <63810935+agerard-godaddy@users.noreply.github.com>
2024-01-19 08:50:10 -0700

    Fix docsify plugin create (#663)
    * fix: docsify create
    
    * docs: changelog

--- packages/gasket-plugin-docsify/lib/create.js
+++ packages/gasket-plugin-docsify/lib/create.js
@@ -1,6 +1,6 @@
 module.exports = function create(_gasket, { pkg }) {
   // Workaround for https://github.com/docsifyjs/docsify/issues/2345
-  pkg.add('dependencies', {
-    'strip-indent': require('docsify/package.json').dependencies['strip-indent']
+  pkg.add('devDependencies', {
+    'strip-indent': '^3.0.0'
   });
 };
