ae67004d6255cc14e81f398f8ee91f3799920b3d
Jordan Pina <106619535+jpina1-godaddy@users.noreply.github.com>
2024-01-23 13:20:36 -0800

    chore: removed applyEnvironmentOverrides (#649)

--- packages/gasket-utils/lib/index.js
+++ packages/gasket-utils/lib/index.js
@@ -1,7 +1,6 @@
 const tryRequire = require('./try-require');
 const { tryResolve } = require('./try-resolve');
 const applyConfigOverrides = require('./apply-config-overrides');
-const applyEnvironmentOverrides = require('./apply-env-overrides');
 const runShellCommand = require('./run-shell-command');
 const PackageManager = require('./package-manager');
 const requireWithInstall = require('./require-with-install');
@@ -10,7 +9,6 @@ module.exports = {
   tryRequire,
   tryResolve,
   applyConfigOverrides,
-  applyEnvironmentOverrides,
   runShellCommand,
   PackageManager,
   requireWithInstall
