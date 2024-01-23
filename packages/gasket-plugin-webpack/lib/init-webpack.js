190dceba100bdb7f9c58285d0fbda093dc0079af
Bree Betts <bbetts@godaddy.com>
2024-01-23 13:36:49 -0800

    simplify execWaterfallSync

--- packages/gasket-plugin-webpack/lib/init-webpack.js
+++ packages/gasket-plugin-webpack/lib/init-webpack.js
@@ -1,11 +1,10 @@
 /**
  * Sets up a context object with special getters
  *
- * @param {Gasket} gasket - The Gasket API
  * @param {object} context - Additional context-specific information
  * @returns {object} context
  */
-function setupContext(gasket, context) {
+function setupContext(context) {
   return {
     ...context,
     get webpack() {
@@ -24,7 +23,7 @@ function setupContext(gasket, context) {
 module.exports = function initWebpack(gasket, initConfig, context) {
   const WebpackMetricsPlugin = require('./webpack-metrics-plugin');
 
-  let baseConfig = {
+  const baseConfig = {
     ...initConfig,
     plugins: [
       ...(initConfig && initConfig.plugins ? initConfig.plugins : []),
@@ -33,9 +32,5 @@ module.exports = function initWebpack(gasket, initConfig, context) {
   };
 
   // eslint-disable-next-line no-sync
-  gasket.execApplySync('webpackConfig', (_, handler) => {
-    baseConfig = handler(baseConfig, setupContext(gasket, context));
-  });
-
-  return baseConfig;
+  return gasket.execApplySync('webpackConfig', baseConfig, setupContext(context));
 };
