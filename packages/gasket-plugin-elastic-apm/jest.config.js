6c39e657ad018647b3168d717c8c484e5a4b1ffe
Bree Betts <bbetts@godaddy.com>
2024-01-24 16:27:53 -0800

    cleanup deprecations in @gasket/plugin-elastic-apm and add jest.config file to support env vars in testing

---
+++ packages/gasket-plugin-elastic-apm/jest.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  setupFiles: ['<rootDir>/test/setup.js'],
+  testEnvironment: 'node'
+};
---
+++ packages/gasket-plugin-elastic-apm/test/setup.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-process-env */
+process.env.ELASTIC_APM_SERVER_URL = 'FAKE_ELASTIC_APM_URL';
+process.env.ELASTIC_APM_SECRET_TOKEN = 'TOKEN_1234';
+
