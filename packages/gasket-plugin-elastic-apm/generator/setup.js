5c92cee11dc9d6bbcc000609b43995a9699912f7
Bree Betts <bbetts@godaddy.com>
2024-02-05 20:14:52 -0800

    add captureHeaders default

--- packages/gasket-plugin-elastic-apm/generator/setup.js
+++ packages/gasket-plugin-elastic-apm/generator/setup.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 // Elastic APM setup
 require('elastic-apm-node').start({
   serviceName: 'my-service-name',
+  captureHeaders: false,
   secretToken: process.env.ELASTIC_APM_SECRET_TOKEN,
   serverUrl: process.env.ELASTIC_APM_SERVER_URL
 });
