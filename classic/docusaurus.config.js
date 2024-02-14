c994e4246a8466dc7b2d37761252e0ff91e654fc
Michael Mason <mmason2@godaddy.com>
2024-02-14 09:57:47 -0800

    test GH pages

--- classic/docusaurus.config.js
+++ classic/docusaurus.config.js
@@ -1,10 +1,4 @@
-// @ts-check
-// `@type` JSDoc annotations allow editor autocompletion and type checking
-// (when paired with `@ts-check`).
-// There are various equivalent ways to declare your Docusaurus config.
-// See: https://docusaurus.io/docs/api/docusaurus-config
-
-import { themes as prismThemes } from 'prism-react-renderer';
+const { themes } = require('prism-react-renderer');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -103,8 +97,8 @@ const config = {
         copyright: `Made with ❤️ by the Gasket Team`,
       },
       prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
+        theme: themes.github,
+        darkTheme: themes.dracula,
       },
     }),
 };
