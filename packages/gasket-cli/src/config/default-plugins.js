44f204f4d0285e352d52914f9067f69d7e1eca71
Michael Mason <mmason2@godaddy.com>
2024-02-26 15:34:30 -0800

    fix default plugin

--- packages/gasket-cli/src/config/default-plugins.js
+++ packages/gasket-cli/src/config/default-plugins.js
@@ -1,4 +1,4 @@
-import { plugin as pluginCommand } from '@gasket/plugin-command';
+import pluginCommand from '@gasket/plugin-command';
 // import { hooks as pluginLifecycle } from '@gasket/plugin-lifecycle';
 // import { hooks as pluginMetadata } from '@gasket/plugin-metadata';
 // import { hooks as pluginStart } from '@gasket/plugin-start';
