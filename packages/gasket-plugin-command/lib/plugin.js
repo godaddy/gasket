d462378c91c3d7f379aad148156ef074c8dc895f
Michael Mason <mmason2@godaddy.com>
2024-02-27 10:33:00 -0800

    update export

--- packages/gasket-plugin-command/lib/plugin.js
+++ packages/gasket-plugin-command/lib/plugin.js
@@ -5,6 +5,7 @@ import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 const pkg = require('../package.json');
 
+
 export const plugin = {
   name: pkg.name,
   hooks: {
