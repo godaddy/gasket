5c947ae6111d1738a90ef5bd53e4190abfca2f9f
Michael Mason <mmason2@godaddy.com>
2024-02-27 10:31:03 -0800

    update export

--- packages/gasket-cli/src/config/default-plugins.js
+++ packages/gasket-cli/src/config/default-plugins.js
@@ -1,4 +1,4 @@
-import pluginCommand from '@gasket/plugin-command';
+import { plugin as pluginCommand } from '@gasket/plugin-command';
 import pluginLifecycle from '@gasket/plugin-lifecycle';
 import pluginMetadata from '@gasket/plugin-metadata';
 import pluginStart from '@gasket/plugin-start';
