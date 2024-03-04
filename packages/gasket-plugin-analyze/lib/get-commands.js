31e8b29306a95acf7195b3224a17fb892f3794b1
Michael Mason <mmason2@godaddy.com>
2024-03-04 11:11:50 -0800

    parse env flag manually, run gasket init & configure globally

--- packages/gasket-plugin-analyze/lib/get-commands.js
+++ packages/gasket-plugin-analyze/lib/get-commands.js
@@ -2,19 +2,16 @@
  * Get the analyze command
  *
  * @param {Gasket} gasket - Gasket
- * @param {Object} GasketCommand - Base Gasket command to extend
  * @returns {GasketCommand} command
  */
-module.exports = function getCommands(gasket, { GasketCommand }) {
-
-  class AnalyzeCommand extends GasketCommand {
-    async gasketRun() {
-      await this.gasket.exec('build');
+module.exports = function getCommands(gasket) {
+  const AnalyzeCommand = {
+    id: 'analyze',
+    description: 'Analyze application code bundles',
+    action: async function () {
+      await gasket.exec('build');
     }
-  }
-
-  AnalyzeCommand.id = 'analyze';
-  AnalyzeCommand.description = 'Analyze application code bundles';
+  };
 
   return AnalyzeCommand;
 };
