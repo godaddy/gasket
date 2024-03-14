7299599bf7f36715abb4dc4f32b55db169def0c6
Michael Mason <mmason2@godaddy.com>
2024-03-14 10:40:37 -0700

    fix logger dep

--- packages/gasket-cli/lib/config/default-plugins.js
+++ packages/gasket-cli/lib/config/default-plugins.js
@@ -2,5 +2,6 @@ module.exports = [
   require('@gasket/plugin-lifecycle'),
   require('@gasket/plugin-metadata'),
   require('@gasket/plugin-start'),
-  require('@gasket/plugin-git')
+  require('@gasket/plugin-git'),
+  require('@gasket/plugin-logger')
 ];
