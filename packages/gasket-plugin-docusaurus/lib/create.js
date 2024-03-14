4197767bf477e7009dc97b2f79d1f682aac7d0dd
Michael Mason <mmason2@godaddy.com>
2024-03-14 12:11:43 -0700

    linter

--- packages/gasket-plugin-docusaurus/lib/create.js
+++ packages/gasket-plugin-docusaurus/lib/create.js
@@ -4,6 +4,6 @@ module.exports = async function create(gasket, { pkg }) {
   pkg.add('devDependencies', {
     '@docusaurus/core': devDependencies['@docusaurus/core'],
     '@docusaurus/preset-classic': devDependencies['@docusaurus/preset-classic'],
-    'react': devDependencies['react'],
+    'react': devDependencies.react
   });
 };
