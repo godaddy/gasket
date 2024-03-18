f79a1480c1bb0be906b1e9d7293bb237f51ff994
Michael Mason <mmason2@godaddy.com>
2024-03-18 14:00:43 -0700

    update test

--- packages/gasket-plugin-metrics/test/get-command-options.test.js
+++ packages/gasket-plugin-metrics/test/get-command-options.test.js
@@ -10,13 +10,11 @@ describe('getCommandOptions', () => {
 
   it('has expected options', async () => {
     const results = await getCommandOptionsHook();
-    const expected = [
-      {
-        name: 'record',
-        description: `${name}: Whether or not to emit this command as part of Gasket's metrics lifecycle`,
-        default: true
-      }
-    ];
+    const expected = {
+      name: 'record',
+      description: `${name}: Whether or not to emit this command as part of Gasket's metrics lifecycle`,
+      default: true
+    };
     expect(results).toEqual(expected);
   });
 });
