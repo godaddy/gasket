302d5d613cab31fc804755f7ac64371c22b273fa
Kawika Bader <ekbader@gmail.com>
2024-03-29 09:49:51 -0700

    Add types for cypress

--- packages/gasket-plugin-cypress/generator/cypress.config.js
+++ packages/gasket-plugin-cypress/generator/cypress.config.js
@@ -1,8 +1,9 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   video: false,
   e2e: {
+    // eslint-disable-next-line no-unused-vars
     setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:8080',
     supportFile: false,
