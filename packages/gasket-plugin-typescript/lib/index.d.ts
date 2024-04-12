e09ade56ae69c273c9ce4d5156190d0db59b581a
Michael Mason <mmason2@godaddy.com>
2024-04-12 11:12:01 -0700

    move types around, add new test file

--- packages/gasket-plugin-typescript/lib/index.d.ts
+++ packages/gasket-plugin-typescript/lib/index.d.ts
@@ -1,5 +1,6 @@
+import { gasket } from 'gasket.ts';
 import { MaybeAsync } from '@gasket/engine';
-import { CreateContext } from '@gasket/cli';
+import { CreateContext } from 'create-gasket-app';
 
 declare module '@gasket/engine' {
   export interface HookExecTypes {
