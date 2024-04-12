a096c18983edcfbb42567d94c434095cfcaaf838
Michael Mason <mmason2@godaddy.com>
2024-04-12 07:51:12 -0700

    tune the server.ts file

--- packages/gasket-plugin-typescript/generator/server.ts
+++ packages/gasket-plugin-typescript/generator/server.ts
@@ -1,8 +1,3 @@
 import gasket from 'gasket.ts';
 
-async function main() : Promise<void> {
-  await gasket.actions.createLogger();
-  await gasket.actions.startServer();
-}
-
-main();
+void gasket.actions.startServer();
