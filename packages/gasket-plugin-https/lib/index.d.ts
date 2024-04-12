7744a02fe0ae50dacfa4b1fdd98bc8b1d2b94396
Michael Mason <mmason2@godaddy.com>
2024-04-12 12:23:27 -0700

    merge

--- packages/gasket-plugin-https/lib/index.d.ts
+++ packages/gasket-plugin-https/lib/index.d.ts
@@ -81,11 +81,7 @@ declare module '@gasket/engine' {
     buffer?: Buffer;
   }
 
-<<<<<<< HEAD
-  type DevProxConfig = RequireAtLeastOne<BaseDevProxyConfig, 'target' | 'forward'>;
-=======
   type DevProxyConfig = RequireAtLeastOne<BaseDevProxyConfig, 'target' | 'forward'>;
->>>>>>> a062bb106b00616227438aa71c5136e9f13074fa
 
   interface ServerOptions {
     hostname?: string,
@@ -100,11 +96,7 @@ declare module '@gasket/engine' {
 
   export interface GasketConfig extends ServerOptions {
     terminus?: TerminusOptions
-<<<<<<< HEAD
-    devProxy?: DevProxConfig
-=======
     devProxy?: DevProxyConfig
->>>>>>> a062bb106b00616227438aa71c5136e9f13074fa
   }
 
   type CreatedServers = {
