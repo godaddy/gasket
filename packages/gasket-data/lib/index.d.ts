f41fe75aa6bbc8df3d37e5c22ae7d238b6f05c52
Ethan Gilchrist <egilchrist@godaddy.com>
2024-04-22 15:50:12 -0400

    Update GasketData type
    Properly define gasket data to be an object containing a config key which contains string or number keys that are potentially undefined.

--- packages/gasket-data/lib/index.d.ts
+++ packages/gasket-data/lib/index.d.ts
@@ -1,5 +1,7 @@
 declare module '@gasket/data' {
-    export interface GasketData {}
+    export interface GasketData {
+      config: { [key: string | number]: string | undefined };
+    }
 
     const gasketData: GasketData;
     export default gasketData;
