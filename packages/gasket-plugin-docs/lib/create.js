d1e94b4491391540fcaf713fc430824f19820049
Michael Mason <mmason2@godaddy.com>
2024-04-26 15:54:56 -0700

    @gasket/plugin-docs: adjust create hook to itself as dep, add plugin to the gasket config

--- packages/gasket-plugin-docs/lib/create.js
+++ packages/gasket-plugin-docs/lib/create.js
@@ -1,5 +1,10 @@
+const { name, version } = require('../package.json');
 const { defaultConfig } = require('./configure');
 
-module.exports = function create(_gasket, { gitignore }) {
+module.exports = function create(_gasket, { pkg, gasketConfig, gitignore }) {
   gitignore?.add(defaultConfig.outputDir, 'Documentation');
+  gasketConfig.addPlugin('pluginDocs', name);
+  pkg.add('dependencies', {
+    [name]: version
+  });
 };
