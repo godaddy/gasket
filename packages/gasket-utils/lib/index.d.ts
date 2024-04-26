0132a1e8519a9c7a0a6b5bb31eca8ed9d5c20f7e
Kawika Bader <ekbader@gmail.com>
2024-04-26 14:44:50 -0700

    Add typechecking for gasket-utils

--- packages/gasket-utils/lib/index.d.ts
+++ packages/gasket-utils/lib/index.d.ts
@@ -1,12 +1,30 @@
 import type { GasketConfig } from '@gasket/engine';
 
+interface PackageManagerOptions {
+  /** Name of manager, either `npm` (default) or `yarn` */
+  packageManager: string;
+  /** Target directory where `node_module` should exist */
+  dest: string;
+  /** @deprecated Path to userconfig */
+  npmconfig?: string;
+}
+
 /**
  * Wrapper class for executing commands for a given package manager
  */
 export interface PackageManager {
+  constructor(options: PackageManagerOptions): void;
+
+  /** Name of manager, either `npm` (default) or `yarn` */
+  manager: string;
+  /** Target directory where `node_module` should exist */
+  dest: string;
+  /** @deprecated Path to userconfig */
+  npmconfig: string;
+
   /**
-   * Executes npm in the application directory `this.dest`.
-   * This installation can be run multiple times.
+   * Executes npm in the application directory `this.dest`. This installation
+   * can be run multiple times.
    *
    * @param cmd The command that needs to be executed.
    * @param args Additional CLI arguments to pass to `npm`.
@@ -21,13 +39,13 @@ export interface PackageManager {
   link(packages?: Array<string>): Promise<void>;
 
   /**
-   * Executes npm install in the application directory `this.dest`.
-   * This installation can be run multiple times.
+   * Executes npm install in the application directory `this.dest`. This
+   * installation can be run multiple times.
    *
    * @param args Additional CLI arguments to pass to `npm`.
    * @public
    */
-  install(args?: Array<string>): Promise<void>
+  install(args?: Array<string>): Promise<void>;
 
   /**
    * Executes yarn or npm info, and returns parsed JSON data results.
@@ -36,29 +54,47 @@ export interface PackageManager {
    * @returns stdout and data
    * @public
    */
-  info(args?: Array<string>): Promise<{ data: any, stdout: string }>;
+  info(args?: Array<string>): Promise<{ data: any; stdout: string }>;
 }
 
-export function tryRequire(path: string): object|null;
+/**
+ * Tries to require a module, but ignores if it is not found. If not found,
+ * result will be null.
+ * @example
+ * const { tryRequire } = require('@gasket/utils');
+ *
+ *  let someConfig = tryRequire('../might/be/a/path/to/some/file');
+ *
+ *  if(!someConfig) {
+ *   someConfig = require('./default-config')
+ * }
+ */
+export function tryRequire(path: string): object | null;
+
+interface ConfigContext {
+  /** Name of environment */
+  env: string;
+  /** Name of command */
+  commandId?: string;
+  /** Project root; required if using localeFile */
+  root?: string;
+  /** Optional file to load relative to gasket root */
+  localFile?: string;
+}
 
 /**
- * Normalize the config by applying any overrides for environments, commands,
- * or local-only config file.
- *
- * @param config - Target config to be normalized
- * @param context - Context for applying overrides
- * @param context.env - Name of environment
- * @param [context.commandId] - Name of command
- * @param [context.root] - Project root; required if using localeFile
- * @param [context.localFile] - Optional file to load relative to gasket root
- * @returns config
+ * Normalize the config by applying any overrides for environments, commands, or local-only config file.
  */
-export function applyConfigOverrides(config: GasketConfig, { env, commandId, root, localFile }: {
-  env: string;
-  commandId?: string;
-  root?: string;
-  localFile?: string;
-}): GasketConfig;
+export function applyConfigOverrides(
+  config: GasketConfig,
+  configContext: ConfigContext
+): GasketConfig;
+
+export function getPotentialConfigs(
+  config: ConfigContext & {
+    config: GasketConfig;
+  }
+): string[];
 
 /**
  * Normalize the config by applying any environment or local overrides
@@ -69,15 +105,24 @@ export function applyConfigOverrides(config: GasketConfig, { env, commandId, roo
  * @returns config
  * @deprecated use applyConfigOverrides
  */
-declare function applyEnvironmentOverrides(gasketConfig: GasketConfig, config: GasketConfig, localFile?: string): object;
+declare function applyEnvironmentOverrides(
+  gasketConfig: GasketConfig,
+  config: GasketConfig,
+  localFile?: string
+): object;
+
+export interface Signal {
+  aborted?: boolean;
+  addEventListener(type: 'abort', listener: () => void): void;
+}
 
 /**
- * Promise friendly wrapper to running a shell command (eg: git, npm, ls)
- * which passes back any { stdout, stderr } to the error thrown.
+ * Promise friendly wrapper to running a shell command (eg: git, npm, ls) which
+ * passes back any { stdout, stderr } to the error thrown.
  *
  * Options can be passed to the underlying spawn. An additional `signal` option
- * can be passed to use AbortController, allowing processes to be killed when
- * no longer needed.
+ * can be passed to use AbortController, allowing processes to be killed when no
+ * longer needed.
  *
  * @example
  * const { runShellCommand } = require('@gasket/utils');
@@ -99,14 +144,45 @@ declare function applyEnvironmentOverrides(gasketConfig: GasketConfig, config: G
  *   await runShellCommand('long-process', ['something'], { signal: controller.signal });
  *   clearTimeout(id);
  * }
- *
- * @param cmd - Binary that is run
- * @param [argv] - Arguments passed to npm binary through spawn.
- * @param [options] options passed to npm binary through spawn
- * @param [options.signal] AbortControl signal allowing process to be canceled
- * @param [debug] When present pipes std{out,err} to process.*
- * @returns results
  */
-export function runShellCommand(cmd: string, argv?: string[], options?: {
-  signal?: object;
-}, debug?: boolean): Promise<{ stdout: string }>;
+export function runShellCommand(
+  /** Binary that is run */
+  cmd: string,
+  /** Arguments passed to npm binary through spawn. */
+  argv?: string[],
+  /** Options passed to npm binary through spawn */
+  options?: {
+    /** AbortControl signal allowing process to be canceled */
+    signal?: Signal;
+    /** Path to the target app (Default: cwd/appName) */
+    cwd?: string;
+  },
+  /** When present pipes std{out,err} to process.* */
+  debug?: boolean
+): Promise<{ stdout: string }>;
+
+export interface PkgManager {
+  pkgManager: string;
+  cmd: string;
+  flags: string[];
+  logMsg: (msg: string) => string;
+}
+
+export interface TargetConfig {
+  environments: string;
+}
+
+export interface environments {
+  dev;
+}
+
+export interface ConfigContext {
+  /** Name of environment */
+  env: string;
+  /** Name of command */
+  commandId?: string;
+  /** Project root; required if using localeFile */
+  root?: string;
+  /** Optional file to load relative to gasket root */
+  localFile?: string;
+}
