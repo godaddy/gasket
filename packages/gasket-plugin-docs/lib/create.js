cca223746a166bd954caa78888dd0d522dbd23f6
Michael Mason <mmason2@godaddy.com>
2024-04-29 16:14:37 -0700

    @gasket/plugin-docs,@gasket/plugin-docusaurus - opt for preset to add plugin dependency

--- packages/gasket-plugin-docs/lib/create.js
+++ packages/gasket-plugin-docs/lib/create.js
@@ -1,10 +1,7 @@
-const { name, version } = require('../package.json');
+const { name } = require('../package.json');
 const { defaultConfig } = require('./configure');
 
 module.exports = function create(_gasket, { pkg, gasketConfig, gitignore }) {
   gitignore?.add(defaultConfig.outputDir, 'Documentation');
   gasketConfig.addPlugin('pluginDocs', name);
-  pkg.add('dependencies', {
-    [name]: version
-  });
 };
