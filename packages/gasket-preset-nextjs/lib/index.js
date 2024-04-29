156cc5687dc568db9b1535c8e1f3bd5d90ec9970
Michael Mason <mmason2@godaddy.com>
2024-04-29 16:21:09 -0700

    @gasket/preset-nextjs: type module, update main entry, create index preset file

--- packages/gasket-preset-nextjs/lib/index.js
+++ packages/gasket-preset-nextjs/lib/index.js
@@ -1,3 +1,31 @@
-module.exports = {
-  require
-};
+import pluginExpress from '@gasket/plugin-express';
+import pluginHttps from '@gasket/plugin-https';
+import pluginNext from '@gasket/plugin-nextjs';
+import pluginRedux from '@gasket/plugin-redux';
+import pluginWebpack from '@gasket/plugin-webpack';
+import pluginWinston from '@gasket/plugin-winston';
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const { name, dependencies } = require('../package.json');
+
+export default {
+  name,
+  hooks: {
+    async presetConfig(gasket, context) {
+      context.nextApp = true;
+      return {
+        plugins: [
+          pluginWebpack,
+          pluginExpress,
+          pluginHttps,
+          pluginNext,
+          pluginRedux,
+          pluginWinston
+        ]
+      }
+    },
+    async create(gasket, { pkg }) {
+      pkg.add('dependencies', dependencies);
+    }
+  }
+}
