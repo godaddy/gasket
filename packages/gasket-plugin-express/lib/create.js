c6b56c8e579ce8bc704732d3c9f00e3c0fc9bece
Michael Mason <mmason2@godaddy.com>
2024-04-29 10:45:22 -0700

    @gasket/plugin-express: adjust imports to be named

--- packages/gasket-plugin-express/lib/create.js
+++ packages/gasket-plugin-express/lib/create.js
@@ -26,12 +26,12 @@ module.exports = async function create(gasket, context) {
   if ('typescript' in context && context.typescript) {
     context.files.add(`${generatorDir}/**/!(*.js)`);
     context.gasketConfig
-      .addImport('routes', './routes')
+      .addImport('{ routes }', './routes')
       .injectValue('express.routes', 'routes');
   } else {
     context.files.add(`${generatorDir}/**/!(*.ts)`);
     context.gasketConfig
-      .addImport('routes', './routes/index.js')
+      .addImport('{ routes }', './routes/index.js')
       .injectValue('express.routes', 'routes');
   }
 };
