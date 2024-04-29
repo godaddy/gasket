662e25aa9e6c327084a8103513c2a420a1131a2f
Michael Mason <mmason2@godaddy.com>
2024-04-29 10:29:15 -0700

    remove @gasket/plugin-metadata from default plugins

--- packages/create-gasket-app/lib/config/default-plugins.js
+++ packages/create-gasket-app/lib/config/default-plugins.js
@@ -1,9 +1,7 @@
-import pluginMetadata from '@gasket/plugin-metadata';
 import pluginGit from '@gasket/plugin-git';
 import pluginLogger from '@gasket/plugin-logger';
 
 export const defaultPlugins = [
   pluginLogger,
-  pluginMetadata,
   pluginGit
 ];
