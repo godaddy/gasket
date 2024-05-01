6566c9ef51de7c86fbbbff01fe3d127bb0a5a4f2
Michael Mason <mmason2@godaddy.com>
2024-05-01 16:58:51 -0700

    @gasket/preset-nextjs: add tests, refactor, update package file

--- packages/gasket-preset-nextjs/lib/index.js
+++ packages/gasket-preset-nextjs/lib/index.js
@@ -1,55 +1,16 @@
-import pluginExpress from '@gasket/plugin-express';
-import pluginHttps from '@gasket/plugin-https';
-import pluginNext from '@gasket/plugin-nextjs';
-import pluginRedux from '@gasket/plugin-redux';
-import pluginWebpack from '@gasket/plugin-webpack';
-import pluginWinston from '@gasket/plugin-winston';
 import { createRequire } from 'module';
+import presetPrompt from './preset-prompt.js';
+import presetConfig from './preset-config.js';
 const require = createRequire(import.meta.url);
 const { name, dependencies } = require('../package.json');
 
 export default {
   name,
   hooks: {
-    async presetPrompt(gasket, context, { prompt }) {
-      if (!('typescript' in context)) {
-        const { typescript } = await prompt([
-          {
-            name: 'typescript',
-            message: 'Do you want to use TypeScript?',
-            type: 'confirm',
-            default: false
-          }
-        ]);
-
-        Object.assign(context, { typescript });
-      }
-    },
-    async presetConfig(gasket, context) {
-      let testPlugin, typescriptPlugin;
-
-      if ('testPlugin' in context) {
-        testPlugin = await import(context.testPlugin);
-      }
-
-      if (context.typescript) {
-        typescriptPlugin = await import('@gasket/plugin-typescript');
-      }
-
-      return {
-        plugins: [
-          pluginWebpack,
-          pluginExpress,
-          pluginHttps,
-          pluginNext,
-          pluginRedux,
-          pluginWinston,
-          typescriptPlugin ? typescriptPlugin.default || typescriptPlugin : null,
-        ].filter(Boolean)
-      }
-    },
+    presetPrompt,
+    presetConfig,
     async create(gasket, { pkg }) {
       pkg.add('dependencies', dependencies);
     }
   }
-}
+};
