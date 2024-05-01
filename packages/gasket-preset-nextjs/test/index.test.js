dd8bca8f80d914ab9d3fa65e42243373ce629da2
Michael Mason <mmason2@godaddy.com>
2024-05-01 16:58:51 -0700

    @gasket/preset-nextjs: add tests, refactor, update package file

---
+++ packages/gasket-preset-nextjs/test/index.test.js
@@ -0,0 +1,23 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+import preset from '../lib/index.js';
+
+describe('gasket-preset-nextjs', () => {
+
+  it('is an object', () => {
+    expect(typeof preset).toBe('object');
+  });
+
+  it('has the expected name', () => {
+    expect(preset).toHaveProperty('name', require('../package').name);
+  });
+
+  it('has expected hooks', () => {
+    const expected = [
+      'presetPrompt',
+      'presetConfig',
+      'create'
+    ];
+    expect(Object.keys(preset.hooks)).toEqual(expect.arrayContaining(expected));
+  });
+});
