eeec9a4b1103189d254d0102db5edd71a285fa58
Michael Mason <mmason2@godaddy.com>
2024-05-02 12:37:43 -0700

    @gasket/preset-api: remove pkg.add deps from create hook

--- packages/gasket-preset-api/lib/create.js
+++ packages/gasket-preset-api/lib/create.js
@@ -8,12 +8,12 @@ export default function create(gasket, context) {
   const __dirname = fileURLToPath(import.meta.url);
   const generatorDir = `${__dirname}/../../generator`;
 
-  pkg.add('dependencies', dependencies);
-
   if (!context.typescript) {
     files.add(`${generatorDir}/*`);
 
-    pkg.add('devDependencies', devDependencies);
+    pkg.add('devDependencies', {
+      'nodemon': devDependencies.nodemon,
+    });
 
     pkg.add('scripts', {
       start: 'node server.js',
