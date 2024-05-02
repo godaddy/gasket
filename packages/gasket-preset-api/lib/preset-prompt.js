75b7eca8844575e77703cf29711258bb687c28d7
Michael Mason <mmason2@godaddy.com>
2024-05-01 17:15:26 -0700

    remove TODOs

--- packages/gasket-preset-api/lib/preset-prompt.js
+++ packages/gasket-preset-api/lib/preset-prompt.js
@@ -1,7 +1,6 @@
 export default async function presetPrompt(gasket, context, { prompt }) {
   context.apiApp = true;
 
-  // TODOfollowup - TS prompt could possibly be global
   if (!('typescript' in context)) {
     const { typescript } = await prompt([
       {
