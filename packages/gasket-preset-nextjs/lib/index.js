892ed514f19a337570fe19b6886e96446b676950
Michael Mason <mmason2@godaddy.com>
2024-05-02 12:38:17 -0700

    @gasket/preset-nextjs: remove pkg.add deps from create hook, adjust deps

--- packages/gasket-preset-nextjs/lib/index.js
+++ packages/gasket-preset-nextjs/lib/index.js
@@ -2,15 +2,12 @@ import { createRequire } from 'module';
 import presetPrompt from './preset-prompt.js';
 import presetConfig from './preset-config.js';
 const require = createRequire(import.meta.url);
-const { name, dependencies } = require('../package.json');
+const { name } = require('../package.json');
 
 export default {
   name,
   hooks: {
     presetPrompt,
-    presetConfig,
-    async create(gasket, { pkg }) {
-      pkg.add('dependencies', dependencies);
-    }
+    presetConfig
   }
 };
