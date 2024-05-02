946a624c8afe9e21e74a0c58798f94104fed3f31
Michael Mason <mmason2@godaddy.com>
2024-05-02 15:30:59 -0700

    @gasket/preset-api: add generator dir to the file array in the package file

--- packages/gasket-preset-api/lib/create.js
+++ packages/gasket-preset-api/lib/create.js
@@ -9,22 +9,9 @@ export default function create(gasket, context) {
   const __dirname = fileURLToPath(import.meta.url);
   const generatorDir = path.join(__dirname, '..', '..', 'generator');
 
-  console.log('-------------------------');
-  console.log('__dirname', __dirname);
-  console.log('-------------------------');
-
-  console.log('-------------------------');
-  console.log('generatorDir', generatorDir);
-  console.log('-------------------------');
-
-  console.log('-------------------------');
-  console.log('context.typescript', context.typescript);
-  console.log('-------------------------');
   if (!context.typescript) {
     files.add(`${generatorDir}/*`);
-    console.log('-------------------------');
-    console.log('files', files);
-    console.log('-------------------------');
+
     pkg.add('devDependencies', {
       nodemon: devDependencies.nodemon
     });
