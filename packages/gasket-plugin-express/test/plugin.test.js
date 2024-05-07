cc074bca1fe34e6b5aaa2d8144a37b1761b119ba
jpina1 <jpina1@godaddy.com>
2024-05-06 20:23:22 -0700

    chore: added express, fastify

--- packages/gasket-plugin-express/test/plugin.test.js
+++ packages/gasket-plugin-express/test/plugin.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsdoc/require-jsdoc */
 const { setTimeout } = require('timers/promises');
-const GasketEngine = require('@gasket/engine');
+const { GasketEngine } = require('@gasket/core');
 const app = { use: jest.fn(), post: jest.fn(), set: jest.fn() };
 const mockExpress = jest.fn().mockReturnValue(app);
 const bridgedApp = { use: jest.fn() };
