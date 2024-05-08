ffa86eccdc12c5372c04ba649c79776fbe04e5cf
Kawika Bader <ekbader@gmail.com>
2024-05-08 15:59:02 -0700

    Reset service worker plugin to main branch.

--- packages/gasket-plugin-workbox/lib/compose-service-worker.js
+++ packages/gasket-plugin-workbox/lib/compose-service-worker.js
@@ -1,5 +1,3 @@
-/// <reference types="@gasket/plugin-service-worker" />
-
 const { generateSWString } = require('workbox-build');
 const merge = require('deepmerge');
 
@@ -8,20 +6,23 @@ const reComments = /\/\*.*(\n.+)*\*\//g;
 /**
  * Gathers Workbox config by executing the `workbox` lifecycle.
  * Generates service worker strings and appends to the service worker content.
- * @type {import('@gasket/engine').HookHandler<'composeServiceWorker'>}
+ *
+ * @param {Gasket} gasket - Gasket
+ * @param {String} content - Service worker content
+ * @param {Object} context - Service worker context
+ * @returns {Promise<string>} content
  */
 module.exports = async function composeServiceWorker(gasket, content, context) {
   const { exec, config, logger } = gasket;
   const { workbox } = config;
 
-  const configs = (await exec('workbox', workbox.config, context)).filter(
-    Boolean
-  );
+  const configs = (await exec('workbox', workbox.config, context)).filter(Boolean);
   const mergedConfig = merge.all([workbox.config, ...configs]);
 
   const build = await generateSWString(mergedConfig);
-
+  //
   // strip comments from workbox script
+  //
   const workboxContent = build.swString.replace(reComments, '');
 
   if (build.warnings && build.warnings.length) {
