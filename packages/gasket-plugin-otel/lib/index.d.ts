b5ae91ccdeb5200c2f76f60ce60863402ce37aff
Michael Mason <mmason2@godaddy.com>
2024-05-08 13:14:45 -0700

    @gasket/plugin-otel: add type def

---
+++ packages/gasket-plugin-otel/lib/index.d.ts
@@ -0,0 +1,8 @@
+import { MaybeAsync } from '@gasket/engine';
+import { CreateContext } from 'create-gasket-app';
+
+declare module '@gasket/engine' {
+  export interface HookExecTypes {
+    create(context: CreateContext): MaybeAsync<void>;
+  }
+}
