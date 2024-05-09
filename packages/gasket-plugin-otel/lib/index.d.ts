c51e91dedf9d9acf748afc995237859c953af5c6
Michael Mason <mmason2@godaddy.com>
2024-05-09 07:48:32 -0700

    add typecheck

--- packages/gasket-plugin-otel/lib/index.d.ts
+++ packages/gasket-plugin-otel/lib/index.d.ts
@@ -1,6 +1,10 @@
 import { MaybeAsync } from '@gasket/engine';
 import { CreateContext } from 'create-gasket-app';
 
+export interface ProcessEnv {
+  [key: string]: string | undefined
+}
+
 declare module '@gasket/engine' {
   export interface HookExecTypes {
     create(context: CreateContext): MaybeAsync<void>;
