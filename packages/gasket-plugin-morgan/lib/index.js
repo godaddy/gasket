41a0fdac21eac91a4ce17c4a8d9245bddcf5e61e
Andrew Gerard <agerard@godaddy.com>
2024-05-10 13:56:30 -0700

    refactor: do not need hook handler w/o timing

--- packages/gasket-plugin-morgan/lib/index.js
+++ packages/gasket-plugin-morgan/lib/index.js
@@ -13,17 +13,15 @@ const { name } = require('../package.json');
 const plugin = {
   name,
   hooks: {
-    middleware: {
-      handler: (gasket) => {
-        const { logger, config } = gasket;
-        const { morgan: { format = 'tiny', options = {} } = {} } = config;
+    middleware(gasket) {
+      const { logger, config } = gasket;
+      const { morgan: { format = 'tiny', options = {} } = {} } = config;
 
-        const stream = split().on('data', (line) => logger.info(line));
+      const stream = split().on('data', (line) => logger.info(line));
 
-        const morganMiddleware = morgan(format, { ...options, stream });
+      const morganMiddleware = morgan(format, { ...options, stream });
 
-        return [morganMiddleware];
-      }
+      return [morganMiddleware];
     },
     metadata(gasket, meta) {
       return {
