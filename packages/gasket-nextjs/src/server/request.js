930f368d89fb073c6ea0ca666e2bbb2a7df2df19
Andrew Gerard <63810935+agerard-godaddy@users.noreply.github.com>
2024-05-10 12:16:02 -0700

    Next request for server components (#745)
    * fix: lint
    
    * feat: add new request function for server components
    
    * docs: add new request function for server components
    
    * feat: expose /server
    
    * fix: update jsdocs
    
    * fix: cleanup

---
+++ packages/gasket-nextjs/src/server/request.js
@@ -0,0 +1,33 @@
+import { cookies, headers } from 'next/headers';
+
+const reqCache = new Map();
+
+/**
+ * Get a request-like object unique to the current request in server components.
+ * @param {Object} [query] - Optional query parameters to include in the request
+ * @returns {Request} - Request-like object
+ */
+export function request(query) {
+  const headerStore = headers();
+
+  if (!reqCache.has(headerStore)) {
+    const cookieStore = cookies();
+
+    const req = {
+      cookies: cookieStore.getAll()
+        .reduce((acc, { name, value }) => {
+          acc[name] = value;
+          return acc;
+        }, {}),
+      headers: Object.fromEntries(headerStore.entries())
+    };
+
+    if (query) {
+      req.query = query;
+    }
+
+    reqCache.set(headerStore, req);
+  }
+
+  return reqCache.get(headerStore);
+}
