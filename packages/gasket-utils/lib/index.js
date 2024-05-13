b7b04cb06d3afa93698244853e22c0af9ed95669
Andrew Gerard <63810935+agerard-godaddy@users.noreply.github.com>
2024-05-13 14:25:02 -0700

    Remove requireWithInstall (#748)
    * chore: remove requireWithInstall
    
    * docs: remove requireWithInstall

--- packages/gasket-utils/lib/index.js
+++ packages/gasket-utils/lib/index.js
@@ -3,13 +3,11 @@ const { tryResolve } = require('./try-resolve');
 const applyConfigOverrides = require('./apply-config-overrides');
 const runShellCommand = require('./run-shell-command');
 const PackageManager = require('./package-manager');
-const requireWithInstall = require('./require-with-install');
 
 module.exports = {
   tryRequire,
   tryResolve,
   applyConfigOverrides,
   runShellCommand,
-  PackageManager,
-  requireWithInstall
+  PackageManager
 };
