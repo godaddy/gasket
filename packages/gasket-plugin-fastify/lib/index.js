52cea82648ba5e798af88d4bae3500fe42de1e1b
Michael Mason <mmason2@godaddy.com>
2024-05-20 08:11:50 -0700

    add dependencies, update type references

--- packages/gasket-plugin-fastify/lib/index.js
+++ packages/gasket-plugin-fastify/lib/index.js
@@ -1,4 +1,4 @@
-/// <reference types="@gasket/cli" />
+/// <reference types="@gasket/core" />
 /// <reference types="@gasket/plugin-metadata" />
 
 const { peerDependencies, name } = require('../package.json');
