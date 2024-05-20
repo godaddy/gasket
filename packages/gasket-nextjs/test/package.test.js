157b7c6feb2c301506e1014fb08cca575d02bb14
Andrew Gerard <agerard@godaddy.com>
2024-05-20 09:49:22 -0700

    fix: exports for server files

---
+++ packages/gasket-nextjs/test/package.test.js
@@ -0,0 +1,14 @@
+import packageJson from '../package.json';
+
+describe('package.json', function () {
+
+  it('has expected exports', () => {
+    const expected = [
+      '.',
+      './server'
+    ];
+
+    expect(Object.keys(packageJson.exports)).toEqual(expected);
+    expect(Object.keys(packageJson.exports)).toHaveLength(expected.length);
+  });
+});
