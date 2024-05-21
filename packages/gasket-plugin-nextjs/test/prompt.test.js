7f73fe3736e19e92c27233f9b57b4a4224f1a457
jpina1 <jpina1@godaddy.com>
2024-05-21 11:08:43 -0700

    feat: added useRedux prompt in next plugin

--- packages/gasket-plugin-nextjs/test/prompt.test.js
+++ packages/gasket-plugin-nextjs/test/prompt.test.js
@@ -6,7 +6,7 @@ describe('prompt hook', () => {
   beforeEach(() => {
     gasket = {};
     context = {};
-    mockAnswers = { addSitemap: true, nextServerType: 'next-cli' };
+    mockAnswers = { addSitemap: true, useRedux: true,  nextServerType: 'next-cli' };
     prompt = jest.fn().mockImplementation(() => mockAnswers);
   });
 
@@ -19,12 +19,16 @@ describe('prompt hook', () => {
     await promptHook(gasket, context, { prompt });
     const first = prompt.mock.calls[0][0][0];
     const second = prompt.mock.calls[1][0][0];
+    const thrid = prompt.mock.calls[2][0][0];
     expect(first.name).toEqual('nextServerType');
     expect(first.message).toEqual('Which server type would you like to use?');
     expect(first.type).toEqual('list');
     expect(second.name).toEqual('addSitemap');
     expect(second.message).toEqual('Do you want to add a sitemap?');
     expect(second.type).toEqual('confirm');
+    expect(thrid.name).toEqual('useRedux');
+    expect(thrid.message).toEqual('Do you want to use Redux?');
+    expect(thrid.type).toEqual('confirm');
   });
 
   it('sets nextServerType to next-cli', async () => {
@@ -49,11 +53,24 @@ describe('prompt hook', () => {
     expect(result.addSitemap).toEqual(false);
   });
 
+  it('sets useRedux to true', async () => {
+    const result = await promptHook(gasket, context, { prompt });
+    expect(result.useRedux).toEqual(true);
+  });
+
+  it('sets useRedux to false', async () => {
+    mockAnswers = { useRedux: false };
+    const result = await promptHook(gasket, context, { prompt });
+    expect(result.useRedux).toEqual(false);
+  });
+
   it('does not run prompt if prompts are in context', async () => {
     context.addSitemap = false;
     context.nextServerType = false;
+    context.useRedux = false;
     const result = await promptHook(gasket, context, { prompt });
     expect(result).toHaveProperty('addSitemap', false);
+    expect(result).toHaveProperty('useRedux', false);
     expect(prompt).not.toHaveBeenCalled();
   });
 });
--- packages/gasket-plugin-redux/lib/create.js
+++ packages/gasket-plugin-redux/lib/create.js
@@ -4,6 +4,8 @@ const { devDependencies } = require('../package.json');
 
 /** @type {import('@gasket/core').HookHandler<'create'>} */
 module.exports = async function create(gasket, context) {
+  if (Object.keys(context).includes('useRedux') && !context.useRedux) return;
+
   const { pkg, files } = context;
   const generatorDir = `${__dirname}/../generator`;
 
