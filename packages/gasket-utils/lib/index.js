06c0025b80547e61b28f187c58fff25908c974ec
Michael Mason <105235096+mmason2-godaddy@users.noreply.github.com>
2024-05-23 16:38:00 -0700

    [PFX-611] - Remove tryRequire (#765)
    * @gasket/plugin-docusaurus: remove tryRequire, update tests
    
    * @gasket/plugin-response-data: remove tryRequire
    
    * @gasket/plugin-response-data: remove tryRequire
    
    * gasket-typescript-tests: remove tryRequire test
    
    * @gasket/utils: remove tryRequire
    
    * @gasket/utils: remove tryResolve

--- packages/gasket-utils/lib/index.js
+++ packages/gasket-utils/lib/index.js
@@ -1,13 +1,9 @@
-const tryRequire = require('./try-require');
-const { tryResolve } = require('./try-resolve');
 const applyConfigOverrides = require('./apply-config-overrides');
 const runShellCommand = require('./run-shell-command');
 const PackageManager = require('./package-manager');
 const warnIfOutdated = require('./warn-if-outdated');
 
 module.exports = {
-  tryRequire,
-  tryResolve,
   applyConfigOverrides,
   runShellCommand,
   PackageManager,
