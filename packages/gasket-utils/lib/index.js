03c29ae524759108380567fb55d1b6b62530d7e3
Jordan Pina <106619535+jpina1-godaddy@users.noreply.github.com>
2024-05-24 11:43:19 -0700

    [PFX-598] Add warn-if-outdated support for @ux packages (#768)
    * chore: moved get version to its own function
    
    * chore: updaed changelog

--- packages/gasket-utils/lib/index.js
+++ packages/gasket-utils/lib/index.js
@@ -2,10 +2,12 @@ const applyConfigOverrides = require('./apply-config-overrides');
 const runShellCommand = require('./run-shell-command');
 const PackageManager = require('./package-manager');
 const warnIfOutdated = require('./warn-if-outdated');
+const getPackageLatestVersion = require('./get-package-latest-version');
 
 module.exports = {
   applyConfigOverrides,
   runShellCommand,
   PackageManager,
-  warnIfOutdated
+  warnIfOutdated,
+  getPackageLatestVersion
 };
