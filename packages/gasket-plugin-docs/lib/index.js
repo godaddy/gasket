677add28d3291cf78e718c418ce130e48d10bb34
Michael Mason <mmason2@godaddy.com>
2024-06-04 10:08:39 -0700

    @gasket/plugin-docs: working canary commit

--- packages/gasket-plugin-docs/lib/index.js
+++ packages/gasket-plugin-docs/lib/index.js
@@ -5,11 +5,13 @@ const configure = require('./configure');
 const commands = require('./commands');
 const metadata = require('./metadata');
 const docsSetup = require('./docs-setup');
-const { name } = require('../package.json');
+const { name, version, description } = require('../package.json');
 
 /** @type {import('@gasket/core').Plugin} */
 const plugin = {
   name,
+  version,
+  description,
   hooks: {
     configure,
     create,
