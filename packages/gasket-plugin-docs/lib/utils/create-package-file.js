3a5a7776bd5ac17faa08bb9e1f362c4fcb3f1705
Michael Mason <mmason2@godaddy.com>
2024-06-10 16:10:18 -0700

    move blank package file up one dir

--- packages/gasket-plugin-docs/lib/utils/create-package-file.js
+++ packages/gasket-plugin-docs/lib/utils/create-package-file.js
@@ -3,6 +3,6 @@ const { writeFile } = require('fs').promises;
 // TODO: document this
 module.exports = async function createPackageFile(docsConfigSet) {
   const { docsRoot } = docsConfigSet;
-  const target = path.join(docsRoot, 'package.json');
+  const target = path.join(docsRoot, '..', 'package.json');
   await writeFile(target, JSON.stringify({}), 'utf-8');
 };
