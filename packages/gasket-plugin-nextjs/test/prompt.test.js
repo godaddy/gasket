3aa856ae5c0093391add0668b86c5e9895b0b38a
Jordan Pina <106619535+jpina1-godaddy@users.noreply.github.com>
2024-06-10 13:45:46 -0700

    [PFX-637] Support create App Router (#777)
    * feat: added app router prompt to next plugin
    
    * tests: next plugin
    
    * chore: fix linting errors in next plugin
    
    * chore: reuse index page
    
    * chore: update readme
    
    * chore: removed lang attr on html tag
    
    * chore: added default nextServer outside of approuter prompt

--- packages/gasket-plugin-nextjs/test/prompt.test.js
+++ packages/gasket-plugin-nextjs/test/prompt.test.js
@@ -6,7 +6,7 @@ describe('prompt hook', () => {
   beforeEach(() => {
     gasket = {};
     context = {};
-    mockAnswers = { addSitemap: true, nextServerType: 'next-cli' };
+    mockAnswers = { useAppRouter: false, addSitemap: true, nextServerType: 'next-cli' };
     prompt = jest.fn().mockImplementation(() => mockAnswers);
   });
 
@@ -19,12 +19,16 @@ describe('prompt hook', () => {
     await promptHook(gasket, context, { prompt });
     const first = prompt.mock.calls[0][0][0];
     const second = prompt.mock.calls[1][0][0];
-    expect(first.name).toEqual('nextServerType');
-    expect(first.message).toEqual('Which server type would you like to use?');
-    expect(first.type).toEqual('list');
-    expect(second.name).toEqual('addSitemap');
-    expect(second.message).toEqual('Do you want to add a sitemap?');
-    expect(second.type).toEqual('confirm');
+    const third = prompt.mock.calls[2][0][0];
+    expect(first.name).toEqual('useAppRouter');
+    expect(first.message).toEqual('Do you want to use the App Router? (experimental)');
+    expect(first.type).toEqual('confirm');
+    expect(second.name).toEqual('nextServerType');
+    expect(second.message).toEqual('Which server type would you like to use?');
+    expect(second.type).toEqual('list');
+    expect(third.name).toEqual('addSitemap');
+    expect(third.message).toEqual('Do you want to add a sitemap?');
+    expect(third.type).toEqual('confirm');
   });
 
   it('sets nextServerType to next-cli', async () => {
@@ -49,9 +53,16 @@ describe('prompt hook', () => {
     expect(result.addSitemap).toEqual(false);
   });
 
+  it('sets useAppRouter to true', async () => {
+    mockAnswers = { useAppRouter: true };
+    const result = await promptHook(gasket, context, { prompt });
+    expect(result.useAppRouter).toEqual(true);
+  });
+
   it('does not run prompt if prompts are in context', async () => {
     context.addSitemap = false;
     context.nextServerType = false;
+    context.useAppRouter = false;
     const result = await promptHook(gasket, context, { prompt });
     expect(result).toHaveProperty('addSitemap', false);
     expect(prompt).not.toHaveBeenCalled();
