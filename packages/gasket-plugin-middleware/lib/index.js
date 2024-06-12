b743fbd9b334ab7dc6526ac47cd1f9ff471f820a
Bree Betts <bbetts@godaddy.com>
2024-06-12 14:37:06 -0700

    fix type error in middleware plugin

--- packages/gasket-plugin-middleware/lib/index.js
+++ packages/gasket-plugin-middleware/lib/index.js
@@ -1,10 +1,12 @@
-const { name } = require('../package.json');
+const { name, version, description } = require('../package.json');
 const express = require('./express');
 const fastify = require('./fastify');
 
 /** @type {import('@gasket/core').Plugin} */
 const plugin = {
   name,
+  version,
+  description,
   hooks: {
     express,
     fastify
