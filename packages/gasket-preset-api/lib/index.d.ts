d840eec296479dd13ef864bd5f8e216b97df752f
Michael Mason <mmason2@godaddy.com>
2024-06-14 09:47:10 -0700

    remove type imports

--- packages/gasket-preset-api/lib/index.d.ts
+++ packages/gasket-preset-api/lib/index.d.ts
@@ -2,6 +2,5 @@ import '@gasket/plugin-data';
 import '@gasket/plugin-express';
 import '@gasket/plugin-https';
 import '@gasket/plugin-lint';
-import '@gasket/plugin-start';
 import '@gasket/plugin-swagger';
 import '@gasket/plugin-winston';
