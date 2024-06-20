f65dfc861e8b078d3155373e44ff4401fa718151
Bree Betts <bbetts@godaddy.com>
2024-06-20 13:17:50 -0700

    add tests for presetPrompt

--- packages/gasket-preset-nextjs/test/preset-prompt.test.js
+++ packages/gasket-preset-nextjs/test/preset-prompt.test.js
@@ -20,8 +20,8 @@ describe('presetPrompt', () => {
     mockContext.typescript = true;
     await presetPrompt({}, mockContext, mockPrompt);
     const first = mockPrompt.prompt.mock.calls[0][0][0];
-    expect(mockPrompt.prompt).toHaveBeenCalledTimes(1);
-    expect(first.name).toEqual('server');
+    expect(mockPrompt.prompt).toHaveBeenCalledTimes(4);
+    expect(first.name).toEqual('useAppRouter');
   });
 
   it('prompts for typescript', async () => {
@@ -31,10 +31,39 @@ describe('presetPrompt', () => {
     expect(first.name).toEqual('typescript');
   });
 
+  it('prompts for appRouter', async () => {
+    await presetPrompt({}, mockContext, mockPrompt);
+    const first = mockPrompt.prompt.mock.calls[1][0][0];
+    expect(mockPrompt.prompt).toHaveBeenCalled();
+    expect(first.name).toEqual('useAppRouter');
+  });
+
+  it('prompts for nextServerType', async () => {
+    await presetPrompt({}, mockContext, mockPrompt);
+    const first = mockPrompt.prompt.mock.calls[2][0][0];
+    expect(mockPrompt.prompt).toHaveBeenCalled();
+    expect(first.name).toEqual('nextServerType');
+  });
+
+  it('prompts for nextDevProxy when defaultServer', async () => {
+    mockContext.nextServerType = 'defaultServer';
+    await presetPrompt({}, mockContext, mockPrompt);
+    const first = mockPrompt.prompt.mock.calls[2][0][0];
+    expect(mockPrompt.prompt).toHaveBeenCalled();
+    expect(first.name).toEqual('nextDevProxy');
+  });
+
   it('prompts for server', async () => {
     await presetPrompt({}, mockContext, mockPrompt);
-    const second = mockPrompt.prompt.mock.calls[1][0][0];
-    expect(mockPrompt.prompt).toHaveBeenCalledTimes(2);
+    const second = mockPrompt.prompt.mock.calls[3][0][0];
+    expect(mockPrompt.prompt).toHaveBeenCalledTimes(5);
     expect(second.name).toEqual('server');
+  });
+
+  it('prompts for addSitemap', async () => {
+    await presetPrompt({}, mockContext, mockPrompt);
+    const first = mockPrompt.prompt.mock.calls[4][0][0];
+    expect(mockPrompt.prompt).toHaveBeenCalled();
+    expect(first.name).toEqual('addSitemap');
   });
 });
