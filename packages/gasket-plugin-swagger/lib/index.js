974420b9b10c79d31ad45dfb06ba993c437895a2
Michael Mason <mmason2@godaddy.com>
2024-06-27 11:26:30 -0700

    remove todos to remove build hooks

--- packages/gasket-plugin-swagger/lib/index.js
+++ packages/gasket-plugin-swagger/lib/index.js
@@ -5,6 +5,7 @@
 /// <reference types="@gasket/plugin-metadata" />
 /// <reference types="@gasket/plugin-logger" />
 /// <reference types="@gasket/core" />
+/// <reference types="@gasket/plugin-command" />
 
 
 const path = require('path');
@@ -58,8 +59,6 @@ const plugin = {
       };
       return baseConfig;
     },
-    // @ts-expect-error - TODO: will be cleaned up in tune up ticket
-    // https://godaddy-corp.atlassian.net/browse/PFX-654
     async build(gasket) {
       const { swagger, root } = gasket.config;
       const { jsdoc, definitionFile } = swagger;
