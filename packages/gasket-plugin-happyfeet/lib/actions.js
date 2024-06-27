b6580885eef1ab63fe7e03fd0557b2ad584f8f8d
Bree Betts <bbetts@godaddy.com>
2024-06-27 13:39:06 -0700

    maintain happy instance

--- packages/gasket-plugin-happyfeet/lib/actions.js
+++ packages/gasket-plugin-happyfeet/lib/actions.js
@@ -2,9 +2,14 @@ const happyFeet = require('happy-feet');
 
 /** @type {import('@gasket/core').HookHandler<'actions'>} */
 module.exports = function actions(gasket) {
+  let happy;
   return {
     getHappyFeet(happyConfig) {
-      return happyFeet(gasket.config.happyFeet || happyConfig || {});
+      if (!happy) {
+        const config = gasket.config.happyFeet || happyConfig || {};
+        happy = happyFeet(config);
+      }
+      return happy;
     }
   };
 };
