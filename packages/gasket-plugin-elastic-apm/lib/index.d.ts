167d9ae2bddcd7ec5de9a495c57c4747c4fb28f9
Michael Mason <mmason2@godaddy.com>
2024-07-24 11:36:30 -0700

    @gasket/plugin-elastic-apm: tune types, update index

--- packages/gasket-plugin-elastic-apm/lib/index.d.ts
+++ packages/gasket-plugin-elastic-apm/lib/index.d.ts
@@ -2,6 +2,7 @@ import type { IncomingMessage, ServerResponse } from 'http';
 import type { AgentConfigOptions, Transaction, Payload } from 'elastic-apm-node';
 import type { MaybeAsync, GasketConfig } from '@gasket/core';
 import type { GasketData } from '@gasket/data';
+import type { GasketRequest } from '@gasket/core';
 
 export function filterSensitiveCookies(config: GasketConfig): function(Payload): Payload;
 
@@ -18,18 +19,17 @@ declare module '@gasket/core' {
     apm?: Agent;
   }
 
+  export interface GasketActions {
+    async getApmTransaction(
+      req: GasketRequest
+    ): Promise<Transaction | void>
+  }
+
   export interface HookExecTypes {
     apmTransaction(
       transaction: Transaction,
-      details: {
-        req: IncomingMessage;
-        res: ServerResponse & {
-          locals?: {
-            gasketData: GasketData & {
-              locale?: string;
-            };
-          };
-        };
+      context: {
+        req: GasketRequest;
       }
     ): MaybeAsync<void>;
   }
