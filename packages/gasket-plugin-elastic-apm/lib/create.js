41493b2d2eaaa731222a8f7dc4bed38d6ee30bba
Michael Mason <mmason2@godaddy.com>
2024-07-29 12:25:32 -0700

    tune create hook, add dotenv dep, adjust tests

--- packages/gasket-plugin-elastic-apm/lib/create.js
+++ packages/gasket-plugin-elastic-apm/lib/create.js
@@ -1,12 +1,17 @@
-const { devDependencies } = require('../package.json');
+const { name, version, devDependencies } = require('../package.json');
 
 /** @type {import('@gasket/core').HookHandler<'create'>} */
-module.exports = function create(gasket, { pkg, files }) {
+module.exports = function create(gasket, { pkg, files, gasketConfig }) {
   const generatorDir = `${__dirname}/../generator`;
 
+  gasketConfig.addPlugin('pluginElasticApm', name);
+
   pkg.add('dependencies', {
+    [name]: `^${version}`,
+    'dotenv': devDependencies['dotenv'],
     'elastic-apm-node': devDependencies['elastic-apm-node']
   });
+
   pkg.add('scripts', {
     start: 'gasket start --require ./setup.js'
   });
--- packages/gasket-plugin-elastic-apm/test/create.test.js
+++ packages/gasket-plugin-elastic-apm/test/create.test.js
@@ -1,25 +1,54 @@
 const create = require('../lib/create');
+const { name, version, devDependencies } = require('../package.json');
 
 describe('create lifecycle', function () {
+  let mockContext;
+
+  beforeEach(() => {
+    mockContext = {
+      pkg: {
+        add: jest.fn()
+      },
+      files: {
+        add: jest.fn()
+      },
+      gasketConfig: {
+        addPlugin: jest.fn()
+      }
+    };
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
   it('is a function', () => {
     expect(typeof create).toStrictEqual('function');
   });
 
+  it('adds the plugin to the gasket config', async () => {
+    await create({}, mockContext);
+    expect(mockContext.gasketConfig.addPlugin).toHaveBeenCalledWith('pluginElasticApm', name);
+  });
+
+  it('adds expected dependencies', async () => {
+    await create({}, mockContext);
+    expect(mockContext.pkg.add).toHaveBeenCalledWith('dependencies', {
+      [name]: `^${version}`,
+      'dotenv': devDependencies['dotenv'],
+      'elastic-apm-node': devDependencies['elastic-apm-node'],
+    });
+  });
+
   it('adds the start script with --require', async () => {
-    const add = jest.fn();
-    await create({}, { pkg: { add }, files: { add } });
-    expect(add).toHaveBeenCalledWith('scripts', {
+    await create({}, mockContext);
+    expect(mockContext.pkg.add).toHaveBeenCalledWith('scripts', {
       start: 'gasket start --require ./setup.js'
     });
   });
 
-  it('adds expected dependencies', async () => {
-    const add = jest.fn();
-    await create({}, { pkg: { add }, files: { add } });
-    expect(add).toHaveBeenCalledWith('dependencies', {
-      'elastic-apm-node': require('../package.json').devDependencies['elastic-apm-node'],
-      'dotenv': require('../package.json').devDependencies.dotenv
-    });
+  it('adds the generator files', async () => {
+    await create({}, mockContext);
+    expect(mockContext.files.add).toHaveBeenCalledWith(expect.stringMatching(/generator\/\*$/));
   });
 });
