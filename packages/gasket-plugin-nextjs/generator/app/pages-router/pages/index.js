f8dac1ed9145f2f8b7a42c7e35e06b16c1f44de5
Michael Mason <mmason2@godaddy.com>
2024-08-05 10:34:26 -0700

    tune generated files

--- packages/gasket-plugin-nextjs/generator/app/pages-router/pages/index.js
+++ packages/gasket-plugin-nextjs/generator/app/pages-router/pages/index.js
@@ -1,3 +1,19 @@
-import IndexPage from '../components/index-page';
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import Head from '../components/head.js';
+import GasketEmblem from '@gasket/assets/react/gasket-emblem.js';
+
+const pageStyle = { textAlign: 'center' };
+const logoStyle = { width: '250px', height: '250px' };
+
+export const IndexPage = () => (
+  <div style={ pageStyle }>
+    <Head title='{{{appName}}}' description='{{{appDescription}}}'/>
+    <GasketEmblem style={ logoStyle }/>
+    <h1>Welcome to Gasket!</h1>
+    <p>To get started, edit <code>pages/index.js</code> and save to reload.</p>
+    <p><a href='https://gasket.dev'>Learn Gasket</a></p>
+  </div>
+);
 
 export default IndexPage;
