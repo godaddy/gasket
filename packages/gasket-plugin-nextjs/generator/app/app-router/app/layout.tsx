6509c43ed66da4f90a8abf84843b1306e7ac7aea
Michael Mason <105235096+mmason2-godaddy@users.noreply.github.com>
2024-08-16 13:50:57 -0700

    [PFX-709] - Tune Custom Server NPM scripts (#851)
    * Remove custom server logic from app-router
    
    * Tune custom server npm scripts
    
    * fix tests

--- packages/gasket-plugin-nextjs/generator/app/app-router/app/layout.tsx
+++ packages/gasket-plugin-nextjs/generator/app/app-router/app/layout.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
-{{#if (eq nextServerType 'customServer')}}
-import gasket from '@/gasket'; // tsconfig path alias
-{{else}}
 import gasket from '../gasket';
-{{/if}}
 import { withGasketData } from '@gasket/nextjs/layout';
 
 function RootLayout({ children }) {
