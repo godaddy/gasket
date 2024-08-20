db3ba8ff569984bae4d05ef8f6dfed65212797cc
Michael Mason <mmason2@godaddy.com>
2024-08-20 16:16:13 -0700

    @gasket/plugin-docs: refactor control structure

--- packages/gasket-plugin-docs/lib/prompt.js
+++ packages/gasket-plugin-docs/lib/prompt.js
@@ -1,16 +1,14 @@
 /** @type {import('@gasket/core').HookHandler<'prompt'>} */
 module.exports = async function promptHook(gasket, context, { prompt }) {
-  if (!('useDocs' in context)) {
-    const { useDocs } = await prompt([
-      {
-        name: 'useDocs',
-        message: 'Do you want to use generated documentation?',
-        type: 'confirm'
-      }
-    ]);
+  if ('useDocs' in context) return context;
 
-    return Object.assign({}, context, { useDocs });
-  }
+  const { useDocs } = await prompt([
+    {
+      name: 'useDocs',
+      message: 'Do you want to use generated documentation?',
+      type: 'confirm'
+    }
+  ]);
 
-  return context;
+  return Object.assign({}, context, { useDocs });
 };
