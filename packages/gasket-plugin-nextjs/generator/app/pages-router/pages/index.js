689b4ff760c2a83fe319843722fd8b418698dc19
Andrew Gerard <agerard@godaddy.com>
2024-08-20 09:49:05 -0700

    fix: examples with react-intl

--- packages/gasket-plugin-nextjs/generator/app/pages-router/pages/index.js
+++ packages/gasket-plugin-nextjs/generator/app/pages-router/pages/index.js
@@ -1,19 +1,30 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+{{#if hasGasketIntl}}
+import { FormattedMessage } from 'react-intl';
+{{/if}}
 import Head from '../components/head.js';
 import GasketEmblem from '@gasket/assets/react/gasket-emblem.js';
 
 const pageStyle = { textAlign: 'center' };
 const logoStyle = { width: '250px', height: '250px' };
 
-const IndexPage = () => (
-  <div style={ pageStyle }>
-    <Head title='{{{appName}}}' description='{{{appDescription}}}'/>
-    <GasketEmblem style={ logoStyle }/>
-    <h1>Welcome to Gasket!</h1>
-    <p>To get started, edit <code>pages/index.js</code> and save to reload.</p>
-    <p><a href='https://gasket.dev'>Learn Gasket</a></p>
-  </div>
-);
+function IndexPage() {
+  return (
+    <div style={pageStyle}>
+      <Head title='{{{appName}}}' description='{{{appDescription}}}'/>
+      <GasketEmblem style={logoStyle}/>
+{{#if hasGasketIntl}}
+      <h1><FormattedMessage id='gasket_welcome' /></h1>
+      <p><FormattedMessage id='gasket_learn' /></p>
+      <p><a href='https://gasket.dev'><FormattedMessage id='gasket_edit_page' /></a></p>
+{{else}}
+      <h1>Welcome to Gasket!</h1>
+      <p>To get started, edit <code>pages/index.js</code> and save to reload.</p>
+      <p><a href='https://gasket.dev'>Learn Gasket</a></p>
+{{/if}}
+    </div>
+  )
+}
 
 export default IndexPage;
