56d626c1345abf7df2a78ddebd35aa4d92cf41fc
bbetts-godaddy <104379885+bbetts-godaddy@users.noreply.github.com>
2024-08-28 10:33:32 -0700

    [PFX-714]: Gasket logger is showing incorrect formatting (#870)
    * update winston log default format
    
    * update winston tests
    
    * make defaultFormat based on env
    
    * update defaultformat and add test

--- packages/gasket-plugin-winston/lib/index.js
+++ packages/gasket-plugin-winston/lib/index.js
@@ -40,15 +40,16 @@ const plugin = {
 
       // eslint-disable-next-line no-sync
       const pluginTransports = gasket.execSync('winstonTransports');
+      const defaultFormat = gasket.config.env.startsWith('local') ?
+        format.simple() :
+        format.combine(format.splat(), format.json());
 
       return createLogger({
         ...config.winston,
         transports: configTransports.concat(
           pluginTransports.flat().filter(Boolean)
         ),
-        format:
-          config.winston?.format ??
-          format.combine(format.splat(), format.json()),
+        format: config.winston?.format ?? defaultFormat,
         levels: Object.assign({ fatal: 0, warn: 4, trace: 7 }, winstonConfig.syslog.levels),
         exitOnError: true
       });
