702ecb5b423d956bcd63d5becbb26574879ee66b
Michael Mason <105235096+mmason2-godaddy@users.noreply.github.com>
2024-08-30 11:50:53 -0700

    update fastify to new pattern (#875)

--- packages/gasket-plugin-fastify/generator/routes/index.js
+++ packages/gasket-plugin-fastify/generator/routes/index.js
@@ -1,25 +1,21 @@
-export const routes = [
-  (app) => {
-    {{#if useSwagger}}
-    /**
-    * @swagger
-    *
-    * /default:
-    *   get:
-    *     summary: "Get default route"
-    *     produces:
-    *       - "application/json"
-    *     responses:
-    *       "200":
-    *         description: "Returns welcome message."
-    *         content:
-    *           application/json
-    */
-    {{/if}}
-    app.get('/default', async (req, res) => {
-      if (res.statusCode === 200) {
-        res.send({ message: 'Welcome to your default route...' });
-      }
-    });
-  } 
-]
+{{#if useSwagger}}
+/**
+* @swagger
+*
+* /default:
+*   get:
+*     summary: "Get default route"
+*     produces:
+*       - "application/json"
+*     responses:
+*       "200":
+*         description: "Returns welcome message."
+*         content:
+*           application/json
+*/
+{{/if}}
+app.get('/default', async (req, res) => {
+  if (res.statusCode === 200) {
+    res.send({ message: 'Welcome to your default route...' });
+  }
+});
