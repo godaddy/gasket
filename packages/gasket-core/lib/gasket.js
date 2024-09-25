300c56771d71568b3526f81367c70f659f8f47d2
Michael Mason <mmason2@godaddy.com>
2024-09-25 16:33:50 -0700

    add logic to load cjs loaded plugins with default exports

--- packages/gasket-core/lib/gasket.js
+++ packages/gasket-core/lib/gasket.js
@@ -38,6 +38,7 @@ export class Gasket {
     // prune nullish and/or empty plugins
     config.plugins = config.plugins
       .filter(Boolean)
+      .map(plugin => plugin.default || plugin) // quality for cjs apps
       .filter(plugin => Boolean(plugin.name) || Boolean(plugin.hooks));
 
     // start the engine
