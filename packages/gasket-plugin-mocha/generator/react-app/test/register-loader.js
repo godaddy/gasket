f6d9521677965d1a7c786a2ef12831682f40b0cb
Jordan Pina <106619535+jpina1-godaddy@users.noreply.github.com>
2024-09-26 10:18:20 -0700

    feat: added node loader for styles for mocha (#929)

--- packages/gasket-plugin-mocha/generator/react-app/test/register-loader.js
+++ packages/gasket-plugin-mocha/generator/react-app/test/register-loader.js
@@ -1,4 +1,8 @@
 import { register } from 'module';
 import { pathToFileURL } from 'url';
 
+// Register the Babel loader
 register('@gasket/plugin-mocha/node-loader-babel', pathToFileURL('./test'));
+
+// Register the styles loader
+register('@gasket/plugin-mocha/node-loader-styles', pathToFileURL('./test'));
