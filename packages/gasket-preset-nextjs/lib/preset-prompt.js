44fbc51ec3abd916e7d116bb1f073475f98b45f4
Andrew Gerard <63810935+agerard-godaddy@users.noreply.github.com>
2024-09-25 17:25:48 -0700

    Fastify fixes (#931)
    * fix: pack the generator dir
    
    * fix: fastify 4 doesn't pair with next 14 well
    
    * fix: no build script for non-TS APIs
    
    * fix: lockfile

--- packages/gasket-preset-nextjs/lib/preset-prompt.js
+++ packages/gasket-preset-nextjs/lib/preset-prompt.js
@@ -13,21 +13,5 @@ export default async function presetPrompt(gasket, context, { prompt }) {
   await nextJsPrompts.promptNextServerType(context, prompt);
   await nextJsPrompts.promptNextDevProxy(context, prompt);
 
-  if (!('server' in context) && context.nextServerType === 'customServer') {
-    const { server } = await prompt([
-      {
-        name: 'server',
-        message: 'Which custom server framework would you like to use?',
-        type: 'list',
-        choices: [
-          { name: 'Express', value: 'express' },
-          { name: 'Fastify', value: 'fastify' }
-        ]
-      }
-    ]);
-
-    Object.assign(context, { server });
-  }
-
   return context;
 }
