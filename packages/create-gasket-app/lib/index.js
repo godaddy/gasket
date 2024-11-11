f2112530c2f5af5ef9e33e76a9114f887cf44051
Jordan Pina <106619535+jpina1-godaddy@users.noreply.github.com>
2024-11-11 10:04:32 -0800

    [PFX-802] Test node 22 (#957)
    * chore: prevent deprecation log
    
    * chore: update message
    
    * chore: package-lock
    
    * chore: set engine to node 22
    
    * chore: added nvmrc and added node 20 to engine

--- packages/create-gasket-app/lib/index.js
+++ packages/create-gasket-app/lib/index.js
@@ -19,6 +19,9 @@ const gasketBin = program
 
 
 async function main() {
+  // Suppress deprecation warning for punycode in node 22
+  // @ts-ignore
+  process.noDeprecation = true;
   const { command, hidden, isDefault } = processCommand(createCommand);
   await warnIfOutdated(pkg.name, pkg.version);
   gasketBin.addCommand(command, { hidden, isDefault });
