ddb5afdad409fd79db64d80a1760cfa0ef353edf
jpina1 <jpina1@godaddy.com>
2024-11-21 14:39:07 -0800

    fix: test

--- packages/create-gasket-app/test/unit/config/default-plugins.test.js
+++ packages/create-gasket-app/test/unit/config/default-plugins.test.js
@@ -6,15 +6,6 @@ import pluginDocs from '@gasket/plugin-docs';
 import pluginDocusaurus from '@gasket/plugin-docusaurus';
 import pluginDynamicPlugins from '@gasket/plugin-dynamic-plugins';
 import { defaultPlugins } from '../../../lib/config/default-plugins.js';
-import { jest } from '@jest/globals';
-
-jest.mock('@gasket/plugin-git', () => jest.fn());
-jest.mock('@gasket/plugin-logger', () => jest.fn());
-jest.mock('@gasket/plugin-metadata', () => jest.fn());
-jest.mock('@gasket/plugin-command', () => jest.fn());
-jest.mock('@gasket/plugin-docs', () => jest.fn());
-jest.mock('@gasket/plugin-docusaurus', () => jest.fn());
-jest.mock('@gasket/plugin-dynamic-plugins', () => jest.fn());
 
 describe('defaultPlugins', () => {
   it('should include the correct plugins', () => {
