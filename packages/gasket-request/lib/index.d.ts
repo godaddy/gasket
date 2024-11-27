b4579bab61d8e702ee758eeaa2d80ea7a1bc2dec
Andrew Gerard <agerard@godaddy.com>
2024-11-27 15:10:03 -0700

    fix: correct req type order

--- packages/gasket-request/lib/index.d.ts
+++ packages/gasket-request/lib/index.d.ts
@@ -44,8 +44,8 @@ export class WeakPromiseKeeper<Key extends WeakKey = WeakKey, Value = any> {
  */
 export async function makeGasketRequest(req: RequestLike): Promise<GasketRequest>;
 
-type RequestActionFn<Result, Args extends Array<unknown>> = (gasket: Gasket, req: RequestLike, ...args: Args) => Promise<Result>;
-type RequestActionWrapperFn<Result, Args extends Array<unknown>> = (gasket: Gasket, req: GasketRequest, ...args: Args) => Promise<Result>;
+type RequestActionFn<Result, Args extends Array<unknown>> = (gasket: Gasket, req: GasketRequest, ...args: Args) => Promise<Result>;
+type RequestActionWrapperFn<Result, Args extends Array<unknown>> = (gasket: Gasket, req: RequestLike, ...args: Args) => Promise<Result>;
 
 export function withGasketRequest<Result, Args extends Array<unknown>>(actionFn: RequestActionFn<Result, Args>): RequestActionWrapperFn<Result, Args>;
 export function withGasketRequestCache<Result, Args extends Array<unknown>>(actionFn: RequestActionFn<Result, Args>): RequestActionWrapperFn<Result, Args>;
