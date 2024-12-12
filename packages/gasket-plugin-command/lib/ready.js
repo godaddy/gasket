027da13e54e85cd42707f263dd906ca2f69a7468
Michael Mason <mmason2@godaddy.com>
2024-12-12 11:34:45 -0800

    Tune command plugin ready hook parse logic

--- packages/gasket-plugin-command/lib/ready.js
+++ packages/gasket-plugin-command/lib/ready.js
@@ -2,7 +2,7 @@ import { gasketBin } from './cli.js';
 
 /** @type {import('@gasket/core').HookHandler<'ready'>} */
 export default async function ready(gasket) {
-  gasket.isReady.then(() => {
+  if (gasket.config.command) {
     gasketBin.parse();
-  });
+  }
 }
