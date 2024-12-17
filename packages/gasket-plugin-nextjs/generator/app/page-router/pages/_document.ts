af0f67f8cef53deda87b3d5ba01c18cae2b429fa
Michael Mason <105235096+mmason2-godaddy@users.noreply.github.com>
2024-12-17 14:28:32 -0800

    Next fixes (#996)
    * tune generated files
    
    * remove ts-expect

--- packages/gasket-plugin-nextjs/generator/app/page-router/pages/_document.ts
+++ packages/gasket-plugin-nextjs/generator/app/page-router/pages/_document.ts
@@ -1,5 +1,11 @@
 import Document from 'next/document';
 import { withGasketData } from '@gasket/nextjs/document';
 import gasket from '@/gasket'; // tsconfig path alias
-
+{{else}}
+{{#if nextDevProxy}}
+import gasket from '@/gasket';
+{{else}}
+import gasket from '../gasket';
+{{/if}}
+{{/if}}
 export default withGasketData(gasket)(Document);
