4e6a07ad72ede5222feaef8e6f6350411c67ef38
Michael Mason <105235096+mmason2-godaddy@users.noreply.github.com>
2024-12-17 16:20:24 -0800

    fix handlebars logic (#997)

--- packages/gasket-plugin-nextjs/generator/app/page-router/pages/_document.ts
+++ packages/gasket-plugin-nextjs/generator/app/page-router/pages/_document.ts
@@ -1,11 +1,10 @@
 import Document from 'next/document';
 import { withGasketData } from '@gasket/nextjs/document';
+{{#if (eq nextServerType 'customServer')}}
 import gasket from '@/gasket'; // tsconfig path alias
-{{else}}
-{{#if nextDevProxy}}
+{{else if nextDevProxy}}
 import gasket from '@/gasket';
 {{else}}
 import gasket from '../gasket';
-{{/if}}
 {{/if}}
 export default withGasketData(gasket)(Document);
