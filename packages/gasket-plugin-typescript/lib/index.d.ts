cecc80816885c055a6807570bcc8b168f785943f
Kawika Bader <ekbader@gmail.com>
2025-01-13 13:16:00 -0700

    fix: lint and types

--- packages/gasket-plugin-typescript/lib/index.d.ts
+++ packages/gasket-plugin-typescript/lib/index.d.ts
@@ -1,12 +1,10 @@
-import type { CreateContext, CreatePrompt } from 'create-gasket-app';
-
 declare module '@gasket/plugin-typescript' {
   export const name = '@gasket/plugin-typescript';
   export const hooks = {};
+}
 
-  /* Externalize TS prompts for preset */
-  export async function promptTypescript(
-    context: CreateContext,
-    prompt: CreatePrompt
-  ): Promise<undefined>
+declare module 'create-gasket-app' {
+  export interface CreateContext {
+    typescript: boolean;
+  }
 }
