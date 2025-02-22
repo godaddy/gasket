f1a02bda3df60e21a80d71c30eafaf59b1945af4
Jordan Pina <106619535+jpina1-godaddy@users.noreply.github.com>
2025-02-21 16:36:23 -0800

    Dynamic docs plugins (#1034)
    * feat: added addEnvironment to ConfigBuilder
    
    * chore: convert analyze to dynamic plugin in local.analyze env
    
    * chore: change comment
    
    * feat: added addDynamicPlugins to ConfigBuilder
    
    * chore: special addDynamicPlugin not necessary
    
    * feat: default analyze plugin to work with Gasket environments and dynamic plugins
    
    * fix: dynamic plugins should be added to gasket config during create
    
    * feat: added addEnvironment to ConfigBuilder
    
    * docs: changelogs
    
    * feat: make docs related plugins dynamic
    
    * chore: update comments
    
    * chore: update changelogs
    
    ---------
    
    Co-authored-by: Andrew Gerard <agerard@godaddy.com>

--- packages/gasket-plugin-metadata/lib/create.js
+++ packages/gasket-plugin-metadata/lib/create.js
@@ -5,7 +5,7 @@ const { name, version } = require('../package.json');
 /** @type {import('@gasket/core').HookHandler<'create'>} */
 export default function create(gasket, { pkg, gasketConfig }) {
   gasketConfig.addPlugin('pluginMetadata', name);
-  pkg.add('dependencies', {
+  pkg.add('devDependencies', {
     [name]: `^${version}`
   });
 }
