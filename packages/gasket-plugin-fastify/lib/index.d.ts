e547aa94bee9f8a3d1370887ffe8395429ff0751
Jordan Pina <106619535+jpina1-godaddy@users.noreply.github.com>
2025-03-11 11:40:05 -0700

    [PFX-864] Express/Fastify/Middleware types (#1050)
    * chore: update middlware types
    
    * chore: remove unused type
    
    * chore: adjust types
    
    * chore: fix types
    
    * chore: gen changeset

--- packages/gasket-plugin-fastify/lib/index.d.ts
+++ packages/gasket-plugin-fastify/lib/index.d.ts
@@ -11,6 +11,15 @@ import type {
 } from 'fastify';
 import type { IncomingMessage, ServerResponse } from 'http';
 
+export interface FastifyConfig {
+  /** Enable compression */
+  compression?: boolean;
+  /** Trust proxy configuration */
+  trustProxy?: FastifyServerOptions['trustProxy'];
+  /** Fastify request logging per route */
+  disableRequestLogging?: boolean;
+}
+
 declare module '@gasket/core' {
   export interface GasketActions {
     /** @deprecated */
@@ -18,32 +27,9 @@ declare module '@gasket/core' {
   }
 
   export interface GasketConfig {
-    fastify?: {
-      /** Enable compression */
-      compression?: boolean;
-      /** Filter for which request URLs invoke Gasket middleware */
-      middlewareInclusionRegex?: RegExp;
-      /** @deprecated */
-      excludedRoutesRegex?: RegExp;
-      /** Trust proxy configuration */
-      trustProxy?: FastifyServerOptions['trustProxy'];
-      /** Fastify request logging per route */
-      disableRequestLogging?: boolean;
-    };
-    /** Middleware configuration */
-    middleware?: {
-      /** Plugin name */
-      plugin: string;
-      /** Paths for middleware */
-      paths?: (string | RegExp)[];
-    }[];
+    fastify?: FastifyConfig
   }
 
-  /**
-   * Handler function type - The middie middleware is added for express
-   * compatibility
-   */
-  type Handler = (req: any, res: any, next: (error?: Error) => void) => void;
 
   /** Error handler function type */
   type ErrorHandler = (
@@ -54,9 +40,6 @@ declare module '@gasket/core' {
   ) => void;
 
   export interface HookExecTypes {
-    middleware(
-      app: FastifyInstance<RawServerDefault, IncomingMessage, ServerResponse<IncomingMessage>, FastifyBaseLogger, FastifyTypeProviderDefault>
-    ): MaybeAsync<MaybeMultiple<Handler> & { paths?: (string | RegExp)[] }>;
     fastify(app: FastifyInstance<RawServerDefault, IncomingMessage, ServerResponse<IncomingMessage>, FastifyBaseLogger, FastifyTypeProviderDefault>): MaybeAsync<void>;
     errorMiddleware(): MaybeAsync<MaybeMultiple<ErrorHandler>>;
   }
