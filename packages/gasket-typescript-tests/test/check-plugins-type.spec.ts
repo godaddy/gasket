9e5608b2b14bedb45a570f3098c97ddab2156c82
Jordan Pina <106619535+jpina1-godaddy@users.noreply.github.com>
2025-03-25 12:02:10 -0700

    [PFX-869] Type checks for plugin imports (#1067)
    * tests: ts plugin type
    
    * chore: gen changeset
    
    * chore: add vitest to fix esm import
    
    * chore: remove white space
    
    * chore: change version
    
    * chore: fix test

---
+++ packages/gasket-typescript-tests/test/check-plugins-type.spec.ts
@@ -0,0 +1,72 @@
+import { Plugin }  from '@gasket/core';
+import pluginAnalyze from '@gasket/plugin-analyze';
+import pluginCommand from '@gasket/plugin-command';
+import pluginCypress from '@gasket/plugin-cypress';
+import pluginData from '@gasket/plugin-data';
+import pluginDocs from '@gasket/plugin-docs';
+import pluginDocsGraphs from '@gasket/plugin-docs-graphs';
+import pluginDocusaurus from '@gasket/plugin-docusaurus';
+import pluginDynamicPlugins from '@gasket/plugin-dynamic-plugins';
+import pluginElasticApm from '@gasket/plugin-elastic-apm';
+import pluginExpress from '@gasket/plugin-express';
+import pluginFastify from '@gasket/plugin-fastify';
+import pluginGit from '@gasket/plugin-git';
+import pluginHappyFeet from '@gasket/plugin-happyfeet';
+import pluginHttps from '@gasket/plugin-https';
+import pluginHttpsProxy from '@gasket/plugin-https-proxy';
+import pluginIntl from '@gasket/plugin-intl';
+import pluginJest from '@gasket/plugin-jest';
+import pluginLint from '@gasket/plugin-lint';
+import pluginLogger from '@gasket/plugin-logger';
+import pluginManifest from '@gasket/plugin-manifest';
+import pluginMetadata from '@gasket/plugin-metadata';
+import pluginMiddleware from '@gasket/plugin-middleware';
+import pluginMocha from '@gasket/plugin-mocha';
+import pluginMorgan from '@gasket/plugin-morgan';
+import pluginNextjs from '@gasket/plugin-nextjs';
+import pluginRedux from '@gasket/plugin-redux';
+import pluginServiceWorker from '@gasket/plugin-service-worker';
+import pluginSwagger from '@gasket/plugin-swagger';
+import pluginTypeScript from '@gasket/plugin-typescript';
+import pluginWebpack from '@gasket/plugin-webpack';
+import pluginWinston from '@gasket/plugin-winston';
+import pluginWorkbox from '@gasket/plugin-workbox';
+
+describe('check plugins type', () => {
+  it('should have correct type', () => {
+    const plugins: Plugin[] = [
+      pluginAnalyze,
+      pluginCommand,
+      pluginCypress,
+      pluginData,
+      pluginDocs,
+      pluginDocsGraphs,
+      pluginDocusaurus,
+      pluginDynamicPlugins,
+      pluginElasticApm,
+      pluginExpress,
+      pluginFastify,
+      pluginGit,
+      pluginHappyFeet,
+      pluginHttps,
+      pluginHttpsProxy,
+      pluginIntl,
+      pluginJest,
+      pluginLint,
+      pluginLogger,
+      pluginManifest,
+      pluginMetadata,
+      pluginMiddleware,
+      pluginMocha,
+      pluginMorgan,
+      pluginNextjs,
+      pluginRedux,
+      pluginServiceWorker,
+      pluginSwagger,
+      pluginTypeScript,
+      pluginWebpack,
+      pluginWinston,
+      pluginWorkbox
+    ];
+  });
+});
