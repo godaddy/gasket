ff4cf5c58b4622130809f830e5afb376c4884fe3
Andrew Gerard <agerard@godaddy.com>
2025-04-18 11:33:38 -0700

    fix(plugin-express): create instance in 1 spot

--- packages/gasket-plugin-express/lib/create-servers.js
+++ packages/gasket-plugin-express/lib/create-servers.js
@@ -1,18 +1,14 @@
-/* eslint-disable max-statements */
 /// <reference types="@gasket/plugin-https" />
 /// <reference types="@gasket/plugin-logger" />
+
+const { getAppInstance } = require('./utils.js');
 
 /**
  * Create the Express instance and setup the lifecycle hooks.
  * @type {import('@gasket/core').HookHandler<'createServers'>}
  */
-module.exports = async function createServers(gasket, serverOpts) {
-  const express = require('express');
-  const { http2 } = gasket.config;
-  const app = http2 ? require('http2-express')(express) : express();
-
-  app.use(require('cookie-parser')());
-
+async function createServers(gasket, serverOpts) {
+  const app = getAppInstance(gasket);
   await gasket.exec('express', app);
 
   const postRenderingStacks = (await gasket.exec('errorMiddleware')).filter(Boolean);
@@ -22,4 +18,6 @@ module.exports = async function createServers(gasket, serverOpts) {
     ...serverOpts,
     handler: app
   };
-};
+}
+
+module.exports = createServers;
