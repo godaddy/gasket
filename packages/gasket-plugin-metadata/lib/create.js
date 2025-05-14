6cf18c27d26dc183a0f1a2f06933e683f820df16
Andrew Gerard <63810935+agerard-godaddy@users.noreply.github.com>
2025-05-14 14:59:43 -0700

    Fix metadata require (#1107)
    * fix: need to create require for esm
    
    * changeset
    
    * test: adjust abort controller test timeout

--- packages/gasket-plugin-metadata/lib/create.js
+++ packages/gasket-plugin-metadata/lib/create.js
@@ -1,3 +1,5 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 const { name, version } = require('../package.json');
 
 /** @type {import('@gasket/core').HookHandler<'create'>} */
