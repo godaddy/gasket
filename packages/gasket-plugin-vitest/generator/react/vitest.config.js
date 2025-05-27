00666cc83eef8ee0282d5ce16cd74668cc75a2c4
bbetts-godaddy <bbetts@godaddy.com>
2025-05-27 13:11:19 -0600

    fix generated code

--- packages/gasket-plugin-vitest/generator/react/vitest.config.js
+++ packages/gasket-plugin-vitest/generator/react/vitest.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-{{#if typescript}}
-import tsConfigPaths from 'vite-tsconfig-paths';
-{{/if}}
 
 export default defineConfig({
   plugins: [
-    react(),
-    {{#if typescript}}
-    tsConfigPaths()
-    {{/if}}
+    react()
   ],
   test: {
     environment: 'jsdom',
