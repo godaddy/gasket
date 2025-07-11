72c0ea31ac18adb99c7fc63f73a92d7150c20e8e
Michael Mason <mmason2@godaddy.com>
2025-07-11 10:48:02 -0700

    Make lifecycle graphs configurable

--- packages/gasket-plugin-docs-graphs/lib/index.d.ts
+++ packages/gasket-plugin-docs-graphs/lib/index.d.ts
@@ -1,5 +1,14 @@
 import type { Plugin } from '@gasket/core';
 
+declare module '@gasket/core' {
+  export interface GasketConfig {
+    docs?: {
+      outputDir?: string;
+      graphs?: boolean;
+    };
+  }
+}
+
 declare const plugin: Plugin;
 
 export default plugin;
