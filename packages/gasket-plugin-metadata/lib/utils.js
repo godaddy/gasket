3a50d5cb62f8dd5ee369c6140d2b34968b871909
Michael Mason <mmason2@godaddy.com>
2025-07-15 09:33:14 -0700

    Fix obsolete linter config

--- packages/gasket-plugin-metadata/lib/utils.js
+++ packages/gasket-plugin-metadata/lib/utils.js
@@ -1,19 +1,22 @@
-import { fileURLToPath } from 'url';
+import path from 'path';
+import { readFile } from 'fs/promises';
 
 /**
  * Try to import a module.
- *
  * @param {string} tryPath - The path to the module to import.
  * @returns {Promise<any>} The imported module.
  */
 export async function tryImport(tryPath) {
-  const options = {};
-  if (tryPath.includes('.json')) {
-    options.with = { type: 'json' };
-  }
+  // Our linter doesn't allow import() syntax with options :(
+  // That's the only reason we need to use fs/promises here
   try {
-    const path = fileURLToPath(import.meta.resolve(tryPath));
-    const mod = await import(path, options);
+    const pkgPath = path.resolve(tryPath);
+    let mod;
+    if (tryPath.includes('.json')) {
+      mod = JSON.parse(await readFile(pkgPath, 'utf8'));
+    } else {
+      mod = await import(pkgPath);
+    }
     return mod.default || mod;
   } catch (err) {
     if (err.code === 'MODULE_NOT_FOUND' || err.code === 'ERR_MODULE_NOT_FOUND') {
