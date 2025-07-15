341ba38384dfec46795c33bcfceabff0c486af48
Michael Mason <mmason2@godaddy.com>
2025-07-15 08:25:50 -0700

    Disable lifecycle graphs via config

--- packages/gasket-plugin-docs-graphs/lib/index.d.ts
+++ packages/gasket-plugin-docs-graphs/lib/index.d.ts
@@ -1,5 +1,13 @@
 import type { Plugin } from '@gasket/core';
 
+declare module '@gasket/core' {
+  export interface GasketConfig {
+    docs?: {
+      graphs?: boolean;
+    };
+  }
+}
+
 declare const plugin: Plugin;
 
 export default plugin;
