21fc0f2c08aaedbd68be51c38ce86c6f6e098d42
Michael Mason <mmason2@godaddy.com>
2025-07-15 08:13:44 -0700

    Opt for import over require

--- packages/gasket-plugin-metadata/lib/utils.js
+++ packages/gasket-plugin-metadata/lib/utils.js
@@ -1,15 +1,22 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+import { fileURLToPath } from 'url';
 
 /**
+ * Try to import a module.
  *
- * @param tryPath
+ * @param {string} tryPath - The path to the module to import.
+ * @returns {Promise<any>} The imported module.
  */
-export function tryRequire(tryPath) {
+export async function tryImport(tryPath) {
+  const options = {};
+  if (tryPath.includes('.json')) {
+    options.with = { type: 'json' };
+  }
   try {
-    return require(tryPath);
+    const path = fileURLToPath(import.meta.resolve(tryPath));
+    const mod = await import(path, options);
+    return mod.default || mod;
   } catch (err) {
-    if (err.code === 'MODULE_NOT_FOUND') {
+    if (err.code === 'MODULE_NOT_FOUND' || err.code === 'ERR_MODULE_NOT_FOUND') {
       return null;
     }
     throw err;
