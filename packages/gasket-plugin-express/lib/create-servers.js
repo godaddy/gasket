59cf97f4626923adda5faebeb62adc4f41aeaaad
Michael Mason <105235096+mmason2-godaddy@users.noreply.github.com>
2025-10-23 09:29:22 -0700

    ESM port plugin-express (#1256)
    * ESM port plugin-express
    
    * changeset
    
    * syncpack
    
    * node prefix for std lib

--- packages/gasket-plugin-express/lib/create-servers.js
+++ packages/gasket-plugin-express/lib/create-servers.js
@@ -1,13 +1,13 @@
 /// <reference types="@gasket/plugin-https" />
 /// <reference types="@gasket/plugin-logger" />
 
-const { getAppInstance } = require('./utils.js');
+import { getAppInstance } from './utils.js';
 
 /**
  * Create the Express instance and setup the lifecycle hooks.
  * @type {import('@gasket/core').HookHandler<'createServers'>}
  */
-async function createServers(gasket, serverOpts) {
+export default async function createServers(gasket, serverOpts) {
   const app = getAppInstance(gasket);
   await gasket.exec('express', app);
 
@@ -19,5 +19,3 @@ async function createServers(gasket, serverOpts) {
     handler: app
   };
 }
-
-module.exports = createServers;
