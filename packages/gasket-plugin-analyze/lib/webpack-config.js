060ae91169b33ff6d4e4f562fd787a825db0289c
Jacob Page <85361835+jpage-godaddy@users.noreply.github.com>
2025-11-24 08:58:46 -0800

    Fix hook type (#1280)
    The `webpackConfig` hook is invoked with `execWaterfallSync`, so hooks must not be asynchronous.

--- packages/gasket-plugin-analyze/lib/webpack-config.js
+++ packages/gasket-plugin-analyze/lib/webpack-config.js
@@ -1,10 +1,14 @@
 /// <reference types="@gasket/plugin-webpack" />
 
+import merge from 'deepmerge';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import defaultConfig from './default-config.js';
+
 /**
  * Adds the Webpack Bundle Analyzer plugin if the analyze flag is set.
  * @type {import('@gasket/core').HookHandler<'webpackConfig'>}
  */
-export default async function webpackConfigHook(gasket, webpackConfig, context) {
+export default function webpackConfigHook(gasket, webpackConfig, context) {
   // eslint-disable-next-line no-process-env
   const { ANALYZE } = process.env;
   const {
@@ -18,10 +22,6 @@ export default async function webpackConfigHook(gasket, webpackConfig, context) 
 
   // Only add the analyzer plugin if enabled
   if (enabled) {
-    const merge = (await import('deepmerge')).default;
-    const { BundleAnalyzerPlugin } = await import('webpack-bundle-analyzer');
-    const defaultConfig = (await import('./default-config.js')).default;
-
     const { isServer } = context;
     const bundleAnalyzerConfig = merge(defaultConfig, userConfig);
     const analyzerOptions = isServer
