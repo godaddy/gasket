1ce401138d08b83a7d256243d11eb32a40864fef
Michael Mason <mmason2@godaddy.com>
2026-01-27 08:55:24 -0800

    @gasket/plugin-nextjs

--- packages/gasket-plugin-nextjs/scripts/fix-cjs-paths.js
+++
@@ -1,6 +0,0 @@
-import { readFileSync, writeFileSync } from 'fs';
-
-const file = 'cjs/webpack-config.cjs';
-const content = readFileSync(file, 'utf8');
-const updated = content.replace(/\.mjs/g, '.cjs');
-writeFileSync(file, updated);
